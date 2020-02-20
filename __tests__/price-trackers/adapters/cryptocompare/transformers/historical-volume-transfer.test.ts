8b65ec65cbc8dae79e394a08a771c22eb20d1429
Brian Faust <hello@basecode.sh>
2020-02-20 14:30:00 +0200

    wip

---
+++ __tests__/price-trackers/adapters/cryptocompare/transformers/historical-volume-transfer.test.ts
@@ -0,0 +1,14 @@
+import { HistoricalVolumeTransformer } from "../../../../../src/price-trackers/adapters/cryptocompare/transformers/historical-volume-transformer";
+
+const stubResponse = require("../__fixtures__/historical.json");
+const stubOptions = { type: "day", dateFormat: "DD.MM" };
+
+describe("CryptoCompare", function() {
+	describe("HistoricalVolumeTransformer", function() {
+		it("should transform the given data", async () => {
+			const subject = new HistoricalVolumeTransformer(stubResponse.Data);
+
+			expect(subject.transform(stubOptions)).toMatchSnapshot();
+		});
+	});
+});
