63d64559d8850d339fec605d07e813bf07fe0cf1
Brian Faust <hello@basecode.sh>
2020-03-23 12:28:32 +0200

    wip

--- __tests__/clients/adapters/eth.test.ts
+++ __tests__/clients/adapters/eth.test.ts
@@ -2,7 +2,7 @@ import { Ethereum } from "../../../src/clients/adapters/eth";
 
 let subject: Ethereum;
 
-beforeEach(() => (subject = new Ethereum()));
+beforeEach(() => (subject = new Ethereum("...")));
 
 describe("Ethereum", function() {
 	it.todo("should ...");
