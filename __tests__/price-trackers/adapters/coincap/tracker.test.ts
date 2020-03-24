795f611877cccb88582e29fcb16a21ceaf1af8e3
Brian Faust <hello@basecode.sh>
2020-03-24 10:31:38 +0200

    wip

--- __tests__/price-trackers/adapters/coincap/tracker.test.ts
+++
@@ -1,9 +0,0 @@
-import { CoinCap } from "../../../../src/price-trackers/adapters/coincap/tracker";
-
-let subject: CoinCap;
-
-beforeEach(() => (subject = new CoinCap()));
-
-describe("CoinCap", function() {
-	it.todo("should ...");
-});
