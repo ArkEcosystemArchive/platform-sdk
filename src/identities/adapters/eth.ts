32a94c8cb00a26245a3519176d86e92e32a328cc
Brian Faust <faustbrian@users.noreply.github.com>
2020-03-24 06:09:24 +0200

    feat: identity generation for ARK (#14)

---
+++ src/identities/adapters/eth.ts
@@ -0,0 +1,28 @@
+import { NotImplemented } from "../../exceptions";
+import { Identity, IdentityInput, KeyPair } from "../contracts";
+
+export class Ethereum implements Identity {
+	public constructor(network: string) {
+		//
+	}
+
+	public getAddress(opts: IdentityInput): string {
+		throw new NotImplemented(this.constructor.name, "getAddress");
+	}
+
+	public getPublicKey(opts: IdentityInput): string {
+		throw new NotImplemented(this.constructor.name, "getPublicKey");
+	}
+
+	public getPrivateKey(opts: IdentityInput): string {
+		throw new NotImplemented(this.constructor.name, "getPrivateKey");
+	}
+
+	public getWIF(opts: IdentityInput): string {
+		throw new NotImplemented(this.constructor.name, "getWIF");
+	}
+
+	public getKeyPair(opts: IdentityInput): KeyPair {
+		throw new NotImplemented(this.constructor.name, "getKeyPair");
+	}
+}
