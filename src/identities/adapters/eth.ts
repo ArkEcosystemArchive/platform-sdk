c48615ba3577a432f6bdfa15353a580c74ac8840
Brian Faust <faustbrian@users.noreply.github.com>
2020-03-24 07:33:00 +0200

    feat: identity generation for ETH (#16)

--- src/identities/adapters/eth.ts
+++ src/identities/adapters/eth.ts
@@ -1,4 +1,6 @@
-import { NotImplemented } from "../../exceptions";
+import Wallet from "ethereumjs-wallet";
+
+import { NotSupported } from "../../exceptions";
 import { Identity, IdentityInput, KeyPair } from "../contracts";
 
 export class Ethereum implements Identity {
@@ -7,22 +9,87 @@ export class Ethereum implements Identity {
 	}
 
 	public getAddress(opts: IdentityInput): string {
-		throw new NotImplemented(this.constructor.name, "getAddress");
+		if (opts.passphrase) {
+			throw new NotSupported("getAddress#passphrase", this.constructor.name);
+		}
+
+		if (opts.multiSignature) {
+			throw new NotSupported("getAddress#multiSignature", this.constructor.name);
+		}
+
+		if (opts.publicKey) {
+			return Wallet.fromPublicKey(Buffer.from(opts.publicKey, "hex")).getAddress().toString("hex");
+		}
+
+		if (opts.privateKey) {
+			return Wallet.fromPrivateKey(Buffer.from(opts.privateKey, "hex")).getAddress().toString("hex");
+		}
+
+		if (opts.wif) {
+			throw new NotSupported("getAddress#wif", this.constructor.name);
+		}
+
+		throw new Error("No input provided.");
 	}
 
 	public getPublicKey(opts: IdentityInput): string {
-		throw new NotImplemented(this.constructor.name, "getPublicKey");
+		if (opts.passphrase) {
+			throw new NotSupported("getPublicKey#passphrase", this.constructor.name);
+		}
+
+		if (opts.multiSignature) {
+			throw new NotSupported("getPublicKey#multiSignature", this.constructor.name);
+		}
+
+		if (opts.wif) {
+			throw new NotSupported("getPublicKey#wif", this.constructor.name);
+		}
+
+		throw new Error("No input provided.");
 	}
 
 	public getPrivateKey(opts: IdentityInput): string {
-		throw new NotImplemented(this.constructor.name, "getPrivateKey");
+		if (opts.passphrase) {
+			throw new NotSupported("getPrivateKey#privateKey", this.constructor.name);
+		}
+
+		if (opts.wif) {
+			throw new NotSupported("getPrivateKey#wif", this.constructor.name);
+		}
+
+		throw new Error("No input provided.");
 	}
 
 	public getWIF(opts: IdentityInput): string {
-		throw new NotImplemented(this.constructor.name, "getWIF");
+		if (opts.passphrase) {
+			throw new NotSupported("getWIF#wif", this.constructor.name);
+		}
+
+		throw new Error("No input provided.");
 	}
 
 	public getKeyPair(opts: IdentityInput): KeyPair {
-		throw new NotImplemented(this.constructor.name, "getKeyPair");
+		if (opts.passphrase) {
+			throw new NotSupported("getKeyPair#passphrase", this.constructor.name);
+		}
+
+		if (opts.publicKey) {
+			throw new NotSupported("getKeyPair#publicKey", this.constructor.name);
+		}
+
+		if (opts.privateKey) {
+			const keyPair = Wallet.fromPrivateKey(Buffer.from(opts.privateKey, "hex"));
+
+			return {
+				publicKey: keyPair.getPublicKey().toString("hex"),
+				privateKey: keyPair.getPrivateKey().toString("hex"),
+			};
+		}
+
+		if (opts.wif) {
+			throw new NotSupported("getKeyPair#wif", this.constructor.name);
+		}
+
+		throw new Error("No input provided.");
 	}
 }
