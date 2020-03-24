e15f504da71561550af7edb387dc4b3c63ec6387
Brian Faust <hello@basecode.sh>
2020-03-24 05:24:47 +0200

    wip

--- __tests__/clients/adapters/ark.test.ts
+++ __tests__/clients/adapters/ark.test.ts
@@ -1,4 +1,5 @@
 import "jest-extended";
+import nock from "nock";
 
 import { Ark } from "../../../src/clients/adapters/ark";
 import { Block, Transaction, Wallet } from "../../../src/clients/adapters/ark/dto";
@@ -7,9 +8,15 @@ let subject: Ark;
 
 beforeEach(() => (subject = new Ark("https://dexplorer.ark.io/api")));
 
+beforeAll(() => nock.disableNetConnect());
+
 describe("Ark", function () {
 	describe("#getBlock", () => {
 		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.get("/blocks/13114381566690093367")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/block.json`));
+
 			const result = await subject.getBlock("13114381566690093367");
 
 			expect(result).toBeInstanceOf(Block);
@@ -18,7 +25,24 @@ describe("Ark", function () {
 
 	describe("#getBlocks", () => {
 		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.get("/blocks")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/blocks.json`));
+
 			const result = await subject.getBlocks();
+
+			expect(result).toBeArray();
+			expect(result[0]).toBeInstanceOf(Block);
+		});
+	});
+
+	describe("#searchBlocks", () => {
+		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.post("/blocks/search")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/blocks.json`));
+
+			const result = await subject.searchBlocks({});
 
 			expect(result).toBeArray();
 			expect(result[0]).toBeInstanceOf(Block);
@@ -27,6 +51,10 @@ describe("Ark", function () {
 
 	describe("#getTransaction", () => {
 		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.get("/transactions/3e3817fd0c35bc36674f3874c2953fa3e35877cbcdb44a08bdc6083dbd39d572")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/transaction.json`));
+
 			const result = await subject.getTransaction(
 				"3e3817fd0c35bc36674f3874c2953fa3e35877cbcdb44a08bdc6083dbd39d572",
 			);
@@ -37,7 +65,24 @@ describe("Ark", function () {
 
 	describe("#getTransactions", () => {
 		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.get("/transactions")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/transactions.json`));
+
 			const result = await subject.getTransactions();
+
+			expect(result).toBeArray();
+			expect(result[0]).toBeInstanceOf(Transaction);
+		});
+	});
+
+	describe("#searchTransactions", () => {
+		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.post("/transactions/search")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/transactions.json`));
+
+			const result = await subject.searchTransactions({});
 
 			expect(result).toBeArray();
 			expect(result[0]).toBeInstanceOf(Transaction);
@@ -46,7 +91,11 @@ describe("Ark", function () {
 
 	describe("#getWallet", () => {
 		it("should succeed", async () => {
-			const result = await subject.getWallet("D6Z26L69gdk9qYmTv5uzk3uGepigtHY4ax");
+			nock("https://dexplorer.ark.io/api")
+				.get("/wallets/DNjuJEDQkhrJ7cA9FZ2iVXt5anYiM8Jtc9")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/wallet.json`));
+
+			const result = await subject.getWallet("DNjuJEDQkhrJ7cA9FZ2iVXt5anYiM8Jtc9");
 
 			expect(result).toBeInstanceOf(Wallet);
 		});
@@ -54,7 +103,24 @@ describe("Ark", function () {
 
 	describe("#getWallets", () => {
 		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.get("/wallets")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/wallets.json`));
+
 			const result = await subject.getWallets();
+
+			expect(result).toBeArray();
+			expect(result[0]).toBeInstanceOf(Wallet);
+		});
+	});
+
+	describe("#searchWallets", () => {
+		it("should succeed", async () => {
+			nock("https://dexplorer.ark.io/api")
+				.post("/wallets/search")
+				.reply(200, require(`${__dirname}/__fixtures__/ark/wallets.json`));
+
+			const result = await subject.searchWallets({});
 
 			expect(result).toBeArray();
 			expect(result[0]).toBeInstanceOf(Wallet);
