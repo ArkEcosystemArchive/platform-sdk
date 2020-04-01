c9221631c1d691aee76a864cfa601edb6e814bd8
Brian Faust <hello@basecode.sh>
2020-04-01 08:15:35 +0300

    wip

---
+++ __tests__/coins/lsk/dto/peer.test.ts
@@ -0,0 +1,28 @@
+import { Peer } from "../../../../src/coins/lsk/dto";
+import Fixture from "../__fixtures__/client/getPeers.json";
+
+let subject: Peer;
+
+beforeEach(() => (subject = new Peer(Fixture.data[0])));
+
+describe("Lisk", function () {
+	test("#getIp", () => {
+		expect(subject.getIp()).toBe("217.182.72.43");
+	});
+
+	test("#getPort", () => {
+		expect(subject.getPort()).toBe(5001);
+	});
+
+	test("#getVersion", () => {
+		expect(subject.getVersion()).toBe("3.0.0-beta.0");
+	});
+
+	test("#getHeight", () => {
+		expect(subject.getHeight()).toBe(377747);
+	});
+
+	test("#getLatency", () => {
+		expect(subject.getLatency()).toBe(-1);
+	});
+});
