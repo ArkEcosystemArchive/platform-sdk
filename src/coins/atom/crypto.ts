3a6590d8d15388275f30f7a0ab3516d69191ecf8
Brian Faust <hello@basecode.sh>
2020-04-27 12:57:05 +0300

    wip

--- src/coins/atom/crypto.ts
+++ src/coins/atom/crypto.ts
@@ -1,3 +1,6 @@
+import { signWithPrivateKey, getNewWalletFromSeed } from "@lunie/cosmos-keys";
+import { createSignedTransaction } from "@lunie/cosmos-api";
+
 import { NotImplemented } from "../../exceptions";
 import { KeyValuePair } from "../../types";
 import { Crypto } from "../contracts/crypto";
@@ -7,7 +10,69 @@ export class Atom implements Crypto {
 		//
 	}
 
-	public createTransfer(data: KeyValuePair): object {
+	public async createTransfer(data: KeyValuePair): Promise<object> {
+		const privateKey = Buffer.from(getNewWalletFromSeed(data.passphrase, "cosmos").privateKey, "hex");
+
+		const signMessage = await createSignedTransaction(
+			{ gas: 1000, gasPrices: [{ amount: "10", denom: "uatom" }], memo: `Hi from Lunie` },
+			[
+				{
+					type: `cosmos-sdk/Send`,
+					value: {
+						inputs: [
+							{
+								address: `cosmos1qperwt9wrnkg5k9e5gzfgjppzpqhyav5j24d66`,
+								coins: [{ denom: `STAKE`, amount: `1` }],
+							},
+						],
+						outputs: [
+							{
+								address: `cosmos1yeckxz7tapz34kjwnjxvmxzurerquhtrmxmuxt`,
+								coins: [{ denom: `STAKE`, amount: `1` }],
+							},
+						],
+					},
+				},
+			],
+			(signMessage) => signWithPrivateKey(signMessage, privateKey),
+			"test-chain",
+			0,
+			12,
+		);
+
+		console.log(signMessage);
+
+		// return createSignedTransaction(
+		// 	{
+		// 		gas: 1000,
+		// 		gasPrices: [{ amount: "10", denom: "uatom" }],
+		// 		memo: "Hi from Lunie",
+		// 	},
+		// 	[
+		// 		{
+		// 			type: "cosmos-sdk/Send",
+		// 			value: {
+		// 				inputs: [
+		// 					{
+		// 						address: "cosmos1qperwt9wrnkg5k9e5gzfgjppzpqhyav5j24d66",
+		// 						coins: [{ denom: "STAKE", amount: "1" }],
+		// 					},
+		// 				],
+		// 				outputs: [
+		// 					{
+		// 						address: "cosmos1yeckxz7tapz34kjwnjxvmxzurerquhtrmxmuxt",
+		// 						coins: [{ denom: "STAKE", amount: "1" }],
+		// 					},
+		// 				],
+		// 			},
+		// 		},
+		// 	],
+		// 	(signMessage) => signWithPrivateKey(signMessage, privateKey),
+		// 	"test-chain",
+		// 	0,
+		// 	12,
+		// );
+
 		throw new NotImplemented(this.constructor.name, "createTransfer");
 	}
 
