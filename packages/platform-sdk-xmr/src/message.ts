b0caa6079243fe4b32072d7948ddfb63c356a69a
Brian Faust <hello@basecode.sh>
2020-04-28 15:28:31 +0300

    wip

---
+++ packages/platform-sdk-xmr/src/message.ts
@@ -0,0 +1,11 @@
+import { Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+
+export class Message implements Contracts.Message {
+	public sign(input): Contracts.SignedMessage {
+		throw new Exceptions.NotImplemented(this.constructor.name, 'sign');
+	}
+
+	public verify(input): boolean {
+		throw new Exceptions.NotImplemented(this.constructor.name, 'verify');
+	}
+}
