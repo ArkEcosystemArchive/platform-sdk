3479ea46794f01783f0178b89ef4121125d39d6c
Brian Faust <hello@basecode.sh>
2020-05-01 08:47:01 +0300

    wip

--- packages/platform-sdk-btc/src/services/client.ts
+++ packages/platform-sdk-btc/src/services/client.ts
@@ -68,6 +68,10 @@ export class ClientService implements Contracts.ClientService {
 		throw new Exceptions.NotImplemented(this.constructor.name, "searchBlocks");
 	}
 
+	public async fees(days: number): Promise<Contracts.KeyValuePair> {
+		throw new Exceptions.NotImplemented(this.constructor.name, "fees");
+	}
+
 	public async syncing(): Promise<boolean> {
 		throw new Exceptions.NotImplemented(this.constructor.name, "syncing");
 	}
--- packages/platform-sdk-xmr/src/services/client.ts
+++ packages/platform-sdk-xmr/src/services/client.ts
@@ -59,6 +59,10 @@ export class ClientService implements Contracts.ClientService {
 		throw new Exceptions.NotImplemented(this.constructor.name, "configuration");
 	}
 
+	public async fees(days: number): Promise<Contracts.KeyValuePair> {
+		throw new Exceptions.NotImplemented(this.constructor.name, "fees");
+	}
+
 	public async syncing(): Promise<boolean> {
 		throw new Exceptions.NotImplemented(this.constructor.name, "syncing");
 	}
