478a790db28c7c84d22d54e56896d4b53eb03b66
Brian Faust <faustbrian@users.noreply.github.com>
2020-05-07 07:11:03 +0300

    refactor(btc): use the platform-sdk-server for communication (#151)

--- packages/platform-sdk-btc/__tests__/services/client.test.ts
+++ packages/platform-sdk-btc/__tests__/services/client.test.ts
@@ -1,4 +1,5 @@
 import "jest-extended";
+import { BigNumber } from "@arkecosystem/utils";
 import nock from "nock";
 
 import { ClientService } from "../../src/services/client";
@@ -6,7 +7,7 @@ import { TransactionData, WalletData } from "../../src/dto";
 
 let subject: ClientService;
 
-beforeEach(async () => (subject = await ClientService.construct({ peer: "https://btc-testnet.ark.io/" })));
+beforeEach(async () => (subject = await ClientService.construct({ peer: "https://coins.com/api/btc" })));
 
 afterEach(() => nock.cleanAll());
 
@@ -15,27 +16,42 @@ beforeAll(() => nock.disableNetConnect());
 describe("ClientService", function () {
 	describe("#transaction", () => {
 		it("should succeed", async () => {
-			nock("https://blockchain.info")
-				.get("/rawtx/b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da?format=json")
+			nock(/.*/)
+				.get("/api/btc/transactions/68ad0264053ab94fa7749e78d2f728911d166ca9af8dbb68e6ee264958ca7f32")
 				.reply(200, require(`${__dirname}/../__fixtures__/client/transaction.json`));
 
 			const result = await subject.transaction(
-				"b6f6991d03df0e2e04dafffcd6bc418aac66049e2cd74b80f14ac86db1e3f0da",
+				"68ad0264053ab94fa7749e78d2f728911d166ca9af8dbb68e6ee264958ca7f32",
 			);
 
 			expect(result).toBeInstanceOf(TransactionData);
+			expect(result.id()).toBe("68ad0264053ab94fa7749e78d2f728911d166ca9af8dbb68e6ee264958ca7f32");
+			expect(result.type()).toBeUndefined();
+			expect(result.typeGroup()).toBeUndefined();
+			expect(result.timestamp()).toBe(1561095453000);
+			expect(result.confirmations()).toEqual(BigNumber.make(159414));
+			// expect(result.nonce()).toBe("...");
+			// expect(result.sender()).toBe("...");
+			// expect(result.recipient()).toBe("...");
+			expect(result.amount()).toEqual(BigNumber.make(3050000));
+			expect(result.fee()).toEqual(BigNumber.make(10000));
+			expect(result.memo()).toBeUndefined();
+			expect(result.blockId()).toBe(1563778);
 		});
 	});
 
 	describe("#wallet", () => {
 		it("should succeed", async () => {
-			nock("https://blockchain.info")
-				.get("/rawaddr/1AJbsFZ64EpEfS5UAjAfcUG8pH8Jn3rn1F")
+			nock(/.*/)
+				.get("/api/btc/wallets/my48EN4kDnGEpRZMBfiDS65wdfwfgCGZRz")
 				.reply(200, require(`${__dirname}/../__fixtures__/client/wallet.json`));
 
-			const result = await subject.wallet("1AJbsFZ64EpEfS5UAjAfcUG8pH8Jn3rn1F");
+			const result = await subject.wallet("my48EN4kDnGEpRZMBfiDS65wdfwfgCGZRz");
 
 			expect(result).toBeInstanceOf(WalletData);
+			expect(result.address()).toBe("my48EN4kDnGEpRZMBfiDS65wdfwfgCGZRz");
+			expect(result.publicKey()).toBe("76a914c05f53de525d80151e209a729cf1c7909c88f88e88ac");
+			expect(result.balance()).toEqual(BigNumber.make(3050000));
 		});
 	});
 });
