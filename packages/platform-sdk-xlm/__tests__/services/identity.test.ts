8e3ab1bc81deb2c3f7f649364ccb5f3216bac732
Brian Faust <faustbrian@users.noreply.github.com>
2020-05-12 09:00:25 +0300

    feat(xlm): implement `IdentityService` (#196)

---
+++ packages/platform-sdk-xlm/__tests__/services/identity.test.ts
@@ -0,0 +1,60 @@
+import "jest-extended";
+
+import { IdentityService } from "../../src/services/identity";
+import { identity } from "../__fixtures__/identity";
+
+let subject: IdentityService;
+
+beforeEach(async () => (subject = await IdentityService.construct({ network: "devnet" })));
+
+describe("IdentityService", () => {
+	describe("#address", () => {
+		it("should generate an output from a passphrase", async () => {
+			const result: any = await subject.address().fromPassphrase(identity.passphrase);
+
+			expect(result).toBe(identity.address);
+		});
+
+		it("should generate an output from a private key", async () => {
+			const result: any = await subject.address().fromPrivateKey(identity.privateKey);
+
+			expect(result).toBe(identity.address);
+		});
+	});
+
+	describe("#publicKey", () => {
+		it("should generate an output from a passphrase", async () => {
+			const result: any = await subject.publicKey().fromPassphrase(identity.passphrase);
+
+			expect(result).toBe(identity.publicKey);
+		});
+	});
+
+	describe("#privateKey", () => {
+		it("should generate an output from a passphrase", async () => {
+			const result: any = await subject.privateKey().fromPassphrase(identity.passphrase);
+
+			expect(result).toBe(identity.privateKey);
+		});
+	});
+
+	describe("#keys", () => {
+		it("should generate an output from a passphrase", async () => {
+			const result: any = await subject.keys().fromPassphrase(identity.passphrase);
+
+			expect(result).toEqual({
+				privateKey: identity.privateKey,
+				publicKey: identity.publicKey,
+			});
+		});
+
+		it("should generate an output from a private key", async () => {
+			const result: any = await subject.keys().fromPrivateKey(identity.privateKey);
+
+			expect(result).toEqual({
+				privateKey: identity.privateKey,
+				publicKey: identity.publicKey,
+			});
+		});
+	});
+});
