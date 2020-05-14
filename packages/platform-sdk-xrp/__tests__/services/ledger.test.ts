584d573135354c08f0c744da0108e63eee3e698d
Brian Faust <faustbrian@users.noreply.github.com>
2020-05-14 05:17:31 +0300

    Update ledger.test.ts

--- packages/platform-sdk-xrp/__tests__/services/ledger.test.ts
+++ packages/platform-sdk-xrp/__tests__/services/ledger.test.ts
@@ -11,58 +11,58 @@ const createMockService = async (record: string) => {
 
 describe("destruct", () => {
 	it("should pass with a resolved transport closure", async () => {
-		await createMockService("").then(async (xrp) => {
-			await expect(xrp.destruct()).resolves.toBeUndefined();
-		});
+		const xrp = await createMockService("")
+		
+		await expect(xrp.destruct()).resolves.toBeUndefined();
 	});
 });
 
 describe("getVersion", () => {
 	it("should pass with an app version", async () => {
-		await createMockService(ledger.appVersion.record).then(async (xrp) => {
-			await expect(xrp.getVersion()).resolves.toEqual(ledger.appVersion.result);
-		});
+		const xrp = await createMockService(ledger.appVersion.record)
+		
+		await expect(xrp.getVersion()).resolves.toEqual(ledger.appVersion.result);
 	});
 });
 
 describe("getPublicKey", () => {
 	it("should pass with a compressed publicKey", async () => {
-		await createMockService(ledger.publicKey.record).then(async (xrp) => {
-			await expect(xrp.getPublicKey(ledger.bip44.path)).resolves.toEqual(ledger.publicKey.result);
-		});
+		const xrp = await createMockService(ledger.publicKey.record)
+		
+		await expect(xrp.getPublicKey(ledger.bip44.path)).resolves.toEqual(ledger.publicKey.result);
 	});
 });
 
 describe("signTransaction", () => {
 	it("should pass with a signature", async () => {
-		await createMockService(ledger.transaction.record).then(async (xrp) => {
-			await expect(
-				xrp.signTransaction(ledger.bip44.path, Buffer.from(ledger.transaction.payload, "hex")),
-			).resolves.toEqual(ledger.transaction.result);
-		});
+		const xrp = await createMockService(ledger.transaction.record)
+		
+		await expect(
+			xrp.signTransaction(ledger.bip44.path, Buffer.from(ledger.transaction.payload, "hex")),
+		).resolves.toEqual(ledger.transaction.result);
 	});
 });
 
 describe("signTransactionWithSchnorr", () => {
 	it("should fail with a 'NotImplemented' error", async () => {
-		await createMockService("").then(async (xrp) => {
-			await expect(xrp.signTransactionWithSchnorr("", Buffer.alloc(0))).rejects.toThrow();
-		});
+		const xrp = await createMockService("")
+		
+		await expect(xrp.signTransactionWithSchnorr("", Buffer.alloc(0))).rejects.toThrow();
 	});
 });
 
 describe("signMessage", () => {
 	it("should fail with a 'NotImplemented' error", async () => {
-		await createMockService("").then(async (xrp) => {
-			await expect(xrp.signMessage("", Buffer.alloc(0))).rejects.toThrow();
-		});
+		const xrp = await createMockService("")
+		
+		await expect(xrp.signMessage("", Buffer.alloc(0))).rejects.toThrow();
 	});
 });
 
 describe("signMessageWithSchnorr", () => {
 	it("should fail with a 'NotImplemented' error", async () => {
-		await createMockService("").then(async (xrp) => {
-			await expect(xrp.signMessageWithSchnorr("", Buffer.alloc(0))).rejects.toThrow();
-		});
+		const xrp = await createMockService("")
+		
+		await expect(xrp.signMessageWithSchnorr("", Buffer.alloc(0))).rejects.toThrow();
 	});
 });
