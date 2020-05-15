c17213a6d2d1add2bc63537d51a169ae1ab8a765
Brian Faust <hello@basecode.sh>
2020-05-15 09:16:15 +0300

    wip

--- packages/platform-sdk-trx/src/services/ledger.ts
+++ packages/platform-sdk-trx/src/services/ledger.ts
@@ -1,28 +1,38 @@
 import { Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import Tron from "@ledgerhq/hw-app-trx";
+import LedgerTransport from "@ledgerhq/hw-transport-node-hid-singleton";
 
 export class LedgerService implements Contracts.LedgerService {
+	readonly #ledger: LedgerTransport;
+	readonly #transport: Tron;
+
 	private constructor(transport: Contracts.LedgerTransport) {
-		//
+		this.#ledger = transport;
+		this.#transport = new Tron(transport);
 	}
 
 	public static async construct(opts: Contracts.LedgerOptions): Promise<LedgerService> {
-		return new LedgerService(opts.transport);
+		return new LedgerService(opts.transport || (await LedgerTransport.create()));
 	}
 
 	public async destruct(): Promise<void> {
-		//
+		await this.#ledger.close();
 	}
 
 	public async getVersion(): Promise<string> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "getVersion");
+		const { version } = await this.#transport.getAppConfiguration();
+
+		return version;
 	}
 
 	public async getPublicKey(path: string): Promise<string> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "getPublicKey");
+		const { publicKey } = await this.#transport.getAddress(path);
+
+		return publicKey;
 	}
 
 	public async signTransaction(path: string, payload: Buffer): Promise<string> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "signTransaction");
+		return this.#transport.signTransaction(path, payload);
 	}
 
 	public async signTransactionWithSchnorr(path: string, payload: Buffer): Promise<string> {
