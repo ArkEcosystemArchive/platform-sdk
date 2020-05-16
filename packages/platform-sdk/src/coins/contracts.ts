111ee2dd9514ef4e9cddf4a75c772a93d4f715f1
Brian Faust <faustbrian@users.noreply.github.com>
2020-05-16 05:53:40 +0300

    refactor: replace `AbstractFactory` with `CoinFactory` (#247)

---
+++ packages/platform-sdk/src/coins/contracts.ts
@@ -0,0 +1,47 @@
+import {
+	ClientService,
+	FeeService,
+	IdentityService,
+	LedgerService,
+	LinkService,
+	MessageService,
+	PeerService,
+	TransactionService,
+} from "../contracts/coins";
+
+export interface FactoryServices {
+	client: ClientService;
+	fee: FeeService;
+	identity: IdentityService;
+	ledger: LedgerService;
+	link: LinkService;
+	message: MessageService;
+	peer: PeerService;
+	transaction: TransactionService;
+}
+
+export interface CoinServices {
+	client: any;
+	fee: any;
+	identity: any;
+	ledger: any;
+	link: any;
+	message: any;
+	peer: any;
+	transaction: any;
+}
+
+export interface CoinOptions {
+	network: string;
+	peer?: string;
+	services?: {
+		client: {};
+		fee: {};
+		identity: {};
+		ledger: { transport?: any }; // todo: add contract for the transport
+		link: {};
+		message: {};
+		peer: {};
+		transaction: {};
+	};
+}
