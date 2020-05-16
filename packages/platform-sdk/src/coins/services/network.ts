2d54a0db43e0064c5f474a1f534557082b8d42d8
Brian Faust <hello@basecode.sh>
2020-05-16 07:34:33 +0300

    wip

--- packages/platform-sdk/src/coins/services/network.ts
+++ packages/platform-sdk/src/coins/services/network.ts
@@ -1,17 +1,20 @@
 import delve from "dlv";
 
-interface Network {
-	explorer: string;
-	currency: {
-		ticker: string;
-		symbol: string;
-	};
-	crypto: {
-		chainId?: string;
-		slip44: number;
-		bech32?: string;
-	};
-}
+// interface Network {
+// 	explorer: string;
+// 	currency: {
+// 		ticker: string;
+// 		symbol: string;
+// 	};
+// 	crypto: {
+// 		chainId?: string;
+// 		slip44: number;
+// 		bech32?: string;
+// 	};
+// }
+
+// todo: come up with an interface because networks can be nested like "cosmos.mainnet" due to some coins supporting multiple sub-coins or forks like ERC20
+type Network = any;
 
 export class NetworkRepository {
 	readonly #networks: Network[];
