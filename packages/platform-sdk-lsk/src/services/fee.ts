351623eb86afef705468f10c815b9a5aa62353c3
Brian Faust <hello@basecode.sh>
2020-05-17 15:39:42 +0300

    wip

--- packages/platform-sdk-lsk/src/services/fee.ts
+++ packages/platform-sdk-lsk/src/services/fee.ts
@@ -1,4 +1,5 @@
-import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import { Coins, Contracts } from "@arkecosystem/platform-sdk";
+import * as transactions from "@liskhq/lisk-transactions";
 
 export class FeeService implements Contracts.FeeService {
 	public static async construct(config: Coins.Config): Promise<FeeService> {
@@ -10,6 +11,35 @@ export class FeeService implements Contracts.FeeService {
 	}
 
 	public async all(days: number): Promise<Contracts.TransactionFees> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "all");
+		return {
+			// Core
+			transfer: this.transform(transactions.constants.TRANSFER_FEE),
+			secondSignature: this.transform(transactions.constants.SIGNATURE_FEE),
+			delegateRegistration: this.transform(transactions.constants.DELEGATE_FEE),
+			vote: this.transform(transactions.constants.VOTE_FEE),
+			multiSignature: this.transform(transactions.constants.MULTISIGNATURE_FEE),
+			ipfs: this.transform(0),
+			multiPayment: this.transform(0),
+			delegateResignation: this.transform(0),
+			htlcLock: this.transform(0),
+			htlcClaim: this.transform(0),
+			htlcRefund: this.transform(0),
+			// Magistrate
+			businessRegistration: this.transform(0),
+			businessResignation: this.transform(0),
+			businessUpdate: this.transform(0),
+			bridgechainRegistration: this.transform(0),
+			bridgechainResignation: this.transform(0),
+			bridgechainUpdate: this.transform(0),
+		};
+	}
+
+	private transform(fee: number): Contracts.TransactionFee {
+		return {
+			static: fee,
+			max: fee,
+			min: fee,
+			avg: fee,
+		};
 	}
 }
