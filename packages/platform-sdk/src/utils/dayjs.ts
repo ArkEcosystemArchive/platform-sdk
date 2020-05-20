ce9dc6a4f97356dccf1d5da0152af1ec1011283c
Brian Faust <faustbrian@users.noreply.github.com>
2020-05-20 10:57:34 +0300

    feat(sdk): add "quarter of year" plugin to `dayjs` (#301)

--- packages/platform-sdk/src/utils/dayjs.ts
+++ packages/platform-sdk/src/utils/dayjs.ts
@@ -1,12 +1,17 @@
+import "dayjs/plugin/advancedFormat";
+import "dayjs/plugin/localizedFormat";
+import "dayjs/plugin/quarterOfYear";
 import "dayjs/plugin/utc";
 
 import day from "dayjs";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 import localizedFormat from "dayjs/plugin/localizedFormat";
+import quarterOfYear from "dayjs/plugin/quarterOfYear";
 import utc from "dayjs/plugin/utc";
 
 day.extend(advancedFormat);
 day.extend(localizedFormat);
+day.extend(quarterOfYear);
 day.extend(utc);
 
 export const dayjs = day.utc;
