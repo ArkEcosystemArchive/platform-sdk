bb2ad1d9f0be8f10887690ea5980315a876bb273
Brian Faust <faustbrian@users.noreply.github.com>
2020-05-25 09:55:31 +0300

    refactor(support): replace `bent` with `reqwest` (#350)

--- packages/platform-sdk-support/src/http.ts
+++ packages/platform-sdk-support/src/http.ts
@@ -1,35 +1,21 @@
-import bent from "bent";
-
-const ensureTrailingSlash = (url: string): string => {
-	const lastCharacter = url.substr(-1);
-
-	if (lastCharacter != "/") {
-		url = url + "/";
-	}
-
-	return url;
-};
+import { Reqwest } from "@kodekeep/reqwest";
 
 export class Http {
-	readonly #host: string | undefined;
+	readonly #client: Reqwest;
 
-	private constructor(host?: string) {
-		this.#host = host ? ensureTrailingSlash(host) : undefined;
+	private constructor(host: string) {
+		this.#client = Reqwest.new(host);
 	}
 
-	public static new(host?: string) {
+	public static new(host: string) {
 		return new Http(host);
 	}
 
 	public async get(path: string, query: object = {}): Promise<Record<string, any>> {
-		// @ts-ignore
-		const searchParams: string = new URLSearchParams(query).toString();
-
-		return bent("json")(searchParams ? `${this.#host}${path}?${searchParams}` : `${this.#host}${path}`);
+		return (await this.#client.get(path, query)).throw().json();
 	}
 
 	public async post(path: string, body: object, headers: object = {}) {
-		// @ts-ignore
-		return bent(this.#host, "POST", "json", 200)(path, body, headers);
+		return (await this.#client.withHeaders(headers).post(path, body)).throw().json();
 	}
 }
