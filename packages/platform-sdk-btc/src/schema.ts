cdaffb98b056f48149991b6b78085d5515c61757
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2020-05-27 18:10:07 -0400

    chore(btc): update joi schema

--- packages/platform-sdk-btc/src/schema.ts
+++ packages/platform-sdk-btc/src/schema.ts
@@ -3,4 +3,8 @@ import Joi from "@hapi/joi";
 export const schema = Joi.object({
 	network: Joi.string().allow("livenet", "testnet"),
 	peer: Joi.string().uri().optional(),
+	httpClient: Joi.object(),
+	services: Joi.object()
+		.keys({ ledger: Joi.object().keys({ transport: Joi.function().class() }) })
+		.optional(),
 });
