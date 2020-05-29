dc0dda77df40cb16436186757aa3137fb4bed845
Brian Faust <hello@basecode.sh>
2020-05-29 07:20:40 +0300

    wip

--- packages/platform-sdk-profiles/__tests__/env.test.ts
+++ packages/platform-sdk-profiles/__tests__/env.test.ts
@@ -2,7 +2,6 @@ import "jest-extended";
 
 import { Environment } from "../src/env";
 import { Profiles } from "../src/profiles";
-import { Settings } from "../src/settings";
 import { HttpClient } from "./stubs/client";
 
 let subject: Environment;
@@ -11,8 +10,4 @@ beforeEach(() => (subject = new Environment({ httpClient: new HttpClient(), stor
 
 test("Environment#profiles", async () => {
 	expect(subject.profiles()).toBeInstanceOf(Profiles);
-});
-
-test("Environment#settings", async () => {
-	expect(subject.settings()).toBeInstanceOf(Settings);
 });
