86ce4654a2ba25d17f5486a17a64332658f6c028
Brian Faust <hello@basecode.sh>
2020-05-31 14:41:17 +0300

    wip

---
+++ packages/platform-sdk-crypto/src/aes.ts
@@ -0,0 +1,25 @@
+import forge from "node-forge";
+
+export class AES {
+	public static encrypt(message: string, password: string, salt: string, iv: string): string {
+		const derivedKey = forge.pkcs5.pbkdf2(password, salt, 5000, 32);
+
+		const cipher = forge.cipher.createCipher("AES-CBC", derivedKey);
+		cipher.start({ iv: forge.util.decode64(iv) });
+		cipher.update(forge.util.createBuffer(message));
+		cipher.finish();
+
+		return forge.util.encode64(cipher.output.getBytes());
+	}
+
+	public static decrypt(cipherText: string, password: string, salt: string, iv: string): string {
+		const derivedKey = forge.pkcs5.pbkdf2(password, salt, 5000, 32);
+
+		const decipher = forge.cipher.createDecipher("AES-CBC", derivedKey);
+		decipher.start({ iv: forge.util.decode64(iv) });
+		decipher.update(forge.util.createBuffer(forge.util.decode64(cipherText)));
+		decipher.finish();
+
+		return decipher.output.toString();
+	}
+}
