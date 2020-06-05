73f9d6e35d0880e5d266401e8a79d017a832d7f9
Brian Faust <hello@basecode.sh>
2020-06-05 05:15:29 +0300

    wip

--- packages/platform-sdk-ada/src/services/message.ts
+++ packages/platform-sdk-ada/src/services/message.ts
@@ -1,6 +1,6 @@
 import { Coins, Contracts } from "@arkecosystem/platform-sdk";
 import { BIP39 } from "@arkecosystem/platform-sdk-crypto";
-import { sign, mnemonicToRootKeypair } from "cardano-crypto.js";
+import { mnemonicToRootKeypair, sign, verify } from "cardano-crypto.js";
 
 export class MessageService implements Contracts.MessageService {
 	public static async construct(config: Coins.Config): Promise<MessageService> {
@@ -12,15 +12,20 @@ export class MessageService implements Contracts.MessageService {
 	}
 
 	public async sign(input: Contracts.MessageInput): Promise<Contracts.SignedMessage> {
-		// const { message, publicKey, signature } = Crypto.Message.sign(input.message, BIP39.normalize(input.mnemonic));
-
 		const walletSecret = await mnemonicToRootKeypair(BIP39.normalize(input.mnemonic), 1);
 
-		return { message: input.message, signer: "", signature: sign(new Buffer(input.message), walletSecret) };
+		return {
+			message: input.message,
+			signer: walletSecret.slice(64, 96).toString("hex"),
+			signature: sign(new Buffer(input.message), walletSecret).toString("hex"),
+		};
 	}
 
 	public async verify(input: Contracts.SignedMessage): Promise<boolean> {
-		return true;
-		// return Crypto.Message.verify({ message: input.message, publicKey: input.signer, signature: input.signature });
+		return verify(
+			Buffer.from(input.message, "utf8"),
+			Buffer.from(input.signer, "hex"),
+			Buffer.from(input.signature, "hex"),
+		);
 	}
 }
