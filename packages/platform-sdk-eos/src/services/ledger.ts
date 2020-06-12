9cbd966d586b4c1ddc3ca9a54c7f179c086a216f
Simon <sleepdefic1t@users.noreply.github.com>
2020-06-12 03:00:43 -0400

    feat(eos): implement `LedgerService` (#412)

--- packages/platform-sdk-eos/src/services/ledger.ts
+++ packages/platform-sdk-eos/src/services/ledger.ts
@@ -1,36 +1,50 @@
 import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import { BIP44 } from "@arkecosystem/platform-sdk-crypto";
+import LedgerTransport from "@ledgerhq/hw-transport-node-hid-singleton";
 
 export class LedgerService implements Contracts.LedgerService {
+	#ledger: LedgerTransport;
+
 	private constructor(transport: Contracts.LedgerTransport) {
-		//
+		this.#ledger = transport;
 	}
 
 	public static async construct(config: Coins.Config): Promise<LedgerService> {
-		return new LedgerService(config.get("services.ledger.transport"));
+		try {
+			return new LedgerService(config.get("services.ledger.transport"));
+		} catch {
+			return new LedgerService(LedgerTransport);
+		}
 	}
 
 	public async destruct(): Promise<void> {
-		//
+		await this.disconnect();
 	}
 
 	public async connect(): Promise<void> {
-		//
+		this.#ledger = await this.#ledger.create();
 	}
 
 	public async disconnect(): Promise<void> {
-		//
+		await this.#ledger.close();
 	}
 
 	public async getVersion(): Promise<string> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "getVersion");
+		const result = await this.#ledger.send(0xd4, 0x06, 0x00, 0x00);
+
+		return `${result[1]}.${result[2]}.${result[3]}`;
 	}
 
 	public async getPublicKey(path: string): Promise<string> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "getPublicKey");
+		const result = await this.#ledger.send(0xd4, 0x02, 0x00, 0x00, this.eosBip44Parse(path));
+
+		return result.slice(1, 1 + result[0]).toString("hex");
 	}
 
 	public async signTransaction(path: string, payload: Buffer): Promise<string> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "signTransaction");
+		const signature = await this.eosSignTransaction(path, payload);
+
+		return signature;
 	}
 
 	public async signTransactionWithSchnorr(path: string, payload: Buffer): Promise<string> {
@@ -43,5 +57,43 @@ export class LedgerService implements Contracts.LedgerService {
 
 	public async signMessageWithSchnorr(path: string, payload: Buffer): Promise<string> {
 		throw new Exceptions.NotImplemented(this.constructor.name, "signMessageWithSchnorr");
+	}
+
+	/**
+	 * EOS-like Bip44 Parsing
+	 */
+	private eosBip44Parse(path: string): Buffer {
+		const elements: number[] = Object.values(BIP44.parse(path));
+		const payload = Buffer.alloc(1 + elements.length * 4);
+		payload[0] = elements.length;
+		elements.forEach((element: number, index: number) => {
+			payload.writeUInt32BE((element += index < 3 ? 0x80000000 : 0), 1 + 4 * index);
+		});
+
+		return payload;
+	}
+
+	/**
+	 * EOS-like Transaction Signing
+	 */
+	private async eosSignTransaction(path: string, rawTxHex: Buffer): Promise<string> {
+		const eosPaths = this.eosBip44Parse(path);
+		const buffer = Buffer.concat([eosPaths, rawTxHex]);
+
+		const chunkSize = 150;
+		const chunks = Array.from({ length: Math.ceil(buffer.length / chunkSize) }, (v, i) => {
+			return buffer.slice(i * chunkSize, i * chunkSize + chunkSize);
+		});
+
+		let response = Buffer.alloc(65);
+		for (let index = 0; index < chunks.length; ++index) {
+			const chunk = chunks[index];
+			await this.#ledger.send(0xd4, 0x04, index === 0 ? 0x00 : 0x80, 0x00, chunk).then((apduResponse) => {
+				response = apduResponse;
+				return response;
+			});
+		}
+
+		return response.slice(0, response.length - 2).toString("hex");
 	}
 }
