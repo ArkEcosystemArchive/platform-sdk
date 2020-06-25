c3abaa132725547629a5572ad120bb98f95ee8bf
Brian Faust <hello@basecode.sh>
2020-06-25 09:48:39 +0300

    wip

--- .pnp.js
+++ .pnp.js
@@ -1120,6 +1120,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@typescript-eslint/eslint-plugin", "virtual:28bf10013f0dc3f5a648d9c22f70a8dd4890ee90e15e92164892c7edce794c8926350a258d8468ebdf2d34bf06cd3d7052aeb5926b807a64078459f2968d96a2#npm:3.4.0"],
             ["@typescript-eslint/parser", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:3.4.0"],
             ["codecov", "npm:3.7.0"],
+            ["collect.js", "npm:4.27.3"],
             ["cross-env", "npm:7.0.2"],
             ["dot-prop", "npm:5.2.0"],
             ["eslint", "npm:7.3.1"],
@@ -6728,6 +6729,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/collect-v8-coverage-npm-1.0.1-39dec86bad-2.zip/node_modules/collect-v8-coverage/",
           "packageDependencies": [
             ["collect-v8-coverage", "npm:1.0.1"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
+      ["collect.js", [
+        ["npm:4.27.3", {
+          "packageLocation": "./.yarn/cache/collect.js-npm-4.27.3-2ff4f100b4-2.zip/node_modules/collect.js/",
+          "packageDependencies": [
+            ["collect.js", "npm:4.27.3"]
           ],
           "linkType": "HARD",
         }]
