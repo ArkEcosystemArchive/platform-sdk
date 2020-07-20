6e5712a7a21543466bdfea02fb6787fdddb13ad4
Brian Faust <faustbrian@users.noreply.github.com>
2020-07-20 08:16:55 +0300

    feat(profiles): implement `MemoryStorage` (#552)

--- packages/platform-sdk-profiles/src/storage/factory.ts
+++ packages/platform-sdk-profiles/src/storage/factory.ts
@@ -1,10 +1,12 @@
 import { Storage } from "../env.models";
 import { LocalStorage } from "./local";
+import { MemoryStorage } from "./memory";
 import { NullStorage } from "./null";
 
 export class StorageFactory {
 	public static make(driver: string): Storage {
 		return {
+			memory: new MemoryStorage(),
 			null: new NullStorage(),
 			indexeddb: new LocalStorage("indexeddb"),
 			websql: new LocalStorage("websql"),
