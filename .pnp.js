52e573388b742d63107ebfeb1297ad0ab3a83ab0
Brian Faust <hello@basecode.sh>
2020-07-22 10:11:28 +0300

    wip

--- .pnp.js
+++ .pnp.js
@@ -79,6 +79,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         "reference": "workspace:packages/platform-sdk-http-got"
       },
       {
+        "name": "@arkecosystem/platform-sdk-http-ky",
+        "reference": "workspace:packages/platform-sdk-http-ky"
+      },
+      {
         "name": "@arkecosystem/platform-sdk-http-node-fetch",
         "reference": "workspace:packages/platform-sdk-http-node-fetch"
       },
@@ -152,6 +156,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ["@arkecosystem/platform-sdk-http-axios", ["workspace:packages/platform-sdk-http-axios"]],
       ["@arkecosystem/platform-sdk-http-bent", ["workspace:packages/platform-sdk-http-bent"]],
       ["@arkecosystem/platform-sdk-http-got", ["workspace:packages/platform-sdk-http-got"]],
+      ["@arkecosystem/platform-sdk-http-ky", ["workspace:packages/platform-sdk-http-ky"]],
       ["@arkecosystem/platform-sdk-http-node-fetch", ["workspace:packages/platform-sdk-http-node-fetch"]],
       ["@arkecosystem/platform-sdk-intl", ["workspace:packages/platform-sdk-intl"]],
       ["@arkecosystem/platform-sdk-ipfs", ["workspace:packages/platform-sdk-ipfs"]],
@@ -986,6 +991,47 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jest", "npm:26.1.0"],
             ["jest-extended", "npm:0.11.5"],
             ["jest-localstorage-mock", "npm:2.4.3"],
+            ["nock", "npm:13.0.2"],
+            ["npm-check-updates", "npm:7.0.2"],
+            ["prettier", "npm:2.0.5"],
+            ["rimraf", "npm:3.0.2"],
+            ["ts-jest", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:26.1.3"],
+            ["typedoc", "virtual:718d81ccdbea80d009ddf282815e1ddbae06dbd32b8310b5d294f48a2ed35e0698a5a02342ad7ebeaf2437a4d1197a11431303cd29d3daf770c5a7253eeda45f#npm:0.17.8"],
+            ["typescript", "patch:typescript@npm%3A3.9.7#builtin<compat/typescript>::version=3.9.7&hash=c79188"]
+          ],
+          "linkType": "SOFT",
+        }]
+      ]],
+      ["@arkecosystem/platform-sdk-http-ky", [
+        ["workspace:packages/platform-sdk-http-ky", {
+          "packageLocation": "./packages/platform-sdk-http-ky/",
+          "packageDependencies": [
+            ["@arkecosystem/platform-sdk-http-ky", "workspace:packages/platform-sdk-http-ky"],
+            ["@arkecosystem/platform-sdk", "workspace:packages/platform-sdk"],
+            ["@sindresorhus/tsconfig", "npm:0.7.0"],
+            ["@types/eslint", "npm:7.2.0"],
+            ["@types/eslint-plugin-prettier", "npm:3.1.0"],
+            ["@types/fs-extra", "npm:9.0.1"],
+            ["@types/jest", "npm:26.0.5"],
+            ["@types/node", "npm:14.0.24"],
+            ["@types/node-fetch", "npm:2.5.7"],
+            ["@types/prettier", "npm:2.0.2"],
+            ["@types/rimraf", "npm:3.0.0"],
+            ["@typescript-eslint/eslint-plugin", "virtual:f2c49ab7656f91042e0f9cd5f5b349c7bdf7caedde8ebdd1bc75b615048cd9ebb6f5c0807a3593da69bd406b31de520702916a80758e65ce103f5bc4ac0f572f#npm:3.7.0"],
+            ["@typescript-eslint/parser", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:3.7.0"],
+            ["codecov", "npm:3.7.2"],
+            ["cross-env", "npm:7.0.2"],
+            ["eslint", "npm:7.5.0"],
+            ["eslint-config-prettier", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:6.11.0"],
+            ["eslint-plugin-jest", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:23.18.0"],
+            ["eslint-plugin-prettier", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:3.1.4"],
+            ["eslint-plugin-simple-import-sort", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:5.0.3"],
+            ["fs-extra", "npm:9.0.1"],
+            ["jest", "npm:26.1.0"],
+            ["jest-extended", "npm:0.11.5"],
+            ["jest-localstorage-mock", "npm:2.4.3"],
+            ["ky", "npm:0.23.0"],
+            ["ky-universal", "virtual:f2c49ab7656f91042e0f9cd5f5b349c7bdf7caedde8ebdd1bc75b615048cd9ebb6f5c0807a3593da69bd406b31de520702916a80758e65ce103f5bc4ac0f572f#npm:0.8.1"],
             ["nock", "npm:13.0.2"],
             ["npm-check-updates", "npm:7.0.2"],
             ["prettier", "npm:2.0.5"],
@@ -5504,6 +5550,27 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             "typescript"
           ],
           "linkType": "HARD",
+        }],
+        ["virtual:f2c49ab7656f91042e0f9cd5f5b349c7bdf7caedde8ebdd1bc75b615048cd9ebb6f5c0807a3593da69bd406b31de520702916a80758e65ce103f5bc4ac0f572f#npm:3.7.0", {
+          "packageLocation": "./.yarn/$$virtual/@typescript-eslint-eslint-plugin-virtual-3124d7dc9f/0/cache/@typescript-eslint-eslint-plugin-npm-3.7.0-98cceb42a7-2.zip/node_modules/@typescript-eslint/eslint-plugin/",
+          "packageDependencies": [
+            ["@typescript-eslint/eslint-plugin", "virtual:f2c49ab7656f91042e0f9cd5f5b349c7bdf7caedde8ebdd1bc75b615048cd9ebb6f5c0807a3593da69bd406b31de520702916a80758e65ce103f5bc4ac0f572f#npm:3.7.0"],
+            ["@typescript-eslint/experimental-utils", "virtual:5c484b0639e2a9a9a4af716ac3b03b46b41501563880938ff7eae29525098a60ab41d6ab3e31563981c3a634fad1642ad30d709f8d6d57959ae50d5319fe4cd0#npm:3.7.0"],
+            ["@typescript-eslint/parser", "virtual:224c5da29f02e93388dd8c0fa002cf6df425366a8d555525b605c1f625380c332b0f71de9b116196809ff055ee6a3bb0f18f917f8cc4b98caf0efaee1c88e947#npm:3.7.0"],
+            ["debug", "virtual:3a4dedf3f7af41df4edd570c93c4126de67c5e458a3b579d255b1e8423f52c355b4621ca7129f7769a7e7f91c1e0abff031564e672c722042a1e843d1951cc0f#npm:4.1.1"],
+            ["eslint", "npm:7.5.0"],
+            ["functional-red-black-tree", "npm:1.0.1"],
+            ["regexpp", "npm:3.1.0"],
+            ["semver", "npm:7.3.2"],
+            ["tsutils", "virtual:5c484b0639e2a9a9a4af716ac3b03b46b41501563880938ff7eae29525098a60ab41d6ab3e31563981c3a634fad1642ad30d709f8d6d57959ae50d5319fe4cd0#npm:3.17.1"],
+            ["typescript", "patch:typescript@npm%3A3.9.7#builtin<compat/typescript>::version=3.9.7&hash=c79188"]
+          ],
+          "packagePeers": [
+            "eslint",
+            "@typescript-eslint/parser",
+            "typescript"
+          ],
+          "linkType": "HARD",
         }]
       ]],
       ["@typescript-eslint/experimental-utils", [
@@ -8480,6 +8547,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
+      ["data-uri-to-buffer", [
+        ["npm:3.0.1", {
+          "packageLocation": "./.yarn/cache/data-uri-to-buffer-npm-3.0.1-830646f9ee-2.zip/node_modules/data-uri-to-buffer/",
+          "packageDependencies": [
+            ["data-uri-to-buffer", "npm:3.0.1"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
       ["data-urls", [
         ["npm:2.0.0", {
           "packageLocation": "./.yarn/cache/data-urls-npm-2.0.0-2b80c32b82-2.zip/node_modules/data-urls/",
@@ -10425,6 +10501,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["fb-watchman", "npm:2.0.1"],
             ["bser", "npm:2.1.1"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
+      ["fetch-blob", [
+        ["npm:2.0.1", {
+          "packageLocation": "./.yarn/cache/fetch-blob-npm-2.0.1-80705154a0-2.zip/node_modules/fetch-blob/",
+          "packageDependencies": [
+            ["fetch-blob", "npm:2.0.1"]
           ],
           "linkType": "HARD",
         }]
@@ -14406,6 +14491,32 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
+      ["ky", [
+        ["npm:0.23.0", {
+          "packageLocation": "./.yarn/cache/ky-npm-0.23.0-6fb31c6f15-2.zip/node_modules/ky/",
+          "packageDependencies": [
+            ["ky", "npm:0.23.0"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
+      ["ky-universal", [
+        ["virtual:f2c49ab7656f91042e0f9cd5f5b349c7bdf7caedde8ebdd1bc75b615048cd9ebb6f5c0807a3593da69bd406b31de520702916a80758e65ce103f5bc4ac0f572f#npm:0.8.1", {
+          "packageLocation": "./.yarn/$$virtual/ky-universal-virtual-7b64449616/0/cache/ky-universal-npm-0.8.1-178ad56617-2.zip/node_modules/ky-universal/",
+          "packageDependencies": [
+            ["ky-universal", "virtual:f2c49ab7656f91042e0f9cd5f5b349c7bdf7caedde8ebdd1bc75b615048cd9ebb6f5c0807a3593da69bd406b31de520702916a80758e65ce103f5bc4ac0f572f#npm:0.8.1"],
+            ["abort-controller", "npm:3.0.0"],
+            ["ky", "npm:0.23.0"],
+            ["node-fetch", "npm:3.0.0-beta.7"],
+            ["web-streams-polyfill", null]
+          ],
+          "packagePeers": [
+            "ky",
+            "web-streams-polyfill"
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
       ["latest-version", [
         ["npm:5.1.0", {
           "packageLocation": "./.yarn/cache/latest-version-npm-5.1.0-ddb9b0eb39-2.zip/node_modules/latest-version/",
@@ -16664,6 +16775,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/node-fetch-npm-2.6.0-29c7a53447-2.zip/node_modules/node-fetch/",
           "packageDependencies": [
             ["node-fetch", "npm:2.6.0"]
+          ],
+          "linkType": "HARD",
+        }],
+        ["npm:3.0.0-beta.7", {
+          "packageLocation": "./.yarn/cache/node-fetch-npm-3.0.0-beta.7-18bf0e0353-2.zip/node_modules/node-fetch/",
+          "packageDependencies": [
+            ["node-fetch", "npm:3.0.0-beta.7"],
+            ["data-uri-to-buffer", "npm:3.0.1"],
+            ["fetch-blob", "npm:2.0.1"]
           ],
           "linkType": "HARD",
         }]
