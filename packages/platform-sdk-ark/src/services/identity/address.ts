f3ae80f78ac2cfbd1c2fdf2738f84c3ae0c456e2
Brian Faust <hello@basecode.sh>
2020-07-27 16:02:15 +0300

    wip

--- packages/platform-sdk-ark/src/services/identity/address.ts
+++ packages/platform-sdk-ark/src/services/identity/address.ts
@@ -30,19 +30,15 @@ export class Address implements Contracts.Address {
 
 	public async validate(address: string): Promise<boolean> {
 		if (this.#config.get("network.id") === "mainnet") {
-			try {
-				const response: any = (
-					await this.#config
-						.get<Contracts.HttpClient>("httpClient")
-						.timeout(1000)
-						.get(`https://neoscan.io/api/main_net/v1/get_last_transactions_by_address/${address}/1`)
-				).json();
+			const response: any = (
+				await this.#config
+					.get<Contracts.HttpClient>("httpClient")
+					.timeout(1000)
+					.get(`https://neoscan.io/api/main_net/v1/get_last_transactions_by_address/${address}/1`)
+			).json();
 
-				if (response && response.length > 0) {
-					throw new Error("This address exists on the NEO Mainnet.");
-				}
-			} catch {
-				//
+			if (response && response.length > 0) {
+				throw new Error("This address exists on the NEO Mainnet.");
 			}
 		}
 
