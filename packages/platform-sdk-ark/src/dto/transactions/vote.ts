1681c4fe375f689e0c2f2463c1c97981f60c1adf
Brian Faust <hello@basecode.sh>
2020-07-30 12:15:11 +0300

    wip

--- packages/platform-sdk-ark/src/dto/transactions/vote.ts
+++ packages/platform-sdk-ark/src/dto/transactions/vote.ts
@@ -4,10 +4,14 @@ import { TransactionData } from "../transaction";
 
 export class VoteData extends TransactionData implements Contracts.VoteData {
 	public votes(): string[] {
-		return this.data.asset.votes.filter((vote: string) => vote.startsWith("+"));
+		return this.data.asset.votes
+			.filter((vote: string) => vote.startsWith("+"))
+			.map((publicKey: string) => publicKey.substr(1));
 	}
 
 	public unvotes(): string[] {
-		return this.data.asset.votes.filter((vote: string) => vote.startsWith("-"));
+		return this.data.asset.votes
+			.filter((vote: string) => vote.startsWith("-"))
+			.map((publicKey: string) => publicKey.substr(1));
 	}
 }
