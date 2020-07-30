c1908f64ac1bdd2d9cbbbb06520dbb204f9261be
Edgar Goetzendorff <hello@dated.fun>
2020-07-31 01:00:33 +0200

    refactor: profile repository methods

--- packages/platform-sdk-profiles/src/repositories/profile-repository.test.ts
+++ packages/platform-sdk-profiles/src/repositories/profile-repository.test.ts
@@ -9,21 +9,21 @@ beforeEach(() => (subject = new ProfileRepository()));
 
 describe("ProfileRepository", () => {
 	it("should push, get, list and forget any given profiles", async () => {
-		expect(subject.all()).toHaveLength(0);
+		expect(subject.values()).toHaveLength(0);
 
 		const john = subject.create("John");
 
-		expect(subject.all()).toHaveLength(1);
+		expect(subject.values()).toHaveLength(1);
 		expect(subject.findById(john.id())).toBeInstanceOf(Profile);
 
 		const jane = subject.create("Jane");
 
-		expect(subject.all()).toHaveLength(2);
+		expect(subject.values()).toHaveLength(2);
 		expect(subject.findById(jane.id())).toBeInstanceOf(Profile);
 
 		subject.forget(jane.id());
 
-		expect(subject.all()).toHaveLength(1);
+		expect(subject.values()).toHaveLength(1);
 		expect(() => subject.findById(jane.id())).toThrow("No profile found for");
 	});
 });
