a4c564dd331e9e558e5ba9885451872642f90ff8
Edgar Goetzendorff <hello@dated.fun>
2020-07-31 10:46:33 +0200

    feat(profiles): implement `ProfileRepository#count` (#609)

--- packages/platform-sdk-profiles/src/repositories/profile-repository.test.ts
+++ packages/platform-sdk-profiles/src/repositories/profile-repository.test.ts
@@ -9,21 +9,21 @@ beforeEach(() => (subject = new ProfileRepository()));
 
 describe("ProfileRepository", () => {
 	it("should push, get, list and forget any given profiles", async () => {
-		expect(subject.values()).toHaveLength(0);
+		expect(subject.count()).toBe(0);
 
 		const john = subject.create("John");
 
-		expect(subject.values()).toHaveLength(1);
+		expect(subject.count()).toBe(1);
 		expect(subject.findById(john.id())).toBeInstanceOf(Profile);
 
 		const jane = subject.create("Jane");
 
-		expect(subject.values()).toHaveLength(2);
+		expect(subject.count()).toBe(2);
 		expect(subject.findById(jane.id())).toBeInstanceOf(Profile);
 
 		subject.forget(jane.id());
 
-		expect(subject.values()).toHaveLength(1);
+		expect(subject.count()).toBe(1);
 		expect(() => subject.findById(jane.id())).toThrow("No profile found for");
 	});
 });
