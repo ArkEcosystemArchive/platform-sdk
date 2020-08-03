a347cb0e20f94a7a40f327b427d76f48344317e6
Brian Faust <faustbrian@users.noreply.github.com>
2020-08-03 13:46:20 +0300

    test(profiles): add tests for `Profile` (#628)

--- packages/platform-sdk-profiles/src/profile.test.ts
+++ packages/platform-sdk-profiles/src/profile.test.ts
@@ -2,13 +2,23 @@ import "jest-extended";
 
 import { ARK } from "@arkecosystem/platform-sdk-ark";
 import { Request } from "@arkecosystem/platform-sdk-http-got";
+import { MarketService } from "@arkecosystem/platform-sdk-markets";
 import nock from "nock";
 
-import { ContactRepository, Identifiers, Profile, SettingRepository, WalletRepository } from "../src";
-import { identity } from "../test/fixtures/identity";
+import { CountAggregate } from "./aggregates/count-aggregate";
+import { TransactionAggregate } from "./aggregates/transaction-aggregate";
+import { WalletAggregate } from "./aggregates/wallet-aggregate";
+import { Authenticator } from "./authenticator";
 import { container } from "./container";
 import { ProfileSetting } from "./profile.models";
 import { DataRepository } from "./repositories/data-repository";
+import { NotificationRepository } from "./repositories/notification-repository";
+import { PluginRepository } from "./repositories/plugin-repository";
+import { Profile } from "./profile";
+import { Identifiers } from "./container.models";
+import { ContactRepository } from "./repositories/contact-repository";
+import { SettingRepository } from "./repositories/setting-repository";
+import { WalletRepository } from "./repositories/wallet-repository";
 
 let subject: Profile;
 
@@ -41,8 +51,16 @@ it("should have a name", () => {
 	expect(subject.name()).toBe("John Doe");
 });
 
-it("should have a wallets repository", () => {
-	expect(subject.wallets()).toBeInstanceOf(WalletRepository);
+it("should have a default avatar", () => {
+	expect(subject.avatar()).toMatchInlineSnapshot(
+		`"<svg version=\\"1.1\\" xmlns=\\"http://www.w3.org/2000/svg\\" width=\\"100\\" height=\\"100\\" viewBox=\\"0 0 100 100\\"><style>circle{mix-blend-mode:soft-light;}</style><rect fill=\\"rgb(233, 30, 99)\\" width=\\"100\\" height=\\"100\\"/><circle r=\\"45\\" cx=\\"80\\" cy=\\"30\\" fill=\\"rgb(76, 175, 80)\\"/><circle r=\\"55\\" cx=\\"0\\" cy=\\"60\\" fill=\\"rgb(255, 152, 0)\\"/><circle r=\\"40\\" cx=\\"50\\" cy=\\"50\\" fill=\\"rgb(3, 169, 244)\\"/></svg>"`,
+	);
+});
+
+it("should have a custom avatar", () => {
+	subject.settings().set(ProfileSetting.Avatar, "custom-avatar");
+
+	expect(subject.avatar()).toBe("custom-avatar");
 });
 
 it("should have a contacts repository", () => {
@@ -53,11 +71,59 @@ it("should have a data repository", () => {
 	expect(subject.data()).toBeInstanceOf(DataRepository);
 });
 
+it("should have a notifications repository", () => {
+	expect(subject.notifications()).toBeInstanceOf(NotificationRepository);
+});
+
+it("should have a plugins repository", () => {
+	expect(subject.plugins()).toBeInstanceOf(PluginRepository);
+});
+
 it("should have a settings repository", () => {
 	expect(subject.settings()).toBeInstanceOf(SettingRepository);
 });
 
-test("#toObject", () => {
+it("should have a wallets repository", () => {
+	expect(subject.wallets()).toBeInstanceOf(WalletRepository);
+});
+
+it("should flush all data", () => {
+	expect(subject.settings().keys()).toHaveLength(1);
+
+	subject.flush();
+
+	expect(subject.settings().keys()).toHaveLength(0);
+});
+
+it("should have a count aggregate", () => {
+	expect(subject.countAggregate()).toBeInstanceOf(CountAggregate);
+});
+
+it("should have a transaction aggregate", () => {
+	expect(subject.transactionAggregate()).toBeInstanceOf(TransactionAggregate);
+});
+
+it("should have a wallet aggregate", () => {
+	expect(subject.walletAggregate()).toBeInstanceOf(WalletAggregate);
+});
+
+it("should have an authenticator", () => {
+	expect(subject.auth()).toBeInstanceOf(Authenticator);
+});
+
+it("should determine if the password uses a password", () => {
+	expect(subject.usesPassword()).toBeFalse();
+
+	subject.auth().setPassword("password");
+
+	expect(subject.usesPassword()).toBeTrue();
+});
+
+it("should have a market", () => {
+	expect(subject.market()).toBeInstanceOf(MarketService);
+});
+
+it("should turn into an object", () => {
 	expect(subject.toObject()).toEqual({
 		id: "uuid",
 		contacts: {},
