5f05f0bef0fa4ff979a1fcc45cd28114f0b905af
Brian Faust <hello@basecode.sh>
2020-08-19 12:25:49 +0300

    chore(profiles): add missing exports

--- packages/platform-sdk-profiles/src/index.ts
+++ packages/platform-sdk-profiles/src/index.ts
@@ -8,16 +8,27 @@ export * from "./environment/env.models";
 export * from "./environment/env";
 export * from "./environment/migrator";
 
+export * from "./profiles/aggregates/count-aggregate";
+export * from "./profiles/aggregates/registration-aggregate";
+export * from "./profiles/aggregates/transaction-aggregate";
+export * from "./profiles/aggregates/wallet-aggregate";
+export * from "./profiles/authenticator";
 export * from "./profiles/profile.models";
 export * from "./profiles/profile";
 
+export * from "./repositories/contact-address-repository";
 export * from "./repositories/contact-repository";
+export * from "./repositories/data-repository";
+export * from "./repositories/notification-repository";
 export * from "./repositories/profile-repository";
 export * from "./repositories/setting-repository";
 export * from "./repositories/wallet-repository";
 
 export * from "./services/avatar";
+export * from "./services/cache";
 
 export * from "./wallets/network";
+export * from "./wallets/read-only-wallet";
+export * from "./wallets/wallet-transaction-service";
 export * from "./wallets/wallet.models";
 export * from "./wallets/wallet";
