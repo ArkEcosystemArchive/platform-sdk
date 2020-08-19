e6dc2bd67e288455258a5d4df3485ffbce909403
Brian Faust <hello@basecode.sh>
2020-08-19 07:35:24 +0300

    wip

--- packages/platform-sdk-atom/src/services/client.test.ts
+++ packages/platform-sdk-atom/src/services/client.test.ts
@@ -1,12 +1,11 @@
 import "jest-extended";
 
-import { DTO } from "@arkecosystem/platform-sdk";
 import { DateTime } from "@arkecosystem/platform-sdk-intl";
 import { BigNumber } from "@arkecosystem/platform-sdk-support";
 import nock from "nock";
 
 import { createConfig } from "../../test/helpers";
-import { TransactionData, WalletData } from "../dto";
+import { SignedTransactionData, TransactionData, WalletData } from "../dto";
 import { ClientService } from "./client";
 
 let subject: ClientService;
