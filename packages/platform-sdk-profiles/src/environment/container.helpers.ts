3090abfb6906a309d1b1763853e4a6751caebeea
Brian Faust <hello@basecode.sh>
2020-09-01 07:35:33 +0300

    wip

--- packages/platform-sdk-profiles/src/environment/container.helpers.ts
+++ packages/platform-sdk-profiles/src/environment/container.helpers.ts
@@ -6,7 +6,3 @@ import { CoinService } from "./services/coin-service";
 
 export const makeCoin = async (coin: string, network: string): Promise<Coins.Coin> =>
 	container.get<CoinService>(Identifiers.CoinService).push(coin, network);
-
-export const listCoins = (): Record<string, Coins.Coin> => container.get<CoinService>(Identifiers.CoinService).all();
-
-export const getAllCoins = (): Coins.Coin[] => container.get<CoinService>(Identifiers.CoinService).values();
