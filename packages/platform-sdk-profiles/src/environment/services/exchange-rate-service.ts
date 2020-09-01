771095581c74ee794ad3ff2f04cc471cf994f0e4
Edgar Goetzendorff <hello@dated.fun>
2020-09-02 01:09:19 +0200

    refactor: get exchange rate once per coin

--- packages/platform-sdk-profiles/src/environment/services/exchange-rate-service.ts
+++ packages/platform-sdk-profiles/src/environment/services/exchange-rate-service.ts
@@ -7,41 +7,36 @@ import { ReadWriteWallet, WalletData } from "../../wallets/wallet.models";
 import { container } from "../container";
 import { Identifiers } from "../container.models";
 
-// TODO: remove duplicate syncing of exchange rates.
-// A profile could have 100 ARK Wallets and we would send 100 requests at the moment.
 export class ExchangeRateService {
 	public async syncAll(): Promise<void> {
 		const profiles: Profile[] = container.get<ProfileRepository>(Identifiers.ProfileRepository).values();
 
 		const promises: Promise<void>[] = [];
 		for (const profile of profiles) {
-			for (const wallet of profile.wallets().values()) {
-				promises.push(this.getExchangeRate(profile, wallet));
+			for (const [currency, wallets] of Object.entries(profile.wallets().allByCoin())) {
+				promises.push(this.getExchangeRate(profile, currency, Object.values(wallets)));
 			}
 		}
 
 		await Promise.allSettled(promises);
 	}
 
-	private async getExchangeRate(profile: Profile, wallet: ReadWriteWallet): Promise<void> {
+	private async getExchangeRate(profile: Profile, currency: string, wallets: ReadWriteWallet[]): Promise<void> {
 		const marketService = MarketService.make(
 			profile.settings().get(ProfileSetting.MarketProvider) || "coingecko",
 			container.get(Identifiers.HttpClient),
 		);
 
 		const exchangeCurrency: string = profile.settings().get(ProfileSetting.ExchangeCurrency) || "BTC";
+		const exchangeRate = await marketService.dailyAverage(
+			currency,
+			exchangeCurrency,
+			+Date.now(),
+		);
 
-		wallet
-			.data()
-			.set(
-				WalletData.ExchangeRate,
-				await marketService.dailyAverage(
-					wallet.currency(),
-					exchangeCurrency,
-					+Date.now(),
-				),
-			);
-
-		wallet.data().set(WalletData.ExchangeCurrency, exchangeCurrency);
+		for (const wallet of wallets) {
+			wallet.data().set(WalletData.ExchangeCurrency, exchangeCurrency);
+			wallet.data().set(WalletData.ExchangeRate, exchangeRate);
+		}
 	}
 }
