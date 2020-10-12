a70c250387eb95b5dbe06908c104876ab0159910
Edgar Goetzendorff <hello@dated.fun>
2020-10-12 08:13:06 +0200

    feat(profiles): implement `WalletAggregate#balancesByNetworkType` (#740)

--- packages/platform-sdk-profiles/src/profiles/aggregates/wallet-aggregate.ts
+++ packages/platform-sdk-profiles/src/profiles/aggregates/wallet-aggregate.ts
@@ -4,6 +4,8 @@ import { Wallet } from "../../wallets/wallet";
 import { ReadWriteWallet } from "../../wallets/wallet.models";
 import { ProfileContract } from "../profile.models";
 
+type NetworkType = "live" | "test";
+
 export class WalletAggregate {
 	readonly #profile: ProfileContract;
 
@@ -11,11 +13,28 @@ export class WalletAggregate {
 		this.#profile = profile;
 	}
 
-	public balance(): BigNumber {
+	public balance(networkType: NetworkType = "live"): BigNumber {
+		return this.balancesByNetworkType()[networkType];
+	}
+
+	public balancesByNetworkType(): Record<NetworkType, BigNumber> {
 		return this.#profile
 			.wallets()
 			.values()
-			.reduce((total: BigNumber, wallet: ReadWriteWallet) => total.plus(wallet.balance()), BigNumber.ZERO);
+			.reduce(
+				(totals: Record<NetworkType, BigNumber>, wallet: ReadWriteWallet) => {
+					const networkType: NetworkType = wallet.network().isLive() ? "live" : "test";
+
+					return {
+						...totals,
+						[networkType]: totals[networkType].plus(wallet.balance()),
+					};
+				},
+				{
+					live: BigNumber.ZERO,
+					test: BigNumber.ZERO,
+				},
+			);
 	}
 
 	public convertedBalance(): BigNumber {
@@ -28,22 +47,30 @@ export class WalletAggregate {
 			);
 	}
 
-	public balancePerCoin(): Record<string, { total: number; percentage: number }> {
+	public balancePerCoin(networkType: NetworkType = "live"): Record<string, { total: number; percentage: number }> {
 		const result = {};
 
-		const totalByProfile: BigNumber = this.#profile.balance();
+		const totalByProfile: BigNumber = this.balance(networkType);
 		const walletsByCoin: Record<string, Record<string, ReadWriteWallet>> = this.#profile.wallets().allByCoin();
 
 		for (const [coin, wallets] of Object.entries(walletsByCoin)) {
-			const totalByCoin: BigNumber = Object.values(wallets).reduce(
-				(total: BigNumber, wallet: ReadWriteWallet) => total.plus(wallet.balance()),
-				BigNumber.ZERO,
+			const matchingWallets = Object.values(wallets).filter(
+				(wallet: ReadWriteWallet) => wallet.network().isLive() === (networkType === "live"),
 			);
 
-			result[coin] = {
-				total: totalByCoin.toFixed(),
-				percentage: totalByProfile.isZero() ? "0.00" : totalByCoin.divide(totalByProfile).times(100).toFixed(2),
-			};
+			if (matchingWallets.length) {
+				const totalByCoin: BigNumber = matchingWallets.reduce(
+					(total: BigNumber, wallet: ReadWriteWallet) => total.plus(wallet.balance()),
+					BigNumber.ZERO,
+				);
+
+				result[coin] = {
+					total: totalByCoin.toFixed(),
+					percentage: totalByProfile.isZero()
+						? "0.00"
+						: totalByCoin.divide(totalByProfile).times(100).toFixed(2),
+				};
+			}
 		}
 
 		return result;
