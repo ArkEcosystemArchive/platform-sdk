48b17df3568c81d0c27ffb2c9e07af12b01a75c2
Brian Faust <hello@basecode.sh>
2020-11-09 04:37:15 +0200

    wip

--- packages/platform-sdk-ark/src/services/fee.ts
+++ packages/platform-sdk-ark/src/services/fee.ts
@@ -2,26 +2,16 @@ import { Coins, Contracts } from "@arkecosystem/platform-sdk";
 import { Arr } from "@arkecosystem/platform-sdk-support";
 
 export class FeeService implements Contracts.FeeService {
+	readonly #config: Coins.Config;
 	readonly #http: Contracts.HttpClient;
-	readonly #peer: string;
 
-	private constructor({ http, peer }) {
-		this.#http = http;
-		this.#peer = peer;
+	private constructor(config: Coins.Config) {
+		this.#config = config;
+		this.#http = config.get<Contracts.HttpClient>("httpClient");
 	}
 
 	public static async construct(config: Coins.Config): Promise<FeeService> {
-		try {
-			return new FeeService({
-				http: config.get<Contracts.HttpClient>("httpClient"),
-				peer: config.get<string>("peer"),
-			});
-		} catch {
-			return new FeeService({
-				http: config.get<Contracts.HttpClient>("httpClient"),
-				peer: `${Arr.randomElement(config.get<string[]>("network.networking.hosts"))}/api`,
-			});
-		}
+			return new FeeService(config);
 	}
 
 	public async destruct(): Promise<void> {
@@ -72,6 +62,14 @@ export class FeeService implements Contracts.FeeService {
 	}
 
 	private async get(path: string, query?: Contracts.KeyValuePair): Promise<Contracts.KeyValuePair> {
-		return (await this.#http.get(`${this.#peer}/${path}`, query)).json();
+		return (await this.#http.get(`${this.host()}/${path}`, query)).json();
+	}
+
+	private host(): string {
+		try {
+			return this.#config.get<string>("peer");
+		} catch {
+			return `${Arr.randomElement(this.#config.get<string[]>("network.networking.hosts"))}/api`;
+		}
 	}
 }
