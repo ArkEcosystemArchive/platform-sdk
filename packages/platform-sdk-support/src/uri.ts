18471beb07c842a0d3544fae72bf8dad07391fbb
Brian Faust <hello@basecode.sh>
2020-11-12 05:32:36 +0200

    wip

--- packages/platform-sdk-support/src/uri.ts
+++ packages/platform-sdk-support/src/uri.ts
@@ -3,6 +3,7 @@ import { number, object, string } from "yup";
 
 export class URI {
 	readonly #pattern: RegExp = new RegExp(/^(?:ark:)([-0-9a-zA-Z]{1,34})([-a-zA-Z0-9+&@#/%=~_|$?!:,.]*)$/);
+	readonly #methods: string[] = ['transfer', 'vote', 'sign-message', 'register-delegate'];
 
 	public serialize(input: Record<string, string>): string {
 		const method: string = input.method;
@@ -20,19 +21,20 @@ export class URI {
 		}
 
 		try {
+			let method: string = parsed[1];
+			const params: any = querystring.parse(parsed[2].substring(1));
+
+			// When this is false we just have to assume that we are handling AIP13
+			// unless we integrate the parsing more tightly to specific coins which
+			// would enable us to validate against specific address validation rules.
+			if (! this.#methods.includes(method)) {
+				params.recipient = method;
+				method = 'transfer';
+			}
+
 			const result = object()
-				.shape({
-					method: string().required(),
-					coin: string().required(),
-					network: string().required(),
-					recipient: string().required(),
-					amount: number().required(),
-					memo: string(),
-				})
-				.validateSync({
-					method: parsed[1],
-					...querystring.parse(parsed[2].substring(1)),
-				});
+				.shape(this.getSchema(method))
+				.validateSync({ method, ...params });
 
 			for (const [key, value] of Object.entries(result)) {
 				result[key] = this.decodeURIComponent(value);
@@ -50,5 +52,46 @@ export class URI {
 		}
 
 		return value;
+	}
+
+	private getSchema(method: string): object {
+		const baseSchema = {
+			method: string().matches(/(transfer|vote|sign-message|register-delegate)/),
+			coin: string().default('ark'),
+			network: string().default('ark.mainnet'),
+			fee: number(),
+		};
+
+		if (method === 'transfer') {
+			return {
+				...baseSchema,
+				recipient: string().required(),
+				amount: number(),
+				memo: string(),
+				vendorField: string(), // Legacy memo, not an ARK agnostic name
+			};
+		}
+
+		if (method === 'vote') {
+			return {
+				...baseSchema,
+				delegate: string().required(),
+			};
+		}
+
+		if (method === 'sign-message') {
+			return {
+				message: string().required(),
+			};
+		}
+
+		if (method === 'register-delegate') {
+			return {
+				...baseSchema,
+				delegate: string().required(),
+			};
+		}
+
+		throw new Error('Invalid method used in URI');
 	}
 }
