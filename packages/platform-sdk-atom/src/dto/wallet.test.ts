528ac6230d70101e2aa923b83f06a3f7c55c3fda
goga-m <goga.mamoulasvili@gmail.com>
2020-11-29 14:03:01 +0200

    test(atom): dto wallet coverage

--- packages/platform-sdk-atom/src/dto/wallet.test.ts
+++ packages/platform-sdk-atom/src/dto/wallet.test.ts
@@ -6,13 +6,42 @@ import Fixture from "../../test/fixtures/client/wallet.json";
 import { WalletData } from "./wallet";
 
 describe("WalletData", function () {
-	it("should succeed", async () => {
-		const result = new WalletData(Fixture.result.value);
+	const subject = new WalletData(Fixture.result.value);
+	const implementedMethods = {
+		address: "cosmos1de7pk372jkp9vrul0gv5j6r3l9mt3wa6m4h6h0",
+		publicKey: "Ap65s+Jdgo8BtvTbkc7GyUti8yJ7RpZ7cE1zCuKgNeXY",
+		balance: BigNumber.make(69519574),
+		entities: [],
+		nonce: BigNumber.make(24242),
+	};
 
-		expect(result).toBeInstanceOf(WalletData);
-		expect(result.address()).toBe("cosmos1de7pk372jkp9vrul0gv5j6r3l9mt3wa6m4h6h0");
-		expect(result.publicKey()).toBe("Ap65s+Jdgo8BtvTbkc7GyUti8yJ7RpZ7cE1zCuKgNeXY");
-		expect(result.balance()).toEqual(BigNumber.make(69519574));
-		expect(result.nonce()).toEqual(BigNumber.make(24242));
+	it("should be instance of WalletData", async () => {
+		expect(subject).toBeInstanceOf(WalletData);
+	});
+
+	describe("Implemented", function () {
+		it.each(Object.keys(implementedMethods))("#%s", async (method: string) => {
+			expect(subject[method]()).toEqual(implementedMethods[method]);
+		});
+	});
+
+	describe("Not implemented", function () {
+		const subject = new WalletData(Fixture.result.value);
+		const nonImplementedMethods = [
+			"secondPublicKey",
+			"username",
+			"rank",
+			"votes",
+			"multiSignature",
+			"isDelegate",
+			"isKnown",
+			"isMultiSignature",
+			"isSecondSignature",
+			"isResignedDelegate",
+		];
+
+		it.each(nonImplementedMethods)("#%s", async (method: string) => {
+			expect(() => subject[method]()).toThrow(/not implemented/);
+		});
 	});
 });
