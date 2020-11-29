e7d7a8d9265f558a9f27465b2e61dace3c753e56
goga-m <goga.mamoulasvili@gmail.com>
2020-11-29 14:02:41 +0200

    test(btc): dto wallet coverage

--- packages/platform-sdk-btc/src/dto/wallet.test.ts
+++ packages/platform-sdk-btc/src/dto/wallet.test.ts
@@ -6,12 +6,41 @@ import Fixture from "../../test/fixtures/client/wallet.json";
 import { WalletData } from "./wallet";
 
 describe("WalletData", function () {
-	it("should succeed", async () => {
-		const result = new WalletData(Fixture);
+	const subject = new WalletData(Fixture);
+	const implementedMethods = {
+		address: "my48EN4kDnGEpRZMBfiDS65wdfwfgCGZRz",
+		publicKey: "76a914c05f53de525d80151e209a729cf1c7909c88f88e88ac",
+		balance: BigNumber.make(3050000),
+		entities: [],
+		nonce: BigNumber.make(24242),
+	};
 
-		expect(result).toBeInstanceOf(WalletData);
-		expect(result.address()).toBe("my48EN4kDnGEpRZMBfiDS65wdfwfgCGZRz");
-		expect(result.publicKey()).toBe("76a914c05f53de525d80151e209a729cf1c7909c88f88e88ac");
-		expect(result.balance()).toEqual(BigNumber.make(3050000));
+	it("should be instance of WalletData", async () => {
+		expect(subject).toBeInstanceOf(WalletData);
+	});
+
+	describe("Implemented", function () {
+		it.each(Object.keys(implementedMethods))("#%s", async (method: string) => {
+			expect(subject[method]()).toEqual(implementedMethods[method]);
+		});
+	});
+
+	describe("Not implemented", function () {
+		const nonImplementedMethods = [
+			"secondPublicKey",
+			"username",
+			"rank",
+			"votes",
+			"multiSignature",
+			"isDelegate",
+			"isKnown",
+			"isMultiSignature",
+			"isSecondSignature",
+			"isResignedDelegate",
+		];
+
+		it.each(nonImplementedMethods)("#%s", async (method: string) => {
+			expect(() => subject[method]()).toThrow(/not implemented/);
+		});
 	});
 });
