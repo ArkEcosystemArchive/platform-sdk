bdbcaecbbda2a7502abd37765972cf6cdc972cea
goga-m <goga.mamoulasvili@gmail.com>
2020-11-29 13:59:37 +0200

    test(neo): dto wallet coverage

---
+++ packages/platform-sdk-neo/src/dto/wallet.test.ts
@@ -0,0 +1,46 @@
+import "jest-extended";
+
+import { BigNumber } from "@arkecosystem/platform-sdk-support";
+
+import Fixture from "../../test/fixtures/client/wallet.json";
+import { WalletData } from "./wallet";
+
+describe("WalletData", function () {
+	const subject = new WalletData(Fixture);
+	const implementedMethods = {
+		address: "AStJyBXGGBK6bwrRfRUHSjp993PB5C9QgF",
+		balance: BigNumber.make(3050000),
+		entities: [],
+		nonce: BigNumber.make(24242),
+	};
+
+	it("should be instance of WalletData", async () => {
+		expect(subject).toBeInstanceOf(WalletData);
+	});
+
+	describe("Implemented", function () {
+		it.each(Object.keys(implementedMethods))("#%s", async (method: string) => {
+			expect(subject[method]()).toEqual(implementedMethods[method]);
+		});
+	});
+
+	describe("Not implemented", function () {
+		const nonImplementedMethods = [
+			"secondPublicKey",
+			"username",
+			"publicKey",
+			"rank",
+			"votes",
+			"multiSignature",
+			"isDelegate",
+			"isKnown",
+			"isMultiSignature",
+			"isSecondSignature",
+			"isResignedDelegate",
+		];
+
+		it.each(nonImplementedMethods)("#%s", async (method: string) => {
+			expect(() => subject[method]()).toThrow(/not implemented/);
+		});
+	});
+});
