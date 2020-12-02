49001620fc1fb0392bda52507c9347a47f8b79c4
faustbrian <faustbrian@users.noreply.github.com>
2020-12-02 03:04:39 +0000

    style: resolve style guide violations

--- packages/platform-sdk-atom/src/index.ts
+++ packages/platform-sdk-atom/src/index.ts
@@ -1,6 +1,6 @@
 import { manifest } from "./manifest";
-import { ServiceProvider } from "./service-provider";
 import { schema } from "./schema";
+import { ServiceProvider } from "./service-provider";
 import * as services from "./services";
 
 export const ATOM = {
