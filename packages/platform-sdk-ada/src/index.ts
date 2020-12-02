a837cdd8a311ae8fe23e08d318949d6246395c5a
Brian Faust <hello@basecode.sh>
2020-12-02 06:07:47 +0200

    wip

--- packages/platform-sdk-ada/src/index.ts
+++ packages/platform-sdk-ada/src/index.ts
@@ -1,11 +1,9 @@
 import { manifest } from "./manifest";
 import { schema } from "./schema";
 import { ServiceProvider } from "./service-provider";
-import * as services from "./services";
 
 export const ADA = {
 	manifest,
 	schema,
 	ServiceProvider,
-	services,
 };
