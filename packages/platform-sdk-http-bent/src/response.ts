c2a8c47503183b45fca88ad67e18698002639ea7
Brian Faust <hello@basecode.sh>
2020-12-04 06:43:20 +0200

    wip

--- packages/platform-sdk-http-bent/src/response.ts
+++
@@ -1,9 +0,0 @@
-import { Http } from "@arkecosystem/platform-sdk";
-
-export class Response extends Http.Response {
-	public constructor(response, error?: Error | undefined) {
-		super(response, error);
-
-		this._body = JSON.stringify(response.body);
-	}
-}
