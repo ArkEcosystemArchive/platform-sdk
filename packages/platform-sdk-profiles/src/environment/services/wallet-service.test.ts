132d9bb6599f8bacad9796ec205e93577d721072
goga-m <goga.mamoulasvili@gmail.com>
2020-12-07 19:46:21 +0200

    test: wallet-service coverage

---
+++ packages/platform-sdk-profiles/src/environment/services/wallet-service.test.ts
@@ -0,0 +1,126 @@
+import "jest-extended";
+
+import { ARK } from "@arkecosystem/platform-sdk-ark";
+import { Request } from "@arkecosystem/platform-sdk-http-got";
+import nock from "nock";
+
+import { identity } from "../../../test/fixtures/identity";
+import { container } from "../../environment/container";
+import { Identifiers } from "../../environment/container.models";
+import { Profile } from "../../profiles/profile";
+import { ProfileSetting } from "../../profiles/profile.models";
+import { ProfileRepository } from "../../repositories/profile-repository";
+import { ReadWriteWallet, WalletData } from "../../wallets/wallet.models";
+import { CoinService } from "./coin-service";
+import { WalletService } from "./wallet-service";
+
+let profile: Profile;
+let wallet: ReadWriteWallet;
+let subject: WalletService;
+
+let liveSpy: jest.SpyInstance;
+let testSpy: jest.SpyInstance;
+
+beforeEach(async () => {
+	nock.cleanAll();
+
+	nock(/.+/)
+		.get("/api/node/configuration")
+		.reply(200, require("../../../test/fixtures/client/configuration.json"))
+		.get("/api/peers")
+		.reply(200, require("../../../test/fixtures/client/peers.json"))
+		.get("/api/node/configuration/crypto")
+		.reply(200, require("../../../test/fixtures/client/cryptoConfiguration.json"))
+		.get("/api/node/syncing")
+		.reply(200, require("../../../test/fixtures/client/syncing.json"))
+		.get("/api/wallets/D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib")
+		.reply(200, require("../../../test/fixtures/client/wallet.json"))
+		.get("/api/delegates")
+		.reply(200, require("../../../test/fixtures/client/delegates-1.json"))
+		.get("/api/delegates?page=2")
+		.reply(200, require("../../../test/fixtures/client/delegates-2.json"))
+		// coingecho
+		.get("/api/v3/coins/dark/history")
+		.query(true)
+		.reply(200, {
+			id: "ark",
+			symbol: "ark",
+			name: "Ark",
+			market_data: {
+				current_price: {
+					btc: 0.0006590832396635801,
+				},
+				market_cap: {
+					btc: 64577.8220851173,
+				},
+				total_volume: {
+					btc: 3054.8117101964535,
+				},
+			},
+		})
+		// coingecho
+		.get("/api/v3/coins/list")
+		.query(true)
+		.reply(200, [
+			{
+				id: "ark",
+				symbol: "ark",
+				name: "ark",
+			},
+			{
+				id: "dark",
+				symbol: "dark",
+				name: "dark",
+			},
+		])
+		.persist();
+
+	const profileRepository = new ProfileRepository();
+
+	container.set(Identifiers.HttpClient, new Request());
+	container.set(Identifiers.CoinService, new CoinService());
+	container.set(Identifiers.Coins, { ARK });
+	container.set(Identifiers.ProfileRepository, profileRepository);
+
+	profile = profileRepository.create("John Doe");
+
+	wallet = await profile.wallets().importByMnemonic(identity.mnemonic, "ARK", "ark.devnet");
+
+	liveSpy = jest.spyOn(wallet.network(), "isLive").mockReturnValue(true);
+	testSpy = jest.spyOn(wallet.network(), "isTest").mockReturnValue(false);
+
+	subject = new WalletService();
+});
+
+afterEach(() => {
+	liveSpy.mockRestore();
+	testSpy.mockRestore();
+});
+
+beforeAll(() => nock.disableNetConnect());
+
+describe("WalletService", () => {
+	it("#syncAll", async () => {
+		expect(() => wallet.votes()).toThrowError(/has not been synced/);
+		await subject.syncAll();
+		expect(() => wallet.votes()).not.toThrowError(/has not been synced/);
+
+		// @ts-ignore
+		const mockUndefinedWallets = jest.spyOn(profile.wallets(), "values").mockReturnValue([undefined]);
+		await subject.syncAll();
+		mockUndefinedWallets.mockRestore();
+	});
+
+	it("#syncByProfile", async () => {
+		expect(() => wallet.votes()).toThrowError(/has not been synced/);
+		await subject.syncByProfile(profile);
+		expect(() => wallet.votes()).not.toThrowError(/has not been synced/);
+
+		// @ts-ignore
+		const mockUndefinedWallets = jest.spyOn(profile.wallets(), "values").mockReturnValue([undefined]);
+		await subject.syncByProfile(profile);
+		mockUndefinedWallets.mockRestore();
+	});
+
+	it("#syncByProfile", async () => {});
+});
