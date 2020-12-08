6a47a8d717b46d76597b8114233655cf7a451e75
goga-m <goga.mamoulasvili@gmail.com>
2020-12-08 16:37:24 +0200

    test: entity-aggregate coverage

--- packages/platform-sdk-profiles/src/profiles/aggregates/entity-aggregate.test.ts
+++ packages/platform-sdk-profiles/src/profiles/aggregates/entity-aggregate.test.ts
@@ -9,8 +9,16 @@ import { container } from "../../environment/container";
 import { Identifiers } from "../../environment/container.models";
 import { CoinService } from "../../environment/services/coin-service";
 import { Profile } from "../profile";
+import { ExtendedTransactionDataCollection } from "../../dto/transaction-collection";
+import * as promiseHelpers from "../../helpers/promise";
 
 let profile: Profile;
+
+const entityActionMap = {
+	registrations: "register",
+	updates: "update",
+	resignations: "resign",
+};
 
 beforeEach(async () => {
 	nock.disableNetConnect();
@@ -31,6 +39,7 @@ beforeEach(async () => {
 	container.set(Identifiers.HttpClient, new Request());
 	container.set(Identifiers.CoinService, new CoinService());
 	container.set(Identifiers.Coins, { ARK });
+
 	profile = new Profile({ id: "uuid" });
 	const address = "D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb";
 
@@ -39,88 +48,140 @@ beforeEach(async () => {
 
 afterAll(() => nock.enableNetConnect());
 
-it("should aggregate registrations for the given type and sub-type", async () => {
-	nock.cleanAll();
-	nock(/.+/)
-		.get("/api/transactions")
-		.query(true)
-		.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
+describe("EntityAggregate", () => {
+	describe.each(["registrations", "updates", "resignations"])("%s", (method: string) => {
+		it("should have more", async () => {
+			nock.cleanAll();
+			nock(/.+/)
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
 
-	const registrations = await profile.entityAggregate().registrations(EntityType.Business, EntitySubType.None);
+			const results = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(results).toBeInstanceOf(ExtendedTransactionDataCollection);
+			expect(results.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeTrue();
+		});
 
-	expect(registrations.items()).toHaveLength(1);
-	expect(registrations.findById("df520b0a278314e998dc93be1e20c72b8313950c19da23967a9db60eb4e990da")).toBeTruthy();
-});
+		it("should have no more", async () => {
+			nock.cleanAll();
+			nock(/.+/)
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/no-more.json"));
 
-it("should aggregate updates for the given type and sub-type", async () => {
-	nock.cleanAll();
-	nock(/.+/)
-		.get("/api/transactions")
-		.query(true)
-		.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
+			const results = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(results).toBeInstanceOf(ExtendedTransactionDataCollection);
+			expect(results.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeFalse();
+		});
 
-	const updates = await profile.entityAggregate().updates(EntityType.Business, EntitySubType.None);
+		it("should skip empty results for processing", async () => {
+			nock.cleanAll();
+			nock(/.+/)
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
 
-	expect(updates.items()).toHaveLength(1);
-	expect(updates.findById("df520b0a278314e998dc93be1e20c72b8313950c19da23967a9db60eb4e990da")).toBeTruthy();
-});
+			const firstPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(firstPage.findById("df520b0a278314e998dc93be1e20c72b8313950c19da23967a9db60eb4e990da")).toBeTruthy();
 
-it("should aggregate resignations for the given type and sub-type", async () => {
-	nock.cleanAll();
-	nock(/.+/)
-		.get("/api/transactions")
-		.query(true)
-		.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
+			nock.cleanAll();
+			nock(/.+/).get("/api/transactions").query(true).reply(200, { meta: {}, data: [] });
 
-	const resignations = await profile.entityAggregate().resignations(EntityType.Business, EntitySubType.None);
+			profile.entityAggregate().flush();
+			const nextPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(nextPage.items()).toHaveLength(0);
+		});
 
-	expect(resignations.items()).toHaveLength(1);
-	expect(resignations.findById("df520b0a278314e998dc93be1e20c72b8313950c19da23967a9db60eb4e990da")).toBeTruthy();
-});
+		it("should fetch once and then stop", async () => {
+			nock.cleanAll();
+			nock(/.+/)
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"))
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"))
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/no-more.json"));
 
-it("#flush", async () => {
-	nock.cleanAll();
-	nock(/.+/)
-		.get("/api/transactions")
-		.query(true)
-		.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
+			const firstPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(firstPage.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeTrue();
 
-	profile.entityAggregate().flush();
-	const resignations = await profile.entityAggregate().resignations(EntityType.Business, EntitySubType.None);
+			const secondPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(secondPage.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeTrue();
 
-	expect(resignations.items()).toHaveLength(1);
-	expect(resignations.findById("df520b0a278314e998dc93be1e20c72b8313950c19da23967a9db60eb4e990da")).toBeTruthy();
-});
+			const thirdPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(thirdPage.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeFalse();
+		});
 
-it("#should fetch all available registrations", async () => {
-	nock.cleanAll();
-	nock(/.+/)
-		.get("/api/transactions")
-		.query(true)
-		.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
+		it("should fetch until there are no more", async () => {
+			nock.cleanAll();
+			nock(/.+/)
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"))
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"))
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/no-more.json"))
+				.get("/api/transactions")
+				.query(true);
 
-	const firstPage = await profile.entityAggregate().registrations(EntityType.Business, EntitySubType.None);
-	expect(firstPage.findById("df520b0a278314e998dc93be1e20c72b8313950c19da23967a9db60eb4e990da")).toBeTruthy();
+			const firstPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(firstPage.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeTrue();
 
-	const secondPage = await profile.entityAggregate().registrations(EntityType.Business, EntitySubType.None);
-	expect(secondPage.items()).toHaveLength(0);
-	expect(profile.entityAggregate().hasMore("0.0.updates")).toBeFalse();
-});
+			const secondPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(secondPage.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeTrue();
 
-it("should filter out empty registration results", async () => {
-	nock.cleanAll();
-	nock(/.+/)
-		.get("/api/transactions")
-		.query(true)
-		.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
+			const thirdPage = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(thirdPage.items()).toHaveLength(1);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeFalse();
 
-	const firstPage = await profile.entityAggregate().registrations(EntityType.Business, EntitySubType.None);
-	expect(firstPage.findById("df520b0a278314e998dc93be1e20c72b8313950c19da23967a9db60eb4e990da")).toBeTruthy();
+			const fourth = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(fourth.items()).toHaveLength(0);
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeFalse();
+		});
 
-	nock.cleanAll();
-	nock(/.+/).get("/api/transactions").query(true).reply(200, { meta: {}, data: [] });
+		it("should flush the history", async () => {
+			nock.cleanAll();
+			nock(/.+/)
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
 
-	profile.entityAggregate().flush();
-	const nextPage = await profile.entityAggregate().registrations(EntityType.Business, EntitySubType.None);
-	expect(nextPage.items()).toHaveLength(0);
+			profile.entityAggregate().flush();
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeFalse();
+			await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+
+			expect(profile.entityAggregate().hasMore(`0.0.${entityActionMap[method]}`)).toBeTrue();
+		});
+
+		it("should handle undefined  promiseAllSettledByKey responses in aggregate", async () => {
+			nock(/.+/)
+				.get("/api/transactions")
+				.query(true)
+				.reply(200, require("../../../test/fixtures/client/registrations/business.json"));
+
+			const promiseAllSettledByKeyMock = jest
+				.spyOn(promiseHelpers, "promiseAllSettledByKey")
+				//@ts-ignore
+				.mockImplementation(() => {
+					return Promise.resolve(undefined);
+				});
+
+			const results = await profile.entityAggregate()[method](EntityType.Business, EntitySubType.None);
+			expect(results).toBeInstanceOf(ExtendedTransactionDataCollection);
+			promiseAllSettledByKeyMock.mockRestore();
+		});
+	});
 });
