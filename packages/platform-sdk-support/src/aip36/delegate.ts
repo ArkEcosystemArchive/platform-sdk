b51de27fbe258f1bdc74d11708b3dcb87fcfc93a
Brian Faust <faustbrian@users.noreply.github.com>
2020-12-09 04:51:02 +0200

    refactor(profiles): drop distribution from AIP36 schema (#855)

--- packages/platform-sdk-support/src/aip36/delegate.ts
+++ packages/platform-sdk-support/src/aip36/delegate.ts
@@ -10,11 +10,6 @@ export class Delegate extends Builder {
 		this.set("delegate.payout.percentage.max", max);
 	}
 
-	public distribution(min: number, max: number): void {
-		this.set("delegate.payout.distribution.min", min);
-		this.set("delegate.payout.distribution.max", max);
-	}
-
 	public frequency(type: string, value: number): void {
 		this.set("delegate.payout.frequency.type", type);
 		this.set("delegate.payout.frequency.value", value);
