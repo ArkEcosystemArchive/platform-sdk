5fc70fe7daac8d48d7950b82d713b7865711a8c6
Brian Faust <hello@basecode.sh>
2020-12-09 06:31:27 +0200

    wip

--- packages/platform-sdk-crypto/src/base64.ts
+++ packages/platform-sdk-crypto/src/base64.ts
@@ -1,9 +1,11 @@
+type BufferEncoding = "utf8" | "ascii" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex" | undefined;
+
 export class Base64 {
-	public static encode(value: string): string {
-		return Buffer.from(value, "utf8").toString("base64");
+	public static encode(value: string, encoding: BufferEncoding = "utf8"): string {
+		return Buffer.from(value, encoding).toString("base64");
 	}
 
-	public static decode(value: string): string {
-		return Buffer.from(value, "base64").toString("utf8");
+	public static decode(value: string, encoding: BufferEncoding = "utf8"): string {
+		return Buffer.from(value, "base64").toString(encoding);
 	}
 }
