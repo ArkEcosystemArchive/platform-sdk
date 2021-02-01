0c7ab868fa3b6e226aa520726dfaa656915050cb
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-02-01 10:48:40 -0300

    100% coverage for client

--- packages/platform-sdk-ada/src/services/client.test.ts
+++ packages/platform-sdk-ada/src/services/client.test.ts
@@ -14,7 +14,7 @@ afterEach(() => nock.cleanAll());
 
 beforeAll(() => nock.disableNetConnect());
 
-describe("ClientService", function () {
+describe("ClientService", function() {
 	describe("#wallet", () => {
 		it("should succeed", async () => {
 			nock("http://localhost:8090")
@@ -26,6 +26,52 @@ describe("ClientService", function () {
 			expect(result).toBeInstanceOf(WalletData);
 			expect(result.address()).toBe("98c83431e94407bc0889e09953461fe5cecfdf18");
 			expect(result.balance().toString()).toEqual("2000000000");
+		});
+	});
+	describe("unimplemented methods", () => {
+		it("#wallets", async () => {
+			await expect(subject.wallets({}))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#transaction", async () => {
+			await expect(subject.transaction(""))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#transactions", async () => {
+			await expect(subject.transactions({}))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#delegate", async () => {
+			await expect(subject.delegate(''))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#delegates", async () => {
+			await expect(subject.delegates({}))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#votes", async () => {
+			await expect(subject.votes(''))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#voters", async () => {
+			await expect(subject.voters('', {}))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#syncing", async () => {
+			await expect(subject.syncing())
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#broadcast", async () => {
+			await expect(subject.broadcast([]))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#broadcastSpread", async () => {
+			await expect(subject.broadcastSpread([], []))
+				.rejects.toThrow(/is not implemented./);
+		});
+		it("#entityHistory", async () => {
+			await expect(subject.entityHistory(''))
+				.rejects.toThrow(/is not implemented./);
 		});
 	});
 });
