5e82164eac5873378d17fb60a067b722ca527a95
marianogoldman <marianogoldman@users.noreply.github.com>
2021-02-01 13:52:27 +0000

    style: resolve style guide violations

--- packages/platform-sdk-ada/src/services/client.test.ts
+++ packages/platform-sdk-ada/src/services/client.test.ts
@@ -14,7 +14,7 @@ afterEach(() => nock.cleanAll());
 
 beforeAll(() => nock.disableNetConnect());
 
-describe("ClientService", function() {
+describe("ClientService", function () {
 	describe("#wallet", () => {
 		it("should succeed", async () => {
 			nock("http://localhost:8090")
@@ -30,48 +30,37 @@ describe("ClientService", function() {
 	});
 	describe("unimplemented methods", () => {
 		it("#wallets", async () => {
-			await expect(subject.wallets({}))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.wallets({})).rejects.toThrow(/is not implemented./);
 		});
 		it("#transaction", async () => {
-			await expect(subject.transaction(""))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.transaction("")).rejects.toThrow(/is not implemented./);
 		});
 		it("#transactions", async () => {
-			await expect(subject.transactions({}))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.transactions({})).rejects.toThrow(/is not implemented./);
 		});
 		it("#delegate", async () => {
-			await expect(subject.delegate(''))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.delegate("")).rejects.toThrow(/is not implemented./);
 		});
 		it("#delegates", async () => {
-			await expect(subject.delegates({}))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.delegates({})).rejects.toThrow(/is not implemented./);
 		});
 		it("#votes", async () => {
-			await expect(subject.votes(''))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.votes("")).rejects.toThrow(/is not implemented./);
 		});
 		it("#voters", async () => {
-			await expect(subject.voters('', {}))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.voters("", {})).rejects.toThrow(/is not implemented./);
 		});
 		it("#syncing", async () => {
-			await expect(subject.syncing())
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.syncing()).rejects.toThrow(/is not implemented./);
 		});
 		it("#broadcast", async () => {
-			await expect(subject.broadcast([]))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.broadcast([])).rejects.toThrow(/is not implemented./);
 		});
 		it("#broadcastSpread", async () => {
-			await expect(subject.broadcastSpread([], []))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.broadcastSpread([], [])).rejects.toThrow(/is not implemented./);
 		});
 		it("#entityHistory", async () => {
-			await expect(subject.entityHistory(''))
-				.rejects.toThrow(/is not implemented./);
+			await expect(subject.entityHistory("")).rejects.toThrow(/is not implemented./);
 		});
 	});
 });
