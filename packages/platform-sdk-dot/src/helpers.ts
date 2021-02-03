99ee9b96f57a07c862d615336fc5856ed5353cc2
Brian Faust <faustbrian@users.noreply.github.com>
2021-02-03 12:22:45 +0200

    refactor(dot): replace `WsProvider` with `HttpProvider` (#963)

---
+++ packages/platform-sdk-dot/src/helpers.ts
@@ -0,0 +1,13 @@
+import { Coins } from "@arkecosystem/platform-sdk";
+import { Arr } from "@arkecosystem/platform-sdk-support";
+import { ApiPromise } from "@polkadot/api";
+import { HttpProvider } from "@polkadot/rpc-provider";
+
+export const createRpcClient = async (config: Coins.Config): Promise<ApiPromise> => {
+	const provider = new HttpProvider(Arr.randomElement(config.get<string[]>("network.networking.hosts")));
+
+	const api = await ApiPromise.create({ provider });
+	await api.isReady;
+
+	return api;
+};
