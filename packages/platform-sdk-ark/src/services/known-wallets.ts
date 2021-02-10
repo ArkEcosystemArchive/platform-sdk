1a02e42f498d62a14a4ecc284f7ff94675d3d2d6
Brian Faust <hello@basecode.sh>
2021-02-10 04:16:08 +0200

    wip

--- packages/platform-sdk-ark/src/services/known-wallets.ts
+++ packages/platform-sdk-ark/src/services/known-wallets.ts
@@ -2,11 +2,11 @@ import { Coins, Contracts } from "@arkecosystem/platform-sdk";
 
 export class KnownWalletService implements Contracts.KnownWalletService {
 	readonly #httpClient: Contracts.HttpClient;
-	readonly #source: string;
+	readonly #source: string | undefined;
 
 	private constructor(config: Coins.Config) {
 		this.#httpClient = config.get<Contracts.HttpClient>(Coins.ConfigKey.HttpClient);
-		this.#source = config.get<string>(Coins.ConfigKey.KnownWallets, "");
+		this.#source = config.getLoose<string>(Coins.ConfigKey.KnownWallets);
 	}
 
 	public static async __construct(config: Coins.Config): Promise<KnownWalletService> {
@@ -16,6 +16,10 @@ export class KnownWalletService implements Contracts.KnownWalletService {
 	public async __destruct(): Promise<void> {}
 
 	public async all(): Promise<Contracts.KnownWallet[]> {
+		if (this.#source === undefined) {
+			return [];
+		}
+
 		try {
 			const results = (await this.#httpClient.get(this.#source)).json();
 
