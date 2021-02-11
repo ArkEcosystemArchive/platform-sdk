02ba43abf98f25fb2c254ab03f0a3f5fc70100f0
Brian Faust <hello@basecode.sh>
2021-02-11 07:53:07 +0200

    wip

--- packages/platform-sdk-ada/src/services/client.test.ts
+++ packages/platform-sdk-ada/src/services/client.test.ts
@@ -33,7 +33,7 @@ describe("ClientService", function () {
 			.reply(200, require(`${__dirname}/../../test/fixtures/client/transactions.json`));
 
 		const result = await subject.transactions({
-			address: "98c83431e94407bc0889e09953461fe5cecfdf18",
+			walletId: "98c83431e94407bc0889e09953461fe5cecfdf18",
 		});
 
 		expect(result).toBeObject();
@@ -48,7 +48,9 @@ describe("ClientService", function () {
 			)
 			.reply(200, require(`${__dirname}/../../test/fixtures/client/transaction.json`));
 
-		const result = await subject.transaction("35b40547f04963d3b41478fc27038948d74718802c486d9125f1884d8c83a31d");
+		const result = await subject.transaction("35b40547f04963d3b41478fc27038948d74718802c486d9125f1884d8c83a31d", {
+			walletId: "98c83431e94407bc0889e09953461fe5cecfdf18",
+		});
 		expect(result).toBeInstanceOf(TransactionData);
 	});
 
@@ -56,24 +58,31 @@ describe("ClientService", function () {
 		it("#wallets", async () => {
 			await expect(subject.wallets({})).rejects.toThrow(/is not implemented./);
 		});
+
 		it("#delegate", async () => {
 			await expect(subject.delegate("")).rejects.toThrow(/is not implemented./);
 		});
+
 		it("#delegates", async () => {
 			await expect(subject.delegates({})).rejects.toThrow(/is not implemented./);
 		});
+
 		it("#votes", async () => {
 			await expect(subject.votes("")).rejects.toThrow(/is not implemented./);
 		});
+
 		it("#voters", async () => {
 			await expect(subject.voters("", {})).rejects.toThrow(/is not implemented./);
 		});
+
 		it("#syncing", async () => {
 			await expect(subject.syncing()).rejects.toThrow(/is not implemented./);
 		});
+
 		it("#broadcast", async () => {
 			await expect(subject.broadcast([])).rejects.toThrow(/is not implemented./);
 		});
+
 		it("#broadcastSpread", async () => {
 			await expect(subject.broadcastSpread([], [])).rejects.toThrow(/is not implemented./);
 		});
