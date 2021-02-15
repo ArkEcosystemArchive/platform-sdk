a4b86fa5f7921e8114c5138b84c81cac07fca93d
Brian Faust <faustbrian@users.noreply.github.com>
2021-02-15 17:26:03 +0200

    refactor(avax): drop `avalanchejs` for message signing (#1026)

--- packages/platform-sdk-avax/src/services/message.ts
+++ packages/platform-sdk-avax/src/services/message.ts
@@ -1,7 +1,10 @@
 import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
-import { Buffer } from "avalanche";
+import { BinTools, Buffer } from "avalanche";
+import { KeyPair } from "avalanche/dist/apis/avm";
+import { getPreferredHRP } from "avalanche/dist/utils";
+import { createHash } from "crypto";
 
-import { keyPairFromMnemonic } from "./helpers";
+import { cb58Decode, cb58Encode, keyPairFromMnemonic } from "./helpers";
 
 export class MessageService implements Contracts.MessageService {
 	readonly #config: Coins.Config;
@@ -24,8 +27,8 @@ export class MessageService implements Contracts.MessageService {
 
 			return {
 				message: input.message,
-				signatory: keypair.getPublicKeyString(),
-				signature: keypair.sign(Buffer.from(input.message)).toString("hex"),
+				signatory: keypair.getAddressString(),
+				signature: cb58Encode(keypair.sign(this.digestMessage(input.message))),
 			};
 		} catch (error) {
 			throw new Exceptions.CryptoException(error);
@@ -33,13 +36,22 @@ export class MessageService implements Contracts.MessageService {
 	}
 
 	public async verify(input: Contracts.SignedMessage): Promise<boolean> {
-		if (input.mnemonic === undefined) {
-			throw new Exceptions.MissingArgument(this.constructor.name, "verify", "input.mnemonic");
-		}
+		const bintools = BinTools.getInstance();
 
-		return keyPairFromMnemonic(this.#config, input.mnemonic).verify(
-			Buffer.from(input.message),
-			Buffer.from(input.signature, "hex"),
-		);
+		const hrp = getPreferredHRP(parseInt(this.#config.get("network.crypto.networkId")));
+		const keypair = new KeyPair(hrp, "X");
+		const signedBuff = cb58Decode(input.signature);
+		const pubKey = keypair.recover(this.digestMessage(input.message), signedBuff);
+
+		return bintools.addressToString(hrp, "X", keypair.addressFromPublicKey(pubKey)) === input.signatory;
+	}
+
+	private digestMessage(msgStr: string): Buffer {
+		const mBuf = Buffer.from(msgStr, "utf8");
+		const msgSize = Buffer.alloc(4);
+		msgSize.writeUInt32BE(mBuf.length, 0);
+		const msgBuf = Buffer.from(`\x1AAvalanche Signed Message:\n${msgSize}${msgStr}`, "utf8");
+
+		return Buffer.from(createHash("sha256").update(msgBuf).digest());
 	}
 }
