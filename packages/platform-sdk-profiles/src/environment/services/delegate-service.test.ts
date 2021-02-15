66de6c615f71714d260ee2ac1bcac5f9ad9d5d6a
faustbrian <faustbrian@users.noreply.github.com>
2021-02-15 12:13:39 +0000

    style: resolve style guide violations

--- packages/platform-sdk-profiles/src/environment/services/delegate-service.test.ts
+++ packages/platform-sdk-profiles/src/environment/services/delegate-service.test.ts
@@ -54,11 +54,7 @@ describe("DelegateService", () => {
 	it("#findByPublicKey", async () => {
 		await subject.syncAll();
 		expect(
-			subject.findById(
-				"ARK",
-				"ark.devnet",
-				"033a5474f68f92f254691e93c06a2f22efaf7d66b543a53efcece021819653a200",
-			),
+			subject.findById("ARK", "ark.devnet", "033a5474f68f92f254691e93c06a2f22efaf7d66b543a53efcece021819653a200"),
 		).toBeTruthy();
 		expect(() => subject.findById("ARK", "ark.devnet", "unknown")).toThrowError(/No delegate for/);
 	});
