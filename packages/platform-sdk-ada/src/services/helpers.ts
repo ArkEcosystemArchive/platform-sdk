1b990ee29ac162ec4d7df0465d66d41ba9275fd3
Brian Faust <hello@basecode.sh>
2021-02-16 13:11:39 +0200

    wip

--- packages/platform-sdk-ada/src/services/helpers.ts
+++ packages/platform-sdk-ada/src/services/helpers.ts
@@ -1,7 +1,7 @@
 import { Coins, Contracts } from "@arkecosystem/platform-sdk";
 import { Arr } from "@arkecosystem/platform-sdk-support";
 
-export const postGraphql = (config: Coins.Config, query: object): Promise<Record<string, any>> => (
+export const postGraphql = (config: Coins.Config, query: string): Promise<Record<string, any>> => (
 	await config
 		.get<Contracts.HttpClient>("httpClient")
 		.post(Arr.randomElement(config.get<string[]>("network.networking.hostsArchival")), { query })
