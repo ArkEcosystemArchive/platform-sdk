3b4e4c74f760da4d5d069937132cf5fbc966d972
faustbrian <faustbrian@users.noreply.github.com>
2021-02-17 04:13:14 +0000

    style: resolve style guide violations

--- packages/platform-sdk-ada/src/services/transaction.helpers.ts
+++ packages/platform-sdk-ada/src/services/transaction.helpers.ts
@@ -6,20 +6,20 @@ const harden = (num: number): number => 0x80000000 + num;
 export const createValue = (value: string): Value => Value.new(BigNum.from_str(value));
 
 export const deriveRootKey = (mnemonic: string): Bip32PrivateKey =>
-    Bip32PrivateKey.from_bip39_entropy(Buffer.from(BIP39.toEntropy(mnemonic), "hex"), Buffer.from(""));
+	Bip32PrivateKey.from_bip39_entropy(Buffer.from(BIP39.toEntropy(mnemonic), "hex"), Buffer.from(""));
 
 export const deriveAccountKey = (rootKey: Bip32PrivateKey, slip44: number, index: number): Bip32PrivateKey =>
-    rootKey
-        .derive(harden(1852)) // CIP1852
-        .derive(harden(slip44))
-        .derive(harden(index));
+	rootKey
+		.derive(harden(1852)) // CIP1852
+		.derive(harden(slip44))
+		.derive(harden(index));
 
 export const deriveUtxoKey = (accountKey: Bip32PrivateKey): Bip32PrivateKey =>
-    accountKey
-        .derive(harden(0)) // External
-        .derive(harden(0));
+	accountKey
+		.derive(harden(0)) // External
+		.derive(harden(0));
 
 export const deriveStakeKey = (accountKey: Bip32PrivateKey): Bip32PrivateKey =>
-    accountKey
-        .derive(harden(2)) // Chimeric
-        .derive(harden(0));
+	accountKey
+		.derive(harden(2)) // Chimeric
+		.derive(harden(0));
