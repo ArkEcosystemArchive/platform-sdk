706c90762bc6c6cf7d2ec056faa8b56423ad1068
Brian Faust <faustbrian@users.noreply.github.com>
2021-02-24 05:40:07 +0200

    fix(egld): expect `egld.mainnet` or `egld.testnet` as networks (#1075)

--- packages/platform-sdk-egld/src/schema.ts
+++ packages/platform-sdk-egld/src/schema.ts
@@ -1,7 +1,7 @@
 import { ValidatorSchema } from "@arkecosystem/platform-sdk-support";
 
 export const schema: any = ValidatorSchema.object({
-	network: ValidatorSchema.string().valid("ada.mainnet", "ada.testnet"),
+	network: ValidatorSchema.string().valid("egld.mainnet", "egld.testnet"),
 	peer: ValidatorSchema.string().uri(),
 	peerMultiSignature: ValidatorSchema.string().uri(),
 	httpClient: ValidatorSchema.object(),
