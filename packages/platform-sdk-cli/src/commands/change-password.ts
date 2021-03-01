98a7221f73a1ce3445505121c5303385a30d3af8
Brian Faust <hello@basecode.sh>
2021-03-01 11:50:59 +0200

    wip

--- packages/platform-sdk-cli/src/commands/change-password.ts
+++ packages/platform-sdk-cli/src/commands/change-password.ts
@@ -4,33 +4,52 @@ import prompts from "prompts";
 
 import { renderLogo } from "../helpers";
 
+export const validatePassword = async (value: string): Promise<boolean | string> => {
+	if (await pwnd(value)) {
+		return "Please change your password, it has been found in a previous breach";
+	}
+
+	if (await strong(value)) {
+		return true;
+	}
+
+	return "The password must contain at least 1 lowercase character, 1 uppercase character, 1 numeric character, 1 special character and must be 8 characters or longer.";
+};
+
 export const changePassword = async (profile: Profile): Promise<void> => {
 	renderLogo();
 
-	const { password } = await prompts({
+	const { oldPassword, newPassword } = await prompts([{
 		type: "password",
 		name: "password",
-		message: "Please enter your password:",
+		message: "Please enter your current password:",
 		validate: async (value: string) => {
 			if (value === undefined) {
 				return false;
 			}
 
-			if (await pwnd(value)) {
-				return "Please change your password, it has been found in a previous breach";
-			}
-
-			if (await strong(value)) {
-				return true;
-			}
-
-			return "The password must contain at least 1 lowercase character, 1 uppercase character, 1 numeric character, 1 special character and must be 8 characters or longer.";
+			return validatePassword(value);
 		},
-	});
+	}, {
+		type: "password",
+		name: "password",
+		message: "Please enter your new password:",
+		validate: async (value: string) => {
+			if (value === undefined) {
+				return false;
+			}
 
-	if (password === undefined) {
+			return validatePassword(value);
+		},
+	}]);
+
+	if (oldPassword === undefined) {
 		return;
 	}
 
-	profile.auth().setPassword(password);
+	if (newPassword === undefined) {
+		return;
+	}
+
+	profile.auth().changePassword(oldPassword, newPassword);
 };
