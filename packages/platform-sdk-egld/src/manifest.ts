63dd35c2408d136340189af09e8bfa7f131fa8ba
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-03-02 08:31:55 -0300

    cr updates

--- packages/platform-sdk-egld/src/manifest.ts
+++ packages/platform-sdk-egld/src/manifest.ts
@@ -2,7 +2,7 @@ import ElrondMainnet from "./networks/egld/mainnet";
 import ElrondTestnet from "./networks/egld/testnet";
 
 export const manifest = {
-	name: "EGLD",
+	name: "Elrond",
 	networks: {
 		"egld.mainnet": ElrondMainnet,
 		"egld.testnet": ElrondTestnet,
