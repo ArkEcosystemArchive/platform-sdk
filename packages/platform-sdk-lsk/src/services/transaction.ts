b89d5722daee79fd6742b296f65e274e30c67d34
faustbrian <faustbrian@users.noreply.github.com>
2021-03-03 05:58:08 +0000

    style: resolve style guide violations

--- packages/platform-sdk-lsk/src/services/transaction.ts
+++ packages/platform-sdk-lsk/src/services/transaction.ts
@@ -1,6 +1,12 @@
 import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
 import { BIP39 } from "@arkecosystem/platform-sdk-crypto";
-import { transfer, registerSecondPassphrase, registerDelegate, castVotes, registerMultisignature } from "@liskhq/lisk-transactions";
+import {
+	castVotes,
+	registerDelegate,
+	registerMultisignature,
+	registerSecondPassphrase,
+	transfer,
+} from "@liskhq/lisk-transactions";
 
 import { SignedTransactionData } from "../dto/signed-transaction";
 
@@ -162,7 +168,7 @@ export class TransactionService implements Contracts.TransactionService {
 				registerSecondPassphrase,
 				registerDelegate,
 				castVotes,
-				registerMultisignature
+				registerMultisignature,
 			}[type](struct);
 
 			return new SignedTransactionData(signedTransaction.id, signedTransaction, signedTransaction);
