9c5ce9503dcf2f12f5196e185341bf84c56a6f58
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-03 05:59:43 +0200

    chore: enable pnpm workspaces (#1140)

--- packages/platform-sdk-support/src/qrcode.test.ts
+++ packages/platform-sdk-support/src/qrcode.test.ts
@@ -30,6 +30,7 @@ describe.each(["utf8", "svg", "terminal"])("%s", (type) => {
 	});
 
 	it("should turn into a string", async () => {
+		// @ts-ignore
 		await expect(QRCode.fromString("https://google.com").toString(type)).resolves.toMatchSnapshot();
 	});
 });
---
+++ scripts/version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
+    cd $dir
+    echo $PWD
+    npm version $1
+    cd ../..
+done
+
+rush update
