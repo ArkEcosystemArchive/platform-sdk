3bbb1fb82dfe7ba78bddb38c1f7c427075c0d055
Brian Faust <hello@basecode.sh>
2021-03-04 05:20:06 +0200

    wip

--- packages/platform-sdk-ada/src/services/identity/address-list.test.ts
+++ packages/platform-sdk-ada/src/services/identity/address-list.test.ts
@@ -1,10 +1,11 @@
 import { hasUncaughtExceptionCaptureCallback } from "process";
 import { AddressList } from "./address-list";
+import { identity } from "../../../test/fixtures/identity";
 
 describe("AddressList", function () {
 	it("should generate 20 addresses", async () => {
 		const subject = new AddressList();
 
-		expect(subject.make("secret", 20)).toHaveLength(20);
+		expect(subject.make(identity.mnemonic, 20)).toHaveLength(20);
 	});
 });
