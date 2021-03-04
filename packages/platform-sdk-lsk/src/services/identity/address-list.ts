96080ccd1985ddd34719a2092944f810ba7eab59
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-04 07:25:23 +0200

    refactor(sdk): make `AddressList#fromMnemonic` asynchronous (#1155)

--- packages/platform-sdk-lsk/src/services/identity/address-list.ts
+++ packages/platform-sdk-lsk/src/services/identity/address-list.ts
@@ -1,7 +1,7 @@
 import { Contracts } from "@arkecosystem/platform-sdk";
 
 export class AddressList implements Contracts.AddressList {
-	public fromMnemonic(mnemonic: string, pageSize: number): Contracts.AddressListEntry[] {
+	public async fromMnemonic(mnemonic: string, pageSize: number): Promise<Contracts.AddressListEntry[]> {
 		return [];
 	}
 }
