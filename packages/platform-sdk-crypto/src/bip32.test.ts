87306d634f6255af618088607080abbc9ae87bb6
Brian Faust <hello@basecode.sh>
2021-03-05 07:06:48 +0200

    wip

--- packages/platform-sdk-crypto/src/bip32.test.ts
+++ packages/platform-sdk-crypto/src/bip32.test.ts
@@ -5,5 +5,9 @@ import { BIP32 } from "./bip32";
 const mnemonic = "TestingOneTwoThree";
 
 test("#fromMnemonic", async () => {
-	expect(BIP32.fromMnemonic("praise you muffin lion enable neck grocery crumble super myself license ghost").toBase58()).toBe("xprv9s21ZrQH143K4DRBUU8Dp25M61mtjm9T3LsdLLFCXL2U6AiKEqs7dtCJWGFcDJ9DtHpdwwmoqLgzPrW7unpwUyL49FZvut9xUzpNB6wbEnz");
+	expect(
+		BIP32.fromMnemonic("praise you muffin lion enable neck grocery crumble super myself license ghost").toBase58(),
+	).toBe(
+		"xprv9s21ZrQH143K4DRBUU8Dp25M61mtjm9T3LsdLLFCXL2U6AiKEqs7dtCJWGFcDJ9DtHpdwwmoqLgzPrW7unpwUyL49FZvut9xUzpNB6wbEnz",
+	);
 });
