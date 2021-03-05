e480bfc4530c7d3d8a0197d1c98d773994b53d24
Brian Faust <hello@basecode.sh>
2021-03-05 07:03:53 +0200

    wip

---
+++ packages/platform-sdk-crypto/src/bip32.test.ts
@@ -0,0 +1,9 @@
+import "jest-extended";
+
+import { BIP32 } from "./bip32";
+
+const mnemonic = "TestingOneTwoThree";
+
+test("#fromMnemonic", async () => {
+	expect(BIP32.fromMnemonic("praise you muffin lion enable neck grocery crumble super myself license ghost").toBase58()).toBe("xprv9s21ZrQH143K4DRBUU8Dp25M61mtjm9T3LsdLLFCXL2U6AiKEqs7dtCJWGFcDJ9DtHpdwwmoqLgzPrW7unpwUyL49FZvut9xUzpNB6wbEnz");
+});
