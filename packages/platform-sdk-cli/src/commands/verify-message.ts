8f0dd6542c20a055c52686f0890ffa8537c9ba15
Brian Faust <hello@basecode.sh>
2021-03-05 12:20:45 +0200

    wip

---
+++ packages/platform-sdk-cli/src/commands/verify-message.ts
@@ -0,0 +1,25 @@
+import { ReadWriteWallet } from "@arkecosystem/platform-sdk-profiles";
+import prompts from "prompts";
+
+import { renderLogo, useLogger } from "../helpers";
+
+export const verifyMessage = async (wallet: ReadWriteWallet): Promise<void> => {
+	renderLogo();
+
+	const { message } = await prompts({
+		type: "text",
+		name: "message",
+		message: "Please enter the JSON:",
+		validate: (value: string) => JSON.parse(value),
+	});
+
+	if (!message) {
+		return;
+	}
+
+	if (await wallet.message().verify(JSON.parse(message))) {
+		useLogger().error("The transaction has been verified.")
+	} else {
+		useLogger().info("The transaction failed to verify.");
+	}
+};
