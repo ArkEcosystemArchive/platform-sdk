f7a4aef34fcef57209a6fbbf30455900be360623
Brian Faust <hello@basecode.sh>
2021-03-08 04:33:45 +0200

    wip

--- packages/platform-sdk/src/contracts/coins/ledger.ts
+++ packages/platform-sdk/src/contracts/coins/ledger.ts
@@ -14,6 +14,8 @@ export interface LedgerService {
 
 	getVersion(): Promise<string>;
 
+	getPublicKey(path: string): Promise<string>;
+
 	getExtendedPublicKey(path: string): Promise<string>;
 
 	signTransaction(path: string, payload: Buffer): Promise<string>;
