356058dad4b6c4022a60b80e3e85b3bdcf58f7a9
Brian Faust <hello@basecode.sh>
2021-03-08 07:52:38 +0200

    wip

--- packages/platform-sdk-crypto/src/hdkey.ts
+++ packages/platform-sdk-crypto/src/hdkey.ts
@@ -2,7 +2,7 @@ import Base, { fromExtendedKey, fromMasterSeed } from "hdkey";
 
 export class HDKey {
 	public static fromSeed(seed: string): Base {
-		return fromMasterSeed(seed);
+		return fromMasterSeed(Buffer.from(seed, 'hex'));
 	}
 
 	public static fromExtendedPublicKey(key: string): Base {
