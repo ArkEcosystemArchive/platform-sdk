a9a3830bee400026d3dd9f8363e96eb37391ca7f
Brian Faust <hello@basecode.sh>
2021-03-08 16:40:40 +0200

    wip

--- packages/platform-sdk-cli/src/commands/import-ledger-wallet.ts
+++ packages/platform-sdk-cli/src/commands/import-ledger-wallet.ts
@@ -3,6 +3,7 @@ import { Environment, Profile } from "@arkecosystem/platform-sdk-profiles";
 import LedgerTransportNodeHID from "@ledgerhq/hw-transport-node-hid-singleton";
 import ora from "ora";
 import prompts from "prompts";
+import createXpub from "create-xpub";
 
 import { renderLogo, useLogger } from "../helpers";
 
@@ -82,6 +83,15 @@ export const importLedgerWallet = async (env: Environment, profile: Profile): Pr
 					const extendedKey = HDKey.fromSeed(extendedPublicKey).derive(`m/${path}`).publicKey.toString("hex");
 					const extendedAddress = await instance.identity().address().fromPublicKey(extendedKey);
 					const expected = confirmations[path];
+
+					console.log(
+						createXpub({
+							depth: i,
+							childNumber: 2147483648,
+							chainCode: '84cf7d9029cdd9fcadbb3717fd92ec0db7d7d9787c57c13c08fc887c389b566b',
+							publicKey: '048bcdcf59f046b13f1eb35b608d1211265fde8cc44fc7a5a7f7107c5cf238095328a0e0d7be17c7d3e48490e8c6433af6d2c3dacc687f3fecaa98a3d05f17de97'
+						}),
+					)
 
 					console.log({ path, expected, ledgerKey, ledgerAddress, extendedKey, extendedAddress });
 				}
