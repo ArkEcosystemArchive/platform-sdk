59c479d5e3a7677c40621d2b4e2ecef82fa70026
Brian Faust <hello@basecode.sh>
2021-03-08 09:24:08 +0200

    wip

--- packages/platform-sdk-cli/src/commands/import-ledger-wallet.ts
+++ packages/platform-sdk-cli/src/commands/import-ledger-wallet.ts
@@ -58,18 +58,35 @@ export const importLedgerWallet = async (env: Environment, profile: Profile): Pr
 
 				useLogger().info(`Extended Public Key: ${extendedPublicKey}`);
 
+				const confirmations = {
+					"44'/111'/0'/0/0": "AYnmdEV3YUzcHWaANJxex8YiDwHG39oqCd",
+					"44'/111'/1'/0/0": "AYN58vVzfhuwz1KcWF5c7RgSmQydMj7AkW",
+					"44'/111'/2'/0/0": "ARiCq378UANqsYWdXHrdZPiFmD2sGATtJb",
+					"44'/111'/3'/0/0": "AUdAbG3NmjKJ3pmz55SDex8mTbhBmU4PST",
+					"44'/111'/4'/0/0": "AWZbSenUzVZ3FtbdvCcd1j56x2Hvht5EZU",
+					"44'/111'/0'/0/1": "AaCxeiExbhRbQaKhQGMYMfqjRawsA9Qzza",
+					"44'/111'/0'/0/2": "AeBoGCKrbfVghNi1TRvQdmdUYrbZ8KEUsn",
+					"44'/111'/0'/0/3": "AUiZ9oBi2NsbZC51zjNzSafbVynZGFVueT",
+					"44'/111'/0'/0/4": "AMF8hHxCnxzkGNzzh9bpnfgBk2DNgKBPUL",
+					"44'/111'/0'/0/5": "APpCDz9rFsZUf4XKTHApmUUhwvTrwfhjoC",
+					"44'/111'/0'/0/6": "ARyWiQ4L5w9cFCb5T9vUsSZsUkhHpbR8gK",
+					"44'/111'/0'/0/7": "AU81qEcvSadKpBq6auKNfDL1neaNqzaTbz",
+					"44'/111'/0'/0/8": "ANzRinSabrDtXb9H6eyTa2GUG6nMgEwJZ9",
+					"44'/111'/0'/0/9": "AHWB5KGPs4i69Rkn1GXA9S7pgTifvz41AT",
+				};
+
 				for (let i = 0; i < 10; i++) {
-					console.log(
-						await instance
-							.identity()
-							.address()
-							.fromPublicKey(
-								HDKey.fromSeed(extendedPublicKey)
-									.derive(`m/44'/111'/0'/0/${i}`)
-									.publicKey.toString("hex"),
-							),
-					);
+					const path = `44'/111'/0'/0/${i}`;
+					const ledgerKey = await instance.ledger().getPublicKey(`m/${path}`);
+					const ledgerAddress = await instance.identity().address().fromPublicKey(ledgerKey);
+					const extendedKey = HDKey.fromSeed(extendedPublicKey).derive(`m/${path}`).publicKey.toString("hex");
+					const extendedAddress = await instance.identity().address().fromPublicKey(extendedKey);
+					const expected = confirmations[path];
+
+					console.log({ path, expected, ledgerKey, ledgerAddress, extendedKey, extendedAddress });
 				}
+
+				process.exit();
 			}
 
 			if (type === "remove") {
