7c9ed1431caa6c07c70a46d54740560e01926f06
Brian Faust <hello@basecode.sh>
2021-03-09 07:08:22 +0200

    wip

--- packages/platform-sdk-crypto/src/bip44.ts
+++ packages/platform-sdk-crypto/src/bip44.ts
@@ -1,4 +1,4 @@
-import * as bip32 from "bip32";
+import { BIP32Interface } from "bip32";
 
 import { BIP32 } from "./bip32";
 
@@ -6,7 +6,7 @@ export class BIP44 {
 	public static deriveChild(
 		mnemonic: string,
 		options: { purpose?: number; coinType: number; account?: number; change?: number; index?: number },
-	): bip32.BIP32Interface {
+	): BIP32Interface {
 		return BIP32.fromMnemonic(mnemonic)
 			.deriveHardened(options.purpose || 44)
 			.deriveHardened(options.coinType)
@@ -15,11 +15,11 @@ export class BIP44 {
 			.derive(options.index || 0);
 	}
 
-	public static deriveChildFromPath(mnemonic: string, path: string, index?: number): bip32.BIP32Interface {
+	public static deriveChildFromPath(mnemonic: string, path: string, index?: number): BIP32Interface {
 		return BIP32.fromMnemonic(mnemonic).derivePath(`${path}${index || 0}`);
 	}
 
-	public static deriveMasterKey(mnemonic: string): bip32.BIP32Interface {
+	public static deriveMasterKey(mnemonic: string): BIP32Interface {
 		return BIP32.fromMnemonic(mnemonic);
 	}
 
@@ -32,7 +32,7 @@ export class BIP44 {
 		change: number;
 		addressIndex: number;
 	} {
-		if (!path.match(new RegExp("^((m/)?(44'?)){1}(/[0-9]+'?){2}(/[0-9]+){2}$", "g"))) {
+		if (!path.match(new RegExp("^((m/)?(44'?))(/[0-9]+'?){2}((/[0-9]+){2})?$", "g"))) {
 			throw new Error(path);
 		}
 
