9af92ebe166b2a020665e07439133c0d19f82233
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-09 07:04:12 +0200

    feat(crypto): implement `HDKey` helper (#1175)

---
+++ packages/platform-sdk-crypto/src/hdkey.ts
@@ -0,0 +1,25 @@
+import Base, { fromExtendedKey, fromMasterSeed } from "hdkey";
+
+const normalise = (value: string | Buffer) => (value instanceof Buffer ? value : Buffer.from(value, "hex"));
+
+export class HDKey {
+	public static fromSeed(seed: string | Buffer): Base {
+		return fromMasterSeed(normalise(seed));
+	}
+
+	public static fromExtendedPublicKey(key: string): Base {
+		if (!key.startsWith("xpub")) {
+			throw new Error("The given key is not an extended public key.");
+		}
+
+		return fromExtendedKey(key);
+	}
+
+	public static fromExtendedPrivateKey(key: string): Base {
+		if (!key.startsWith("xprv")) {
+			throw new Error("The given key is not an extended private key.");
+		}
+
+		return fromExtendedKey(key);
+	}
+}
