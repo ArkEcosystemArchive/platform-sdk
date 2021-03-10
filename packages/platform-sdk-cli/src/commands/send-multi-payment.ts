7c471aa76386edaab4421ebf26331ae43dfb925e
Brian Faust <hello@basecode.sh>
2021-03-10 05:09:14 +0200

    wip

--- packages/platform-sdk-cli/src/commands/send-multi-payment.ts
+++ packages/platform-sdk-cli/src/commands/send-multi-payment.ts
@@ -9,18 +9,49 @@ import { finaliseTransaction } from "./helpers";
 export const sendMultiPayment = async (wallet: ReadWriteWallet): Promise<void> => {
 	renderLogo();
 
-	// memo?: string;
-	// payments: {
-	// 	to: string;
-	// 	amount: string;
-	// }[];
+	const payments: { amount: string; to: string; }[] = [];
 
-	const { mnemonic, amount, to, memo } = await prompts([
+	let addMore = true;
+	while (addMore) {
+		const { amount, to, more } = await prompts([
+			{
+				type: "text",
+				name: "amount",
+				message: "Please enter the amount:",
+				validate: (value: string) => BigNumber.make(value).isGreaterThan(0),
+			},
+			{
+				type: "text",
+				name: "to",
+				message: "Please enter the recipient:",
+				validate: (value: string) => value !== undefined,
+			},
+			{
+				type: "toggle",
+				name: "more",
+				message: "Do you want to add another recipient?",
+				initial: true,
+				active: "yes",
+				inactive: "no",
+			},
+		]);
+
+		payments.push({ amount, to });
+
+		addMore = more;
+	}
+
+	const { mnemonic, memo } = await prompts([
 		{
 			type: "text",
 			name: "to",
 			message: "Please enter the recipient:",
 			validate: (value: string) => value !== undefined,
+		},
+		{
+			type: "text",
+			name: "memo",
+			message: "Please enter the memo:",
 		},
 		{
 			type: "password",
@@ -30,21 +61,8 @@ export const sendMultiPayment = async (wallet: ReadWriteWallet): Promise<void> =
 		},
 	]);
 
-	if (!to) {
-		return;
-	}
-
-	if (!amount) {
-		return;
-	}
-
-	if (!mnemonic) {
-		return;
-	}
-
 	await finaliseTransaction(wallet, mnemonic, "signMultiPayment", {
-		amount: BigNumber.make(amount).toSatoshi().toString(),
-		to,
+		payments,
 		memo,
 	});
 };
