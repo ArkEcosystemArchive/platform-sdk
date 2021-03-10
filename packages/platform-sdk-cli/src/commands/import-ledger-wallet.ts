7c5f1f03c0694b4e3c8770dad2b21ce1fa3fcbb8
Brian Faust <hello@basecode.sh>
2021-03-10 04:39:16 +0200

    wip

--- packages/platform-sdk-cli/src/commands/import-ledger-wallet.ts
+++ packages/platform-sdk-cli/src/commands/import-ledger-wallet.ts
@@ -10,6 +10,7 @@ import { renderLogo, useLogger } from "../helpers";
 const chunk = <T>(value: T[], size: number) =>
 	Array.from({ length: Math.ceil(value.length / size) }, (v, i) => value.slice(i * size, i * size + size));
 
+// @TODO: this currently only supports 5 accounts with 50 wallets. use the DW scanner once implemented
 export const importLedgerWallet = async (env: Environment, profile: Profile): Promise<void> => {
 	renderLogo();
 
@@ -55,58 +56,30 @@ export const importLedgerWallet = async (env: Environment, profile: Profile): Pr
 		// @ts-ignore
 		next: async ({ type, deviceModel }) => {
 			if (type === "add") {
-				console.time("ledger");
-
 				useLogger().info(
 					`Connected [${deviceModel.productName}] with version [${await instance.ledger().getVersion()}]`,
 				);
 
+                // Derive
 				const addressMap = {};
 
-				for (let accountIndex = 0; accountIndex < 50; accountIndex++) {
+				for (let accountIndex = 0; accountIndex < 5; accountIndex++) {
 					const compressedPublicKey = await instance
 						.ledger()
 						.getExtendedPublicKey(`m/44'/${slip44}'/${accountIndex}'`);
 
 					useLogger().info(`Extended Public Key for account [${accountIndex}] >>> ${compressedPublicKey}`);
 
-					for (let addressIndex = 0; addressIndex < 100; addressIndex++) {
+					for (let addressIndex = 0; addressIndex < 50; addressIndex++) {
 						const path = `44'/${slip44}'/${accountIndex}'/0/${addressIndex}`;
-						// const ledgerKey = await instance.ledger().getPublicKey(path);
-						// const ledgerAddress = await instance.identity().address().fromPublicKey(ledgerKey);
-						const extendedKey = HDKey.fromCompressedPublicKey(compressedPublicKey)
-							.derive(`m/0/${addressIndex}`)
-							.publicKey.toString("hex");
+						const extendedKey = HDKey.fromCompressedPublicKey(compressedPublicKey).derive(`m/0/${addressIndex}`).publicKey.toString("hex");
 						const extendedAddress = await instance.identity().address().fromPublicKey(extendedKey);
 
 						addressMap[extendedAddress] = { path, extendedKey };
 					}
 				}
 
-				// const mnemonic = "";
-				// const source = await profile.wallets().importByMnemonic(mnemonic, coin, network);
-
-				// for (const addresses of chunk(Object.keys(addressMap), 128)) {
-				// 	try {
-				// 		await source.transaction().broadcast(
-				// 			await source.transaction().signMultiPayment({
-				// 				from: source.address(),
-				// 				sign: { mnemonic },
-				// 				data: {
-				// 					payments: addresses.map((address: string) => ({
-				// 						amount: `${1e8}`,
-				// 						to: address,
-				// 					}))
-				// 				}
-				// 			})
-				// 		);
-				// 	} catch {
-				// 		//
-				// 	}
-
-				// 	await delay(10000);
-				// }
-
+                // Check
 				const networkSpinner = ora("Checking addresses on network...").start();
 
 				const table = new Table({ head: ["Path", "Address", "Public Key", "Balance"] });
@@ -117,22 +90,44 @@ export const importLedgerWallet = async (env: Environment, profile: Profile): Pr
 					),
 				);
 
+                const wallets: any[] = [];
 				for (const chunk of chunks) {
 					for (const identity of chunk.items()) {
-						table.push([
+                        table.push([
 							addressMap[identity.address()].path,
 							identity.address(),
 							addressMap[identity.address()].extendedKey,
 							identity.balance().toHuman(),
 						]);
+
+						wallets.push({
+                            path: addressMap[identity.address()].path,
+							address: identity.address(),
+							extendedKey: addressMap[identity.address()].extendedKey,
+							balance: identity.balance().toHuman(),
+                        });
 					}
 				}
 
 				networkSpinner.stop();
 
 				console.log(table.toString());
-				console.timeEnd("ledger");
 
+                const { addresses } = await prompts([{
+                    type: 'multiselect',
+                    name: 'addresses',
+                    message: 'What addresses do you want to import?',
+                    choices: wallets.map(wallet => ({ title: `${wallet.address} [${wallet.balance}]`, value: wallet })),
+                }]);
+
+                const imports: any[] = [];
+                for (const address of addresses) {
+                    imports.push(profile.wallets().importByAddressWithLedgerPath(address.address, coin, network, address.path));
+                }
+
+                await Promise.all(imports);
+
+                // Import
 				process.exit();
 			}
 
