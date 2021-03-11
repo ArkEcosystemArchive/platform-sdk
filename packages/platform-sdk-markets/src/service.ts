8061f56980bb65d43e1a7cb3f5d99b03ae8bcafd
Brian Faust <hello@basecode.sh>
2021-03-11 08:58:28 +0200

    wip

--- packages/platform-sdk-markets/src/service.ts
+++ packages/platform-sdk-markets/src/service.ts
@@ -3,13 +3,39 @@ import { PriceTracker as CoinCap } from "@arkecosystem/platform-sdk-coincap";
 import { PriceTracker as CoinGecko } from "@arkecosystem/platform-sdk-coingecko";
 import { PriceTracker as CryptoCompare } from "@arkecosystem/platform-sdk-cryptocompare";
 
+/**
+ *
+ *
+ * @export
+ * @class MarketService
+ */
 export class MarketService {
+	/**
+	 *
+	 *
+	 * @type {Contracts.PriceTracker}
+	 * @memberof MarketService
+	 */
 	#adapter: Contracts.PriceTracker;
 
+	/**
+	 *Creates an instance of MarketService.
+	 * @param {Contracts.PriceTracker} adapter
+	 * @memberof MarketService
+	 */
 	public constructor(adapter: Contracts.PriceTracker) {
 		this.#adapter = adapter;
 	}
 
+	/**
+	 *
+	 *
+	 * @static
+	 * @param {string} name
+	 * @param {Contracts.HttpClient} httpClient
+	 * @returns {MarketService}
+	 * @memberof MarketService
+	 */
 	public static make(name: string, httpClient: Contracts.HttpClient): MarketService {
 		return new MarketService(
 			{
@@ -20,66 +46,189 @@ export class MarketService {
 		);
 	}
 
+	/**
+	 *
+	 *
+	 * @param {Contracts.PriceTracker} adapter
+	 * @memberof MarketService
+	 */
 	public setAdapter(adapter: Contracts.PriceTracker): void {
 		this.#adapter = adapter;
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @returns {Promise<boolean>}
+	 * @memberof MarketService
+	 */
 	public async verifyToken(token: string): Promise<boolean> {
 		return this.#adapter.verifyToken(token);
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @returns {Promise<Contracts.MarketDataCollection>}
+	 * @memberof MarketService
+	 */
 	public async marketData(token: string): Promise<Contracts.MarketDataCollection> {
 		return this.#adapter.marketData(token);
 	}
 
+	/**
+	 *
+	 *
+	 * @param {Contracts.HistoricalPriceOptions} options
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalPrice(options: Contracts.HistoricalPriceOptions): Promise<Contracts.HistoricalData> {
 		return this.#adapter.historicalPrice(options);
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalPriceForDay(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalPrice({ token, currency, days: 24, type: "hour", dateFormat: "HH:mm" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalPriceForWeek(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalPrice({ token, currency, days: 7, type: "day", dateFormat: "ddd" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalPriceForMonth(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalPrice({ token, currency, days: 30, type: "day", dateFormat: "DD" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalPriceForQuarter(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalPrice({ token, currency, days: 120, type: "day", dateFormat: "DD.MM" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalPriceForYear(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalPrice({ token, currency, days: 365, type: "day", dateFormat: "DD.MM" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {Contracts.HistoricalVolumeOptions} options
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalVolume(options: Contracts.HistoricalVolumeOptions): Promise<Contracts.HistoricalData> {
 		return this.#adapter.historicalVolume(options);
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalVolumeForDay(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalVolume({ token, currency, days: 24, type: "hour", dateFormat: "HH:mm" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalVolumeForWeek(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalVolume({ token, currency, days: 7, type: "day", dateFormat: "ddd" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalVolumeForMonth(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalVolume({ token, currency, days: 30, type: "day", dateFormat: "DD" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalVolumeForQuarter(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalVolume({ token, currency, days: 120, type: "day", dateFormat: "DD.MM" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @returns {Promise<Contracts.HistoricalData>}
+	 * @memberof MarketService
+	 */
 	public async historicalVolumeForYear(token: string, currency: string): Promise<Contracts.HistoricalData> {
 		return this.historicalVolume({ token, currency, days: 365, type: "day", dateFormat: "DD.MM" });
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} token
+	 * @param {string} currency
+	 * @param {number} timestamp
+	 * @returns {Promise<number>}
+	 * @memberof MarketService
+	 */
 	public async dailyAverage(token: string, currency: string, timestamp: number): Promise<number> {
 		return this.#adapter.dailyAverage({ token, currency, timestamp });
 	}
