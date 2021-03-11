4e5a47a6be63831294a11e200fc2ac30f86ade00
Brian Faust <hello@basecode.sh>
2021-03-11 08:57:22 +0200

    wip

--- packages/platform-sdk-news/src/blockfolio.ts
+++ packages/platform-sdk-news/src/blockfolio.ts
@@ -2,13 +2,42 @@ import { Contracts } from "@arkecosystem/platform-sdk";
 
 import { BlockfolioResponse } from "./blockfolio.models";
 
+/**
+ *
+ *
+ * @export
+ * @class Blockfolio
+ */
 export class Blockfolio {
+	/**
+	 *
+	 *
+	 * @type {Contracts.HttpClient}
+	 * @memberof Blockfolio
+	 */
 	readonly #httpClient: Contracts.HttpClient;
 
+	/**
+	 *Creates an instance of Blockfolio.
+	 * @param {Contracts.HttpClient} httpClient
+	 * @memberof Blockfolio
+	 */
 	public constructor(httpClient: Contracts.HttpClient) {
 		this.#httpClient = httpClient;
 	}
 
+	/**
+	 *
+	 *
+	 * @param {{
+	 * 		page?: number;
+	 * 		query?: string;
+	 * 		coins: string[];
+	 * 		categories?: string[];
+	 * 	}} query
+	 * @returns {Promise<BlockfolioResponse>}
+	 * @memberof Blockfolio
+	 */
 	public async findByCoin(query: {
 		page?: number;
 		query?: string;
--- packages/platform-sdk-news/src/feed.ts
+++ packages/platform-sdk-news/src/feed.ts
@@ -1,16 +1,46 @@
 import RssParser from "rss-parser";
 
+/**
+ *
+ *
+ * @export
+ * @class FeedService
+ */
 export class FeedService {
+	/**
+	 *
+	 *
+	 * @type {RssParser}
+	 * @memberof FeedService
+	 */
 	readonly #parser: RssParser;
 
+	/**
+	 *Creates an instance of FeedService.
+	 * @memberof FeedService
+	 */
 	public constructor() {
 		this.#parser = new RssParser();
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} url
+	 * @returns {Promise<RssParser.Output<any>>}
+	 * @memberof FeedService
+	 */
 	public async parse(url: string): Promise<RssParser.Output<any>> {
 		return this.#parser.parseURL(url);
 	}
 
+	/**
+	 *
+	 *
+	 * @param {string} url
+	 * @returns {(Promise<RssParser.Item[] | undefined>)}
+	 * @memberof FeedService
+	 */
 	public async items(url: string): Promise<RssParser.Item[] | undefined> {
 		const { items } = await this.parse(url);
 
