83a3ad1f1df3e12ddd13db82455dfa5f8c961268
Brian Faust <hello@basecode.sh>
2021-03-11 09:20:23 +0200

    wip

--- packages/platform-sdk/src/contracts/coins/client.ts
+++ packages/platform-sdk/src/contracts/coins/client.ts
@@ -4,81 +4,395 @@ import { SignedTransactionData, TransactionDataType, WalletData } from "./data";
 
 export type ClientPaginatorCursor = string | number | undefined;
 
+/**
+ *
+ *
+ * @export
+ * @interface MetaPagination
+ */
 export interface MetaPagination {
+	/**
+	 *
+	 *
+	 * @type {ClientPaginatorCursor}
+	 * @memberof MetaPagination
+	 */
 	prev: ClientPaginatorCursor;
+	/**
+	 *
+	 *
+	 * @type {ClientPaginatorCursor}
+	 * @memberof MetaPagination
+	 */
 	self: ClientPaginatorCursor;
+	/**
+	 *
+	 *
+	 * @type {ClientPaginatorCursor}
+	 * @memberof MetaPagination
+	 */
 	next: ClientPaginatorCursor;
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface BroadcastResponse
+ */
 export interface BroadcastResponse {
+	/**
+	 *
+	 *
+	 * @type {string[]}
+	 * @memberof BroadcastResponse
+	 */
 	accepted: string[];
+	/**
+	 *
+	 *
+	 * @type {string[]}
+	 * @memberof BroadcastResponse
+	 */
 	rejected: string[];
+	/**
+	 *
+	 *
+	 * @type {Record<string, string[]>}
+	 * @memberof BroadcastResponse
+	 */
 	errors: Record<string, string[]>;
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface ClientService
+ */
 export interface ClientService {
+	/**
+	 *
+	 *
+	 * @returns {Promise<void>}
+	 * @memberof ClientService
+	 */
 	__destruct(): Promise<void>;
 
+	/**
+	 *
+	 *
+	 * @param {string} id
+	 * @returns {Promise<TransactionDataType>}
+	 * @memberof ClientService
+	 */
 	transaction(id: string): Promise<TransactionDataType>;
+	/**
+	 *
+	 *
+	 * @param {ClientTransactionsInput} query
+	 * @returns {Promise<TransactionDataCollection>}
+	 * @memberof ClientService
+	 */
 	transactions(query: ClientTransactionsInput): Promise<TransactionDataCollection>;
 
+	/**
+	 *
+	 *
+	 * @param {string} id
+	 * @returns {Promise<WalletData>}
+	 * @memberof ClientService
+	 */
 	wallet(id: string): Promise<WalletData>;
+	/**
+	 *
+	 *
+	 * @param {ClientWalletsInput} query
+	 * @returns {Promise<WalletDataCollection>}
+	 * @memberof ClientService
+	 */
 	wallets(query: ClientWalletsInput): Promise<WalletDataCollection>;
 
+	/**
+	 *
+	 *
+	 * @param {string} id
+	 * @returns {Promise<WalletData>}
+	 * @memberof ClientService
+	 */
 	delegate(id: string): Promise<WalletData>;
+	/**
+	 *
+	 *
+	 * @param {ClientWalletsInput} [query]
+	 * @returns {Promise<WalletDataCollection>}
+	 * @memberof ClientService
+	 */
 	delegates(query?: ClientWalletsInput): Promise<WalletDataCollection>;
 
+	/**
+	 *
+	 *
+	 * @param {string} id
+	 * @returns {Promise<VoteReport>}
+	 * @memberof ClientService
+	 */
 	votes(id: string): Promise<VoteReport>;
 	// TODO: return struct like VoteReport
+	/**
+	 *
+	 *
+	 * @param {string} id
+	 * @param {KeyValuePair} [query]
+	 * @returns {Promise<WalletDataCollection>}
+	 * @memberof ClientService
+	 */
 	voters(id: string, query?: KeyValuePair): Promise<WalletDataCollection>;
 
+	/**
+	 *
+	 *
+	 * @returns {Promise<boolean>}
+	 * @memberof ClientService
+	 */
 	syncing(): Promise<boolean>;
 
+	/**
+	 *
+	 *
+	 * @param {SignedTransactionData[]} transactions
+	 * @returns {Promise<BroadcastResponse>}
+	 * @memberof ClientService
+	 */
 	broadcast(transactions: SignedTransactionData[]): Promise<BroadcastResponse>;
+	/**
+	 *
+	 *
+	 * @param {SignedTransactionData[]} transactions
+	 * @param {string[]} hosts
+	 * @returns {Promise<BroadcastResponse>}
+	 * @memberof ClientService
+	 */
 	broadcastSpread(transactions: SignedTransactionData[], hosts: string[]): Promise<BroadcastResponse>;
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface ClientPagination
+ */
 export interface ClientPagination {
+	/**
+	 *
+	 *
+	 * @type {(string | number)}
+	 * @memberof ClientPagination
+	 */
 	cursor?: string | number;
+	/**
+	 *
+	 *
+	 * @type {number}
+	 * @memberof ClientPagination
+	 */
 	limit?: number;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientPagination
+	 */
 	orderBy?: string;
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface ClientTransactionsInput
+ * @extends {ClientPagination}
+ */
 export interface ClientTransactionsInput extends ClientPagination {
 	// Addresses
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientTransactionsInput
+	 */
 	address?: string;
+	/**
+	 *
+	 *
+	 * @type {string[]}
+	 * @memberof ClientTransactionsInput
+	 */
 	addresses?: string[];
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientTransactionsInput
+	 */
 	senderId?: string;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientTransactionsInput
+	 */
 	recipientId?: string;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientTransactionsInput
+	 */
 	walletId?: string;
 	// Public Keys
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientTransactionsInput
+	 */
 	senderPublicKey?: string;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientTransactionsInput
+	 */
 	recipientPublicKey?: string;
 	// AIP36
+	/**
+	 *
+	 *
+	 * @type {number}
+	 * @memberof ClientTransactionsInput
+	 */
 	entityType?: number;
+	/**
+	 *
+	 *
+	 * @type {number}
+	 * @memberof ClientTransactionsInput
+	 */
 	entitySubType?: number;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientTransactionsInput
+	 */
 	entityAction?: string;
 	// Meta
+	/**
+	 *
+	 *
+	 * @type {Record<string, any>}
+	 * @memberof ClientTransactionsInput
+	 */
 	asset?: Record<string, any>;
 	// Transaction Types
+	/**
+	 *
+	 *
+	 * @type {number}
+	 * @memberof ClientTransactionsInput
+	 */
 	type?: number;
+	/**
+	 *
+	 *
+	 * @type {number}
+	 * @memberof ClientTransactionsInput
+	 */
 	typeGroup?: number;
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface ClientWalletsInput
+ * @extends {ClientPagination}
+ */
 export interface ClientWalletsInput extends ClientPagination {
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientWalletsInput
+	 */
 	address?: string;
+	/**
+	 *
+	 *
+	 * @type {string[]}
+	 * @memberof ClientWalletsInput
+	 */
 	addresses?: string[];
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientWalletsInput
+	 */
 	publicKey?: string;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof ClientWalletsInput
+	 */
 	username?: string;
 }
 
 // TODO: move
+/**
+ *
+ *
+ * @export
+ * @interface VoteReport
+ */
 export interface VoteReport {
+	/**
+	 *
+	 *
+	 * @type {number}
+	 * @memberof VoteReport
+	 */
 	used: number;
+	/**
+	 *
+	 *
+	 * @type {number}
+	 * @memberof VoteReport
+	 */
 	available: number;
+	/**
+	 *
+	 *
+	 * @type {string[]}
+	 * @memberof VoteReport
+	 */
 	publicKeys: string[];
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface TransactionDetailInput
+ */
 export interface TransactionDetailInput {
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof TransactionDetailInput
+	 */
 	walletId?: string;
 }
--- packages/platform-sdk/src/contracts/coins/transaction.ts
+++ packages/platform-sdk/src/contracts/coins/transaction.ts
@@ -1,150 +1,651 @@
 import { RawTransactionData, SignedTransactionData } from "./data";
 
+/**
+ *
+ *
+ * @export
+ * @interface TransactionService
+ */
 export interface TransactionService {
+	/**
+	 *
+	 *
+	 * @returns {Promise<void>}
+	 * @memberof TransactionService
+	 */
 	__destruct(): Promise<void>;
 
 	// Core
+	/**
+	 *
+	 *
+	 * @param {TransferInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	transfer(input: TransferInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {SecondSignatureInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	secondSignature(input: SecondSignatureInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {DelegateRegistrationInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	delegateRegistration(
 		input: DelegateRegistrationInput,
 		options?: TransactionOptions,
 	): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {VoteInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	vote(input: VoteInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {MultiSignatureInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	multiSignature(input: MultiSignatureInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {IpfsInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	ipfs(input: IpfsInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {MultiPaymentInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	multiPayment(input: MultiPaymentInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {DelegateResignationInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	delegateResignation(input: DelegateResignationInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {HtlcLockInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	htlcLock(input: HtlcLockInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {HtlcClaimInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	htlcClaim(input: HtlcClaimInput, options?: TransactionOptions): Promise<SignedTransactionData>;
+	/**
+	 *
+	 *
+	 * @param {HtlcRefundInput} input
+	 * @param {TransactionOptions} [options]
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	htlcRefund(input: HtlcRefundInput, options?: TransactionOptions): Promise<SignedTransactionData>;
 
 	// Multi-Signature
+	/**
+	 *
+	 *
+	 * @param {RawTransactionData} transaction
+	 * @param {TransactionInputs} input
+	 * @returns {Promise<SignedTransactionData>}
+	 * @memberof TransactionService
+	 */
 	multiSign(transaction: RawTransactionData, input: TransactionInputs): Promise<SignedTransactionData>;
 
 	// Estimations
+	/**
+	 *
+	 *
+	 * @param {string} [value]
+	 * @returns {Promise<string>}
+	 * @memberof TransactionService
+	 */
 	estimateExpiration(value?: string): Promise<string>;
 }
 
 // Transaction Signing
+/**
+ *
+ *
+ * @export
+ * @interface TransactionInput
+ */
 export interface TransactionInput {
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof TransactionInput
+	 */
 	fee?: string;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof TransactionInput
+	 */
 	feeLimit?: string;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof TransactionInput
+	 */
 	nonce?: string;
+	/**
+	 *
+	 *
+	 * @type {string}
+	 * @memberof TransactionInput
+	 */
 	from: string;
 	sign: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		mnemonic?: string;
+		/**
+		 *
+		 *
+		 * @type {string[]}
+		 */
 		mnemonics?: string[];
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		secondMnemonic?: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		wif?: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		secondWif?: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		privateKey?: string;
 		multiSignature?: {
+			/**
+			 *
+			 *
+			 * @type {number}
+			 */
 			min: number;
+			/**
+			 *
+			 *
+			 * @type {string[]}
+			 */
 			publicKeys: string[];
 		};
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		senderPublicKey?: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		signature?: string;
 	};
 	contract?: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		address: string;
 	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface TransactionOptions
+ */
 export interface TransactionOptions {
+	/**
+	 *
+	 *
+	 * @type {boolean}
+	 * @memberof TransactionOptions
+	 */
 	unsignedBytes: boolean;
+	/**
+	 *
+	 *
+	 * @type {boolean}
+	 * @memberof TransactionOptions
+	 */
 	unsignedJson: boolean;
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface TransferInput
+ * @extends {TransactionInput}
+ */
 export interface TransferInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		amount: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		to: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		memo?: string;
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		expiration?: number;
 	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface SecondSignatureInput
+ * @extends {TransactionInput}
+ */
 export interface SecondSignatureInput extends TransactionInput {
-	data: { mnemonic: string };
+	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
+		mnemonic: string;
+	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface DelegateRegistrationInput
+ * @extends {TransactionInput}
+ */
 export interface DelegateRegistrationInput extends TransactionInput {
-	data: { username: string };
+	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
+		username: string;
+	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface VoteInput
+ * @extends {TransactionInput}
+ */
 export interface VoteInput extends TransactionInput {
-	data: { votes: string[]; unvotes: string[] };
+	data: {
+		/**
+		 *
+		 *
+		 * @type {string[]}
+		 */
+		votes: string[];
+		/**
+		 *
+		 *
+		 * @type {string[]}
+		 */
+		unvotes: string[];
+	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface MultiSignatureInput
+ * @extends {TransactionInput}
+ */
 export interface MultiSignatureInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {string[]}
+		 */
 		publicKeys: string[];
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		lifetime?: number;
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		min: number;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		senderPublicKey?: string;
 	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface IpfsInput
+ * @extends {TransactionInput}
+ */
 export interface IpfsInput extends TransactionInput {
-	data: { hash: string };
+	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
+		hash: string;
+	};
 }
 
 export interface MultiPaymentInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		memo?: string;
-		payments: { to: string; amount: string }[];
+		payments: {
+			/**
+			 *
+			 *
+			 * @type {string}
+			 */
+			to: string;
+			/**
+			 *
+			 *
+			 * @type {string}
+			 */
+			amount: string;
+		}[];
 	};
 }
 
 export type DelegateResignationInput = TransactionInput;
 
+/**
+ *
+ *
+ * @export
+ * @interface HtlcLockInput
+ * @extends {TransactionInput}
+ */
 export interface HtlcLockInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		amount: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		to: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		secretHash: string;
 		expiration: {
+			/**
+			 *
+			 *
+			 * @type {number}
+			 */
 			type: number;
+			/**
+			 *
+			 *
+			 * @type {number}
+			 */
 			value: number;
 		};
 	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface HtlcClaimInput
+ * @extends {TransactionInput}
+ */
 export interface HtlcClaimInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		lockTransactionId: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		unlockSecret: string;
 	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface HtlcRefundInput
+ * @extends {TransactionInput}
+ */
 export interface HtlcRefundInput extends TransactionInput {
-	data: { lockTransactionId: string };
+	data: {
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
+		lockTransactionId: string;
+	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface EntityRegistrationInput
+ * @extends {TransactionInput}
+ */
 export interface EntityRegistrationInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		type: number;
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		subType: number;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		name: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		ipfs: string;
 	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface EntityResignationInput
+ * @extends {TransactionInput}
+ */
 export interface EntityResignationInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		type: number;
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		subType: number;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		registrationId: string;
 	};
 }
 
+/**
+ *
+ *
+ * @export
+ * @interface EntityUpdateInput
+ * @extends {TransactionInput}
+ */
 export interface EntityUpdateInput extends TransactionInput {
 	data: {
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		type: number;
+		/**
+		 *
+		 *
+		 * @type {number}
+		 */
 		subType: number;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		registrationId: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		name?: string;
+		/**
+		 *
+		 *
+		 * @type {string}
+		 */
 		ipfs?: string;
 	};
 }
