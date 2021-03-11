11cd89ef1725db8faf4e3a3b50f6259b71511ac0
Brian Faust <hello@basecode.sh>
2021-03-11 08:41:14 +0200

    wip

--- packages/platform-sdk-support/src/validator.ts
+++ packages/platform-sdk-support/src/validator.ts
@@ -2,9 +2,29 @@ import Joi from "joi";
 
 export const ValidatorSchema = Joi;
 
+/**
+ *
+ *
+ * @export
+ * @class Validator
+ */
 export class Validator {
+	/**
+	 *
+	 *
+	 * @type {(Joi.ValidationError | undefined)}
+	 * @memberof Validator
+	 */
 	#error: Joi.ValidationError | undefined;
 
+	/**
+	 *
+	 *
+	 * @param {object} data
+	 * @param {Joi.Schema} schema
+	 * @returns {*}
+	 * @memberof Validator
+	 */
 	public validate(data: object, schema: Joi.Schema): any {
 		const { error, value } = schema.validate(data);
 
@@ -13,18 +33,42 @@ export class Validator {
 		return value;
 	}
 
+	/**
+	 *
+	 *
+	 * @returns {boolean}
+	 * @memberof Validator
+	 */
 	public passes(): boolean {
 		return this.#error === undefined;
 	}
 
+	/**
+	 *
+	 *
+	 * @returns {boolean}
+	 * @memberof Validator
+	 */
 	public fails(): boolean {
 		return !this.passes();
 	}
 
+	/**
+	 *
+	 *
+	 * @returns {(string[] | undefined)}
+	 * @memberof Validator
+	 */
 	public errors(): string[] | undefined {
 		return this.#error?.details.map((error: Joi.ValidationErrorItem) => error.message);
 	}
 
+	/**
+	 *
+	 *
+	 * @returns {(Joi.ValidationError | undefined)}
+	 * @memberof Validator
+	 */
 	public error(): Joi.ValidationError | undefined {
 		return this.#error;
 	}
