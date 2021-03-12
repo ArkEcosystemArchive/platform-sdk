2aed1eb7bcb4a341c917f75728fbdb8beff1754e
Brian Faust <hello@basecode.sh>
2021-03-12 04:40:18 +0200

    wip

--- packages/platform-sdk-crypto/src/bip39.ts
+++ packages/platform-sdk-crypto/src/bip39.ts
@@ -1,6 +1,14 @@
 import { generateMnemonic, mnemonicToEntropy, mnemonicToSeedSync, validateMnemonic, wordlists } from "bip39";
 
 /**
+ * Implements all functionality that is required to create and validate BIP39
+ * mnemonic passphrases which are used to generate deterministic private keys
+ * which in turn are used to derive public keys and addresses for end-users.
+ *
+ * @remarks
+ * BIP39 mnemonic passphrases should always be generated with 24 words if
+ * they are supported. 12 words are already very secure but technology is
+ * moving fast and mnemonic passphrases are rarely written out by hand.
  *
  * @see {@link https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki}
  *
@@ -9,19 +17,23 @@ import { generateMnemonic, mnemonicToEntropy, mnemonicToSeedSync, validateMnemon
  */
 export class BIP39 {
 	/**
-	 *
+	 * Generates a BIP39 compliant mnemonic passphrases.
 	 *
 	 * @static
 	 * @param {string} [locale="english"]
 	 * @returns {string}
 	 * @memberof BIP39
 	 */
-	public static generate(locale = "english"): string {
+	public static generate(locale: string = "english"): string {
 		return generateMnemonic(undefined, undefined, wordlists[locale]);
 	}
 
 	/**
+	 * Validates that the input is a valid mnemonic passphrase.
 	 *
+	 * @remarks
+	 * Valid in this context means that it only contains words from the BIP39
+	 * wordlists for the various languages that it supports.
 	 *
 	 * @static
 	 * @param {string} mnemonic
@@ -29,12 +41,14 @@ export class BIP39 {
 	 * @returns {boolean}
 	 * @memberof BIP39
 	 */
-	public static validate(mnemonic: string, locale = "english"): boolean {
+	public static validate(mnemonic: string, locale: string = "english"): boolean {
 		return validateMnemonic(BIP39.normalize(mnemonic), wordlists[locale]);
 	}
 
 	/**
-	 *
+	 * Turns the given mnemonic passphrase into a seed which can then be
+	 * used for the derivation of a private key to derive a public key and
+	 * an address from it which will be used to send and receive funds.
 	 *
 	 * @static
 	 * @param {string} mnemonic
@@ -46,7 +60,9 @@ export class BIP39 {
 	}
 
 	/**
-	 *
+	 * Turns the given mnemonic passphrase into entropy which can then be
+	 * used for the derivation of a private key to derive a public key and
+	 * an address from it which will be used to send and receive funds.
 	 *
 	 * @static
 	 * @param {string} mnemonic
@@ -58,7 +74,9 @@ export class BIP39 {
 	}
 
 	/**
-	 *
+	 * Normalises the mnemonic passphrase by ensuring a consistent encoding
+	 * to avoid the loss of funds which could be caused by different encodings
+	 * when going from asian to latin or cyrillic alphabets and character sets.
 	 *
 	 * @static
 	 * @param {string} mnemonic
