6221ef731eec39b97a83d30e81f49ef74359810f
Brian Faust <hello@basecode.sh>
2021-03-12 08:09:01 +0200

    wip

--- packages/platform-sdk-crypto/src/wif.test.ts
+++ packages/platform-sdk-crypto/src/wif.test.ts
@@ -5,11 +5,11 @@ import { WIF } from "./wif";
 test("#encode", () => {
 	expect(
 		WIF.encode({
-			version: 1,
+			version: 170,
 			privateKey: "d8839c2432bfd0a67ef10a804ba991eabba19f154a3d707917681d45822a5712",
 			compressed: true,
 		}),
-	).toMatchInlineSnapshot(`"GqWL4nXjDSjBfMvK95GK6d8zm3v8XQoM7T6VKmCAEtAL4tTtaWt"`);
+	).toMatchInlineSnapshot(`"SGq4xLgZKCGxs7bjmwnBrWcT4C1ADFEermj846KC97FSv1WFD1dA"`);
 });
 
 test("#decode", () => {
