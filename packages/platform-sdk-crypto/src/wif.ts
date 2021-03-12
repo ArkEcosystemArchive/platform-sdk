a187a096834440eb664a08105776385737f4ada0
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-12 08:09:18 +0200

    feat(crypto): implement `WIF` helpers (#1209)

---
+++ packages/platform-sdk-crypto/src/wif.ts
@@ -0,0 +1,19 @@
+import { encode, decode } from "wif";
+
+export interface Structure {
+    readonly version: number;
+    readonly privateKey: string;
+    readonly compressed: boolean;
+}
+
+export class WIF {
+	public static encode({ version, privateKey, compressed }: Structure): string {
+		return encode(version, Buffer.from(privateKey, "hex"), compressed);
+	}
+
+	public static decode(string: string): Structure {
+		const { privateKey, compressed, version } = decode(string);
+
+		return { version, privateKey: privateKey.toString("hex"), compressed };
+	}
+}
