1c785fd77b64aa5d438022f439930a88b3eecc47
Brian Faust <hello@basecode.sh>
2021-03-12 08:08:22 +0200

    wip

--- packages/platform-sdk-crypto/src/wif.ts
+++ packages/platform-sdk-crypto/src/wif.ts
@@ -1,17 +1,19 @@
-import { encode, decode, WIFReturn } from "wif";
+import { encode, decode } from "wif";
 
-export interface WIFInput {
+export interface Structure {
     readonly version: number;
     readonly privateKey: string;
     readonly compressed: boolean;
 }
 
 export class WIF {
-	public static encode({ version, privateKey, compressed }: WIFInput): string {
+	public static encode({ version, privateKey, compressed }: Structure): string {
 		return encode(version, Buffer.from(privateKey, "hex"), compressed);
 	}
 
-	public static decode(string: string, version?: number): WIFReturn {
-		return decode(string, version);
+	public static decode(string: string): Structure {
+		const { privateKey, compressed, version } = decode(string);
+
+		return { version, privateKey: privateKey.toString("hex"), compressed };
 	}
 }
