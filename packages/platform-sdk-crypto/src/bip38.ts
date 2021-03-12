757b87944d7b2e5422e949ed1f36485f27dbce97
Brian Faust <hello@basecode.sh>
2021-03-12 04:44:51 +0200

    wip

--- packages/platform-sdk-crypto/src/bip38.ts
+++ packages/platform-sdk-crypto/src/bip38.ts
@@ -1,6 +1,8 @@
 import { decrypt, encrypt, verify } from "bip38";
 
 /**
+ * Implements all functionality that is required to encrypt and decrypt a
+ * passphrase-protected private key in compliance with BIP38 specifications.
  *
  * @see {@link https://github.com/bitcoin/bips/blob/master/bip-0038.mediawiki}
  *
@@ -9,43 +11,43 @@ import { decrypt, encrypt, verify } from "bip38";
  */
 export class BIP38 {
 	/**
-	 *
+	 * Encrypts a private key using the given mnemonic passphrase.
 	 *
 	 * @static
-	 * @param {Buffer} buffer
+	 * @param {Buffer} privateKey
 	 * @param {string} mnemonic
 	 * @param {boolean} [compressed=true]
 	 * @returns {string}
 	 * @memberof BIP38
 	 */
-	public static encrypt(buffer: Buffer, mnemonic: string, compressed = true): string {
-		return encrypt(buffer, compressed, mnemonic);
+	public static encrypt(privateKey: Buffer, mnemonic: string, compressed: boolean = true): string {
+		return encrypt(privateKey, compressed, mnemonic);
 	}
 
 	/**
-	 *
+	 * Decrypts an encrypted private key using the given mnemonic passphrase.
 	 *
 	 * @static
-	 * @param {string} string
+	 * @param {string} value
 	 * @param {string} mnemonic
 	 * @returns {{ compressed: boolean; privateKey: string }}
 	 * @memberof BIP38
 	 */
-	public static decrypt(string: string, mnemonic: string): { compressed: boolean; privateKey: string } {
-		const { compressed, privateKey } = decrypt(string, mnemonic);
+	public static decrypt(value: string, mnemonic: string): { compressed: boolean; privateKey: string } {
+		const { compressed, privateKey } = decrypt(value, mnemonic);
 
 		return { compressed, privateKey: privateKey.toString("hex") };
 	}
 
 	/**
-	 *
+	 * Verifies if the given value has been encrypted with BIP38.
 	 *
 	 * @static
-	 * @param {string} string
+	 * @param {string} value
 	 * @returns {boolean}
 	 * @memberof BIP38
 	 */
-	public static verify(string: string): boolean {
-		return verify(string);
+	public static verify(value: string): boolean {
+		return verify(value);
 	}
 }
