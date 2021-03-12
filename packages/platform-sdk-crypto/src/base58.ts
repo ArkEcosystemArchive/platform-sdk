181f5ca33af35ce878e06f6aef3352ee435c0765
Brian Faust <hello@basecode.sh>
2021-03-12 04:49:45 +0200

    wip

--- packages/platform-sdk-crypto/src/base58.ts
+++ packages/platform-sdk-crypto/src/base58.ts
@@ -3,6 +3,8 @@ import { base58 } from "bstring";
 const normalise = (value: string | Buffer) => (value instanceof Buffer ? value : Buffer.from(value, "hex"));
 
 /**
+ * Implements all functionality that is required to work with the Base58
+ * binary-to-text encoding scheme as defined by the specifications.
  *
  * @see {@link https://learnmeabitcoin.com/technical/base58}
  *
@@ -11,7 +13,7 @@ const normalise = (value: string | Buffer) => (value instanceof Buffer ? value :
  */
 export class Base58 {
 	/**
-	 *
+	 * Encodes a string in compliance with the Base58 encoding scheme.
 	 *
 	 * @static
 	 * @param {(string | Buffer)} value
@@ -23,7 +25,7 @@ export class Base58 {
 	}
 
 	/**
-	 *
+	 * Decodes a string in compliance with the Base58 encoding scheme.
 	 *
 	 * @static
 	 * @param {(string | Buffer)} value
@@ -35,7 +37,12 @@ export class Base58 {
 	}
 
 	/**
+	 * Validates that the given value has been encoded in compliance with
+	 * the Base58 encoding scheme to ensure that decoding it is possible.
 	 *
+	 * @remarks
+	 * This method should be called before attemtping to decode a value
+	 * to avoid any unhandled or unexpected exceptions.
 	 *
 	 * @static
 	 * @param {string} value
