9f14cfc9003169762db1ce4a84c8e905257462b6
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-18 06:04:59 +0200

    docs(support): add TSDoc comments (#1193)

--- packages/platform-sdk-support/src/validator.ts
+++ packages/platform-sdk-support/src/validator.ts
@@ -2,9 +2,29 @@ import Joi from "joi";
 
 export const ValidatorSchema = Joi;
 
+/**
+ *
+ *
+ * @export
+ * @class Validator
+ */
 export class Validator {
+	/**
+	 * The latest validation error.
+	 *
+	 * @type {(Joi.ValidationError | undefined)}
+	 * @memberof Validator
+	 */
 	#error: Joi.ValidationError | undefined;
 
+	/**
+	 * Compares the given data against the given schema.
+	 *
+	 * @param {object} data
+	 * @param {Joi.Schema} schema
+	 * @returns {*}
+	 * @memberof Validator
+	 */
 	public validate(data: object, schema: Joi.Schema): any {
 		const { error, value } = schema.validate(data);
 
@@ -13,18 +33,42 @@ export class Validator {
 		return value;
 	}
 
+	/**
+	 * Indicates wheter the data has passed validation.
+	 *
+	 * @returns {boolean}
+	 * @memberof Validator
+	 */
 	public passes(): boolean {
 		return this.#error === undefined;
 	}
 
+	/**
+	 * Indicates wheter the data has failed validation.
+	 *
+	 * @returns {boolean}
+	 * @memberof Validator
+	 */
 	public fails(): boolean {
 		return !this.passes();
 	}
 
+	/**
+	 * Returns the human-readable explanation for the latest occurred.
+	 *
+	 * @returns {(string[] | undefined)}
+	 * @memberof Validator
+	 */
 	public errors(): string[] | undefined {
 		return this.#error?.details.map((error: Joi.ValidationErrorItem) => error.message);
 	}
 
+	/**
+	 * Returns the latest error that has occurred.
+	 *
+	 * @returns {(Joi.ValidationError | undefined)}
+	 * @memberof Validator
+	 */
 	public error(): Joi.ValidationError | undefined {
 		return this.#error;
 	}
