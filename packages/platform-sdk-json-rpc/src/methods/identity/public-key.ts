5e86eb491a189163a2db762cb71dd56086ca047a
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-19 07:58:17 +0200

    feat(json-rpc): initial implementation (#1217)

---
+++ packages/platform-sdk-json-rpc/src/methods/identity/public-key.ts
@@ -0,0 +1,28 @@
+import { Coins } from "@arkecosystem/platform-sdk";
+import Joi from "joi";
+
+export const registerPublicKey = (coin: Coins.Coin) => [
+	{
+		name: "identity.publicKey.fromMnemonic",
+		async method({ mnemonic }) {
+			return coin.identity().publicKey().fromMnemonic(mnemonic);
+		},
+		schema: Joi.object().keys({ mnemonic: Joi.string().required() }).required(),
+	},
+	{
+		name: "identity.publicKey.fromMultiSignature",
+		async method({ min, publicKeys }) {
+			return coin.identity().publicKey().fromMultiSignature(min, publicKeys);
+		},
+		schema: Joi.object()
+			.keys({ min: Joi.string().required(), publicKeys: Joi.array().items(Joi.string()) })
+			.required(),
+	},
+	{
+		name: "identity.publicKey.fromWIF",
+		async method({ wif }) {
+			return coin.identity().publicKey().fromWIF(wif);
+		},
+		schema: Joi.object().keys({ wif: Joi.string().required() }).required(),
+	},
+];
