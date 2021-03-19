6693580550ee01b60aa438eab8e64e6712bb8885
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-19 09:52:06 +0200

    feat(json-rpc): retrieve transaction, wallet and delegate details (#1220)

--- packages/platform-sdk-json-rpc/src/methods/client.ts
+++ packages/platform-sdk-json-rpc/src/methods/client.ts
@@ -3,12 +3,45 @@ import Joi from "joi";
 
 export const registerClient = (coin: Coins.Coin) => [
 	{
+		name: "client.transaction",
+		async method({ id }) {
+			return (await coin.client().transaction(id)).toObject();
+		},
+		schema: Joi.object({
+			id: Joi.string().required(),
+		}).required(),
+	},
+	{
+		name: "client.wallet",
+		async method({ id }) {
+			return (await coin.client().wallet(id)).toObject();
+		},
+		schema: Joi.object({
+			id: Joi.string().required(),
+		}).required(),
+	},
+	{
+		name: "client.delegate",
+		async method({ id }) {
+			return (await coin.client().delegate(id)).toObject();
+		},
+		schema: Joi.object({
+			id: Joi.string().required(),
+		}).required(),
+	},
+	{
+		name: "client.syncing",
+		async method() {
+			return coin.client().syncing();
+		},
+	},
+	{
 		name: "client.broadcast",
-		async method(input) {
+		async method({ id, data }) {
 			// @ts-ignore
 			return coin.client().broadcast([{
-				id: () => input.id,
-				toBroadcast: () => input.data,
+				id: () => id,
+				toBroadcast: () => data,
 			}]);
 		},
 		schema: Joi.object({
