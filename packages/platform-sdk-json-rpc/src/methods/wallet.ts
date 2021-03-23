b3ddb38b47fb3794c9aed9b61a19fca1af6d504f
Brian Faust <hello@basecode.sh>
2021-03-23 07:45:13 +0200

    wip

--- packages/platform-sdk-json-rpc/src/methods/wallet.ts
+++ packages/platform-sdk-json-rpc/src/methods/wallet.ts
@@ -4,18 +4,18 @@ import Joi from "joi";
 import { baseSchema, makeCoin } from "../helpers";
 
 export const registerWallet = () => [
-	{
-		name: "wallet.generate",
-		async method({ coin, network }) {
-			const instance = await makeCoin(coin, network);
-			const mnemonic = BIP39.generate();
+    {
+        name: "wallet.generate",
+        async method({ coin, network }) {
+            const instance = await makeCoin(coin, network);
+            const mnemonic = BIP39.generate();
 
-			return {
-				address: await instance.identity().address().fromMnemonic(mnemonic),
-				publicKey: await instance.identity().publicKey().fromMnemonic(mnemonic),
-				mnemonic,
-			};
-		},
-		schema: Joi.object({ ...baseSchema }).required(),
-	},
+            return {
+                address: await instance.identity().address().fromMnemonic(mnemonic),
+                publicKey: await instance.identity().publicKey().fromMnemonic(mnemonic),
+                mnemonic,
+            };
+        },
+        schema: Joi.object(baseSchema).required(),
+    },
 ];
