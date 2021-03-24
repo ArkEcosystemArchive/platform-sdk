58bb8c215278d1e6c9c0e062e8443d0246c0b83f
Brian Faust <faustbrian@users.noreply.github.com>
2021-03-24 08:41:49 +0200

    docs(btc-indexer): add TSDoc comments (#1195)

--- packages/platform-sdk-btc-indexer/src/client.ts
+++ packages/platform-sdk-btc-indexer/src/client.ts
@@ -7,11 +7,45 @@ import { v4 as uuidv4 } from "uuid";
 import { Database } from "./database";
 import { Flags } from "./types";
 
+/**
+ * Implements a JSON-RPC client for bitcoind.
+ *
+ * @export
+ * @class Client
+ */
 export class Client {
+	/**
+	 * The HTTP client instance.
+	 *
+	 * @type {Request}
+	 * @memberof Client
+	 */
 	readonly #client;
+
+	/**
+	 * The logger instance.
+	 *
+	 * @type {Logger}
+	 * @memberof Client
+	 */
 	readonly #logger: Logger;
+
+	/**
+	 * The database instance.
+	 *
+	 * @type {Database}
+	 * @memberof Client
+	 */
 	readonly #database: Database;
 
+	/**
+	 * Creates an instance of Client.
+	 *
+	 * @param {Flags} flags
+	 * @param {Logger} logger
+	 * @param {Database} database
+	 * @memberof Client
+	 */
 	public constructor(flags: Flags, logger: Logger, database: Database) {
 		const { hostname: host, port, protocol } = urlParseLax(flags.host);
 
@@ -20,14 +54,34 @@ export class Client {
 		this.#database = database;
 	}
 
+	/**
+	 * Returns the latest height / block number.
+	 *
+	 * @returns {Promise<number>}
+	 * @memberof Client
+	 */
 	public async height(): Promise<number> {
 		return this.post<number>("getblockcount", []);
 	}
 
+	/**
+	 * Returns the block data for the given ID.
+	 *
+	 * @param {number} id
+	 * @returns {Promise<Record<string, any>>}
+	 * @memberof Client
+	 */
 	public async block(id: number): Promise<Record<string, any>> {
 		return this.post("getblock", [await this.post("getblockhash", [id])]);
 	}
 
+	/**
+	 * Returns the block data for the given ID, including transactions.
+	 *
+	 * @param {number} id
+	 * @returns {Promise<Record<string, any>>}
+	 * @memberof Client
+	 */
 	public async blockWithTransactions(id: number): Promise<Record<string, any>> {
 		const block = await this.block(id);
 
@@ -52,10 +106,28 @@ export class Client {
 		return block;
 	}
 
+	/**
+	 * Returns JSON transaction data for the given ID.
+	 *
+	 * @private
+	 * @param {string} id
+	 * @returns {Promise<Record<string, any>>}
+	 * @memberof Client
+	 */
 	private async transaction(id: string): Promise<Record<string, any>> {
 		return this.post("getrawtransaction", [id, true]);
 	}
 
+	/**
+	 * Sends a HTTP POST request to the bitcoind JSON-RPC.
+	 *
+	 * @private
+	 * @template T
+	 * @param {string} method
+	 * @param {*} params
+	 * @returns {Promise<T>}
+	 * @memberof Client
+	 */
 	private async post<T = Record<string, any>>(method: string, params: any): Promise<T> {
 		return (
 			await this.#client.post("/", {
