004454a4d51812cbbd9ee0a3cdc8b78090b09ab4
Brian Faust <hello@basecode.sh>
2021-03-25 08:34:10 +0200

    wip

--- packages/platform-sdk-profiles/src/drivers/contracts/plugins/plugin-repository.ts
+++ packages/platform-sdk-profiles/src/drivers/contracts/plugins/plugin-repository.ts
@@ -13,70 +13,17 @@ interface Plugin {
 	urls: string[];
 }
 
-export class PluginRepository {
-	readonly #data: DataRepository;
-	readonly #registry: PluginRegistry;
-
-	public constructor() {
-		this.#data = new DataRepository();
-		this.#registry = new PluginRegistry();
-	}
-
-	public all(): Record<string, Plugin> {
-		return this.#data.all() as Record<string, Plugin>;
-	}
-
-	public first(): Plugin {
-		return this.#data.first();
-	}
-
-	public last(): Plugin {
-		return this.#data.last();
-	}
-
-	public keys(): string[] {
-		return this.#data.keys();
-	}
-
-	public values(): Plugin[] {
-		return this.#data.values();
-	}
-
-	public push(plugin: Except<Plugin, "id">): Plugin {
-		const id: string = uuidv4();
-
-		this.#data.set(id, { id, ...plugin });
-
-		return this.findById(id);
-	}
-
-	public fill(data: object): void {
-		this.#data.fill(data);
-	}
-
-	public findById(id: string): Plugin {
-		const plugin: Plugin | undefined = this.#data.get(id);
-
-		if (!plugin) {
-			throw new Error(`Failed to find a plugin for [${id}].`);
-		}
-
-		return plugin;
-	}
-
-	public forget(id: string): void {
-		this.#data.forget(id);
-	}
-
-	public flush(): void {
-		this.#data.flush();
-	}
-
-	public count(): number {
-		return this.keys().length;
-	}
-
-	public registry(): PluginRegistry {
-		return this.#registry;
-	}
+export interface IPluginRepository {
+    all(): Record<string, Plugin>;
+    first(): Plugin;
+    last(): Plugin;
+    keys(): string[];
+    values(): Plugin[];
+    push(plugin: Except<Plugin, "id">): Plugin;
+    fill(data: object): void;
+    findById(id: string): Plugin;
+    forget(id: string): void;
+    flush(): void;
+    count(): number;
+    registry(): PluginRegistry;
 }
