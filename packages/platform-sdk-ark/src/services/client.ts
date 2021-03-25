d0137baa60607a0526bc124f3ad9cbe3c56af7d0
Edgar Goetzendorff <hello@dated.fun>
2021-03-25 11:06:32 +0100

    fix(ark): handle different broadcast response error types

--- packages/platform-sdk-ark/src/services/client.ts
+++ packages/platform-sdk-ark/src/services/client.ts
@@ -279,14 +279,18 @@ export class ClientService implements Contracts.ClientService {
 
 		if (errors) {
 			for (const [key, value] of Object.entries(errors)) {
-				if (!Array.isArray(result.errors[key])) {
-					result.errors[key] = [];
-				}
+				if (Array.isArray(value)) {
+					if (!Array.isArray(result.errors[key])) {
+						result.errors[key] = [];
+					}
 
-				// @ts-ignore
-				for (const error of value) {
 					// @ts-ignore
-					result.errors[key].push(error.type);
+					for (const error of value) {
+						// @ts-ignore
+						result.errors[key].push(error.type);
+					}
+				} else {
+					result.errors[key] = value;
 				}
 			}
 		}
