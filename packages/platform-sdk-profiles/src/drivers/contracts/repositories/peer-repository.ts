49b6460496985a4c1620032f86214deb1450dab9
Brian Faust <hello@basecode.sh>
2021-03-25 09:06:27 +0200

    wip

--- packages/platform-sdk-profiles/src/drivers/contracts/repositories/peer-repository.ts
+++ packages/platform-sdk-profiles/src/drivers/contracts/repositories/peer-repository.ts
@@ -1,6 +1,6 @@
 import { IProfile } from "../profiles/profile";
 
-interface Peer {
+interface IPeer {
 	name: string;
 	host: string;
 	isMultiSignature: boolean;
@@ -8,16 +8,16 @@ interface Peer {
 
 export interface IPeerRepository {
     fill(peers: object): void;
-    all(): Record<string, Peer>;
+    all(): Record<string, IPeer>;
     keys(): string[];
     values(): IProfile[];
-    get(coin: string, network: string): Peer[];
-    create(coin: string, network: string, peer: Peer): void;
+    get(coin: string, network: string): IPeer[];
+    create(coin: string, network: string, peer: IPeer): void;
     has(coin: string, network: string): boolean;
-    update(coin: string, network: string, host: string, peer: Peer): void;
-    forget(coin: string, network: string, peer: Peer): void;
-    toObject(): Record<string, Peer>;
-    getRelay(coin: string, network: string): Peer | undefined;
-    getRelays(coin: string, network: string): Peer[];
-    getMultiSignature(coin: string, network: string): Peer | undefined;
+    update(coin: string, network: string, host: string, peer: IPeer): void;
+    forget(coin: string, network: string, peer: IPeer): void;
+    toObject(): Record<string, IPeer>;
+    getRelay(coin: string, network: string): IPeer | undefined;
+    getRelays(coin: string, network: string): IPeer[];
+    getMultiSignature(coin: string, network: string): IPeer | undefined;
 }
