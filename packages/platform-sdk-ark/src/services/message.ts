f051ff6d5e19f8ef1b43aa06a0cf5c7c9e44e5b7
Edgar Goetzendorff <hello@dated.fun>
2021-03-26 02:48:09 +0100

    feat(ark): sign message with wif

--- packages/platform-sdk-ark/src/services/message.ts
+++ packages/platform-sdk-ark/src/services/message.ts
@@ -3,9 +3,19 @@ import { Contracts as CryptoContracts, Keys } from "@arkecosystem/crypto-identit
 import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
 import { BIP39 } from "@arkecosystem/platform-sdk-crypto";
 
+import { IdentityService } from "./identity";
+
 export class MessageService implements Contracts.MessageService {
+	readonly #identityService: IdentityService;
+
+	private constructor(identityService: IdentityService) {
+		this.#identityService = identityService;
+	}
+
 	public static async __construct(config: Coins.Config): Promise<MessageService> {
-		return new MessageService();
+		const identityService = await IdentityService.__construct(config);
+
+		return new MessageService(identityService);
 	}
 
 	public async __destruct(): Promise<void> {
@@ -14,7 +24,19 @@ export class MessageService implements Contracts.MessageService {
 
 	public async sign(input: Contracts.MessageInput): Promise<Contracts.SignedMessage> {
 		try {
-			const keys: CryptoContracts.KeyPair = Keys.fromPassphrase(BIP39.normalize(input.mnemonic));
+			let keys: CryptoContracts.KeyPair | undefined;
+			
+			if (input.mnemonic) {
+				keys = await this.#identityService.keys().fromMnemonic(BIP39.normalize(input.mnemonic));
+			}
+
+			if (input.wif) {
+				keys = await this.#identityService.keys().fromWIF(input.wif);
+			}
+
+			if (!keys) {
+				throw new Error("Failed to retrieve the keys for the signatory wallet.");
+			}
 
 			return {
 				message: input.message,
