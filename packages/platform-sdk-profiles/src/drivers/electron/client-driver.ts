df20d6b953ed99e8997ae9dac7ca2438ddefc5b8
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-03-29 11:37:18 -0300

    separate client and server parts

---
+++ packages/platform-sdk-profiles/src/drivers/electron/client-driver.ts
@@ -0,0 +1,45 @@
+import { Container } from "../../environment/container";
+import { EnvironmentOptions } from "../../environment/env.models";
+import { Driver, IProfile, IProfileRepository } from "../../contracts";
+import { Identifiers } from "../../environment/container.models";
+import { ProcessRender } from "./contracts";
+import { Events } from "./events";
+
+export class ElectronClientDriver implements Driver {
+	/**
+	 * The main process IPC listeners.
+	 *
+	 * @type {Record<string, Function>}
+	 * @memberof ElectronDriver
+	 */
+	readonly #listeners: Record<string, Function> = {};
+
+	readonly #container: Container;
+
+	public constructor(container: Container) {
+		this.#container = container;
+
+		// TODO Extract this static mapping somewhere else
+		this.#listeners[Events.ProfileFactory.fromName] = (name: string): IProfile | undefined => {
+			return this.#container.get<IProfileRepository>(Identifiers.ProfileRepository).findByName(name);
+		}
+	}
+
+	/**
+	 * Create all necessary container bindings based on the given options.
+	 *
+	 * @param {Container} container
+	 * @param {EnvironmentOptions} options
+	 * @memberof MemoryDriver
+	 */
+	public make(container: Container, options: EnvironmentOptions): void {
+		const ipcRenderer: ProcessRender | undefined = options.driverArguments?.ipcRenderer as unknown as ProcessRender;
+
+		if (ipcRenderer !== undefined) {
+			// @TODO: register classes that use `ipcRenderer.send` instead of in-memory constructs
+
+			// Expose the render process for any additional work
+			container.bind(Identifiers.ProcessRenderer, ipcRenderer);
+		}
+	}
+}
