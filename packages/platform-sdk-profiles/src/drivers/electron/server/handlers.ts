76b9b8322577b36697f312d6cf6222fc3447c947
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-03-31 18:56:59 -0300

    wip

--- packages/platform-sdk-profiles/src/drivers/electron/server/handlers.ts
+++ packages/platform-sdk-profiles/src/drivers/electron/server/handlers.ts
@@ -11,15 +11,15 @@ export class Handlers {
 		this.#env = env;
 	}
 
-	public registerHandlers(env: Environment) {
+	public registerHandlers() {
 
 		ipcMain.handle(Events.ProfileFactory.fromName, async (event, { name }) => {
-			const profile: IProfile = env.profiles().findByName(name) || env.profiles().create(name);
+			const profile: IProfile = this.#env.profiles().findByName(name) || this.#env.profiles().create(name);
 			event.reply(Events.ProfileFactory.fromName, profile.toObject());
 		});
 
 		ipcMain.handle(Events.ProfileFactory.fromId, async (event, { id }) => {
-			const profile: IProfile = env.profiles().findById(id);
+			const profile: IProfile = this.#env.profiles().findById(id);
 			event.reply(Events.ProfileFactory.fromId, profile.toObject());
 		});
 
