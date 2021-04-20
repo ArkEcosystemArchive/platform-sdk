3575662099e34f3af97f2fa08adc6186464727d3
marianogoldman <marianogoldman@users.noreply.github.com>
2021-04-20 17:30:14 +0000

    style: resolve style guide violations

--- packages/platform-sdk-btc-indexer/src/index.ts
+++ packages/platform-sdk-btc-indexer/src/index.ts
@@ -1,8 +1,8 @@
 import Logger from "@ptkdev/logger";
+import pEachSeries from "p-each-series";
 
 import { useClient, useDatabase, useLogger } from "./helpers";
 import { Flags } from "./types";
-import pEachSeries from "p-each-series";
 
 /**
  * Launch the indexer and subscribe to updates for new data.
@@ -20,13 +20,12 @@ export const subscribe = async (flags: Flags): Promise<void> => {
 	// copy of the blockchain to avoid holes in the historical data of users.
 	const [localHeight, remoteHeight] = [database.lastBlockNumber(), await client.height()];
 
-	const step: number = 10;
+	const step = 10;
 	for (let i = localHeight; i <= remoteHeight; i += step) {
-
 		const batch: Promise<Record<string, any>>[] = [];
 		for (let j = i; j < i + step; j++) {
 			batch.push(client.blockWithTransactions(j));
 		}
-		await pEachSeries(batch, block => database.storeBlockWithTransactions(block));
+		await pEachSeries(batch, (block) => database.storeBlockWithTransactions(block));
 	}
 };
