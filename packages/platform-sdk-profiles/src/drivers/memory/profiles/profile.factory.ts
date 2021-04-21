bb501d5c09757203e2fc92b061c500b1d0171e94
Brian Faust <hello@basecode.sh>
2021-04-21 16:23:01 +0300

    wip

--- packages/platform-sdk-profiles/src/drivers/memory/profiles/profile.factory.ts
+++ packages/platform-sdk-profiles/src/drivers/memory/profiles/profile.factory.ts
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
 import { IProfile, IProfileFactory, ProfileSetting } from "../../../contracts";
 import { Profile } from "./profile";
+import { ProfileInitialiser } from "./profile.initialiser";
 
 export class ProfileFactory implements IProfileFactory {
 	public static fromName(name: string): IProfile {
 		const result: IProfile = new Profile({ id: uuidv4(), name, data: "" });
+
+		new ProfileInitialiser(result).reset(name);
 
 		result.save();
 
