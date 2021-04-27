a0dbee4fcebb7638c390d6a2f8dd15ba4133c746
Brian Faust <hello@basecode.sh>
2021-04-27 05:12:58 +0300

    chore(profiles): export container

--- packages/platform-sdk-profiles/src/index.ts
+++ packages/platform-sdk-profiles/src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 
 export * from "./environment/container.models";
+export * from "./environment/container";
 export * from "./environment/env.models";
 export * from "./environment/env";
 
