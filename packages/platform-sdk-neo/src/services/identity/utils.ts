2b7d20aa38ce19c1a165d0fc22b3725e02b6c12c
faustbrian <faustbrian@users.noreply.github.com>
2021-04-29 07:46:23 +0000

    style: resolve style guide violations

--- packages/platform-sdk-neo/src/services/identity/utils.ts
+++ packages/platform-sdk-neo/src/services/identity/utils.ts
@@ -4,10 +4,12 @@ import { wallet } from "@cityofzion/neon-js";
 export const createWallet = (input: string): any => new wallet.Account(input);
 
 export const deriveWallet = (mnemonic: string, coinType: number, account: number, change: number, index: number) => {
-	const privateKey: string = BIP44.deriveChild(mnemonic, { coinType, account, change, index }).privateKey!.toString("hex");
+	const privateKey: string = BIP44.deriveChild(mnemonic, { coinType, account, change, index }).privateKey!.toString(
+		"hex",
+	);
 
 	return createWallet(privateKey);
-}
+};
 
 export const deriveKeyPair = (input: string) => {
 	const { publicKey, privateKey } = createWallet(input);
--- packages/platform-sdk-neo/test/fixtures/identity.ts
+++ packages/platform-sdk-neo/test/fixtures/identity.ts
@@ -4,7 +4,8 @@ export const identity = {
 	publicKey: "0312ff93f39cb02498304f1f3bfb4f730eb546f5ec80a21cb2421d05560a1f5fe3",
 	address: "APPJtAkysCKBssD5EJzEpakntNk81nR7X2",
 	wif: "KzSutJwb6Rwmqx7aYuABXHedpwBW65eCZAgqpdeRunzPfqZPSstA",
-	mnemonic: "traffic fiscal note ahead eight morning answer pelican kitchen tired burst already report property wave world april person input piano laptop outdoor problem hurdle",
+	mnemonic:
+		"traffic fiscal note ahead eight morning answer pelican kitchen tired burst already report property wave world april person input piano laptop outdoor problem hurdle",
 	// Multi Signature
 	multiSignature: {
 		min: 3,
