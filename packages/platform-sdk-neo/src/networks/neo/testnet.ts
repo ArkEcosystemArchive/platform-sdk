a0baa643449c13004babc4059d72858cc4afacf0
Brian Faust <faustbrian@users.noreply.github.com>
2021-04-29 11:16:25 +0300

    chore(neo): update host lists (#1378)

--- packages/platform-sdk-neo/src/networks/neo/testnet.ts
+++ packages/platform-sdk-neo/src/networks/neo/testnet.ts
@@ -19,19 +19,11 @@ const network: Coins.CoinNetwork = {
 	},
 	networking: {
 		hosts: [
-			"https://test1.cityofzion.io:443",
-			"https://test2.cityofzion.io:443",
-			"https://test3.cityofzion.io:443",
+			"https://testnet1.neo2.coz.io:443",
+			"https://testnet2.neo2.coz.io:443",
+			"https://testnet3.neo2.coz.io:443",
 			"http://seed1.ngd.network:20332",
 			"http://seed2.ngd.network:20332",
-			"http://seed3.ngd.network:20332",
-			"http://seed4.ngd.network:20332",
-			"http://seed5.ngd.network:20332",
-			"http://seed6.ngd.network:20332",
-			"http://seed7.ngd.network:20332",
-			"http://seed8.ngd.network:20332",
-			"http://seed9.ngd.network:20332",
-			"http://seed10.ngd.network:20332",
 		],
 	},
 	governance: {
