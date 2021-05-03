fba45332828d7674a7b6f030b616e480b2623a61
faustbrian <faustbrian@users.noreply.github.com>
2021-05-03 06:24:06 +0000

    style: resolve style guide violations

--- packages/platform-sdk-profiles/src/drivers/driver.factory.test.ts
+++ packages/platform-sdk-profiles/src/drivers/driver.factory.test.ts
@@ -5,14 +5,26 @@ import { BTC } from "@arkecosystem/platform-sdk-btc";
 import { ETH } from "@arkecosystem/platform-sdk-eth";
 import { Request } from "@arkecosystem/platform-sdk-http-got";
 
+import { StubStorage } from "../../test/stubs/storage";
 import { container } from "../environment/container";
 import { DriverFactory } from "./driver.factory";
-import { StubStorage } from "../../test/stubs/storage";
 
 it("should create a new driver instance", () => {
-	expect(() => DriverFactory.make("memory", container, { coins: { ARK, BTC, ETH }, httpClient: new Request(), storage: new StubStorage() })).not.toThrow();
+	expect(() =>
+		DriverFactory.make("memory", container, {
+			coins: { ARK, BTC, ETH },
+			httpClient: new Request(),
+			storage: new StubStorage(),
+		}),
+	).not.toThrow();
 });
 
 it("should fail to create a new driver instance", () => {
-	expect(() => DriverFactory.make("404", container, { coins: { ARK, BTC, ETH }, httpClient: new Request(), storage: new StubStorage() })).toThrow();
+	expect(() =>
+		DriverFactory.make("404", container, {
+			coins: { ARK, BTC, ETH },
+			httpClient: new Request(),
+			storage: new StubStorage(),
+		}),
+	).toThrow();
 });
