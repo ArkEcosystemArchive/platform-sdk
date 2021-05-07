8ec4202c12f177095caac75ff693289a3aea700b
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-07 13:07:51 -0300

    working

--- packages/platform-sdk-examples/src/examples/open-existing-profile.ts
+++ packages/platform-sdk-examples/src/examples/open-existing-profile.ts
@@ -10,20 +10,18 @@ export default async () => {
 	if (!profile) {
 		throw Error("Profile doesn't exist");
 	}
-	profile.auth().verifyPassword("my-password");
 
+	profile.password().set("my-password");
 	await env.profiles().restore(profile, "my-password");
 	await profile.sync();
 
+	// Display profile and wallet balances
 	logger.log("Profile balance", profile.balance().toHuman(2));
-
-	// Display profile balance
-	for(const wallet of Object.values(await profile.wallets().all())) {
-		logger.log("Wallet balance", wallet.balance());
+	for (const wallet of Object.values(await profile.wallets().all())) {
+		logger.log("Wallet", wallet.address(), "balance", wallet.balance().toHuman(2));
 	}
 
-	for(const contact of Object.values(await profile.contacts().all())) {
+	for (const contact of Object.values(await profile.contacts().all())) {
 		logger.log("Contact", contact.name());
 	}
-
 };
