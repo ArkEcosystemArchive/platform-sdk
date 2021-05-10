57cf899cbb81385770054f96da9297986e44932e
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-10 10:45:45 -0300

    Transfer

--- packages/platform-sdk-examples/src/examples/transfer.ts
+++ packages/platform-sdk-examples/src/examples/transfer.ts
@@ -5,39 +5,53 @@ export default async () => {
 	const logger = useLogger();
 	const env: Environment = await useEnvironment();
 
-	const mnemonic1: string = "glare arrange have frame family border time bronze weekend moon gesture often";
-	const destinationAddress: string = "DJSB9R56X5c1Wxu9Be6V4g64vWWFFLGgxn";
-
 	// Open profile
-	const profile: Contracts.IProfile | undefined = env.profiles().create("my-test-profile-name");
+	const profile: Contracts.IProfile | undefined = env.profiles().findByName("my-profile-name");
 	if (!profile) {
 		throw Error("Profile doesn't exist");
 	}
 
 	// Restore it and sync
-	// await env.profiles().restore(profile, "my-password");
+	await env.profiles().restore(profile, "my-password");
 	await profile.sync();
 
-	// Create read-write wallet #1
-	const wallet1 = await profile.walletFactory().fromMnemonic({
-		mnemonic: mnemonic1,
-		coin: "ARK",
-		network: "ark.devnet"
-	});
-	await profile.wallets().push(wallet1);
+	// Get read-write wallet #1
+	const wallet: Contracts.IReadWriteWallet = await profile.wallets().first();
+	const mnemonic1: string = "super secure password";
 
 	// Display profile and wallet balances
-	logger.log("Wallet", wallet1.address(), "balance", wallet1.balance().toHuman(2));
+	logger.log("Wallet", wallet.address(), "balance", wallet.balance().toHuman(2));
 
-	const txId = await wallet1.transaction().signTransfer({
-		from: wallet1.address(),
-		sign: {
-			mnemonic: mnemonic1,
-		},
-		data: {
-			amount: "10000000",
-			to: destinationAddress,
+	// Get contact #1
+	const gimli: Contracts.IContact = await profile.contacts().first();
+	const contactAddress: Contracts.IContactAddress = await gimli.addresses().first();
+	const destinationAddress: string = contactAddress.address();
+	logger.log("Gimli's Wallet", destinationAddress);
+
+	const transactionId = await wallet
+		.transaction()
+		.signTransfer({
+			from: wallet.address(),
+			sign: {
+				mnemonic: mnemonic1
+			},
+			data: {
+				amount: "100000000",
+				to: destinationAddress
+			}
+		});
+	logger.log("signedTransactionData", transactionId);
+
+	await wallet.transaction().broadcast(transactionId);
+
+	logger.info(`Transaction [${transactionId}] is awaiting confirmation.`);
+	let awaitingConfirmation = true;
+	while (awaitingConfirmation) {
+		try {
+			awaitingConfirmation = await wallet.transaction().confirm(transactionId);
+		} catch {
+			awaitingConfirmation = false;
 		}
-	});
-	logger.log("txId");
+	}
+	logger.info(`Transaction [${transactionId}] is confirmed.`);
 };
