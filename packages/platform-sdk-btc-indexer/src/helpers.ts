efb68927e70ccd5f1bf110c091e5dd39fc81ce57
Brian Faust <hello@basecode.sh>
2021-05-10 16:26:04 +0300

    wip

--- packages/platform-sdk-btc-indexer/src/helpers.ts
+++ packages/platform-sdk-btc-indexer/src/helpers.ts
@@ -1,6 +1,6 @@
 import { Client } from "./client";
 import { Database } from "./database";
-import Logger from "./logger";
+import { Logger } from "./logger";
 import { Flags } from "./types";
 
 /**
