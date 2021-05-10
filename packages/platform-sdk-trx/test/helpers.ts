bef9a15c907fcaf89b349ff04d356c41d14bc313
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-10 10:30:01 +0300

    refactor(tron): use standard API to list transactions (#1442)

--- packages/platform-sdk-trx/test/helpers.ts
+++ packages/platform-sdk-trx/test/helpers.ts
@@ -1,10 +1,14 @@
 import { Coins } from "@arkecosystem/platform-sdk";
+import { Request } from "@arkecosystem/platform-sdk-http-got";
 
 import { manifest } from "../src/manifest";
 import { schema } from "../src/schema";
 
 export const createConfig = (options?: object) => {
-	const config = new Coins.Config(options || { network: "trx.testnet" }, schema);
+	const config = new Coins.Config({
+		...(options || { network: "trx.testnet" }),
+		...{ httpClient: new Request() },
+	}, schema);
 
 	config.set(Coins.ConfigKey.Network, manifest.networks["trx.testnet"]);
 
