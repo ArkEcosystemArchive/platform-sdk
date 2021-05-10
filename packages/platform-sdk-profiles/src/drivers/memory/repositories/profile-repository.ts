7b1ec5605bc20f041cabbc0f45f52fe42b36e53a
goga-m <goga.mamoulasvili@gmail.com>
2021-05-10 17:48:40 +0300

    fix: save profile with initial data upon profile creation

--- packages/platform-sdk-profiles/src/drivers/memory/repositories/profile-repository.ts
+++ packages/platform-sdk-profiles/src/drivers/memory/repositories/profile-repository.ts
@@ -80,6 +80,7 @@ export class ProfileRepository implements IProfileRepository {
 		this.push(result);
 
 		new ProfileInitialiser(result).initialise(name);
+		result.getAttributes().set("data", new ProfileExporter(result).export());
 
 		return result;
 	}
