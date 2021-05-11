ddb1263f086012ff759c3f96557ccad54b42e19e
Brian Faust <hello@basecode.sh>
2021-05-11 08:01:25 +0300

    wip

--- packages/platform-sdk-atom/src/dto/wallet.ts
+++ packages/platform-sdk-atom/src/dto/wallet.ts
@@ -11,7 +11,7 @@ export class WalletData extends DTO.AbstractWalletData implements Contracts.Wall
 	}
 
 	public publicKey(): string | undefined {
-		return this.data.publicKey.value;
+		return this.data.publicKey;
 	}
 
 	public balance(): BigNumber {
