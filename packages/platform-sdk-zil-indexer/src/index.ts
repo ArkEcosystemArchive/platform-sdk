703c51b78e2d393699aaea35ec5e9bc26f048ba6
Axel DELAMARRE <1091900+axeldelamarre@users.noreply.github.com>
2021-05-11 15:23:54 +0200

    feat(zil-indexer): basic implementation

---
+++ packages/platform-sdk-zil-indexer/src/index.ts
@@ -0,0 +1,88 @@
+import retry from "p-retry";
+import { BN } from "@zilliqa-js/zilliqa";
+import { TransactionObj } from "@zilliqa-js/core";
+
+import { useClient, useDatabase, useLogger, useQueue } from "./helpers";
+
+/**
+ * Launch the indexer and subscribe to updates for new data.
+ *
+ * @param {{
+ * 	network: string;
+ * 	host: string;
+ * 	database: string;
+ * }} flags
+ * @returns {Promise<void>}
+ */
+export const subscribe = async (flags: {
+	network: string;
+	host: string;
+	database: string;
+}): Promise<void> => {
+	const logger = useLogger();
+	const queue = useQueue();
+	const database = useDatabase(flags, logger);
+	const zilliqa = useClient(flags.host);
+
+	// Get the last block we stored in the database and grab the latest block
+	// on the network so that we can sync the missing blocks to complete our
+	// copy of the blockchain to avoid holes in the historical data of users.
+	const { result, error } = await zilliqa.blockchain.getNumTxBlocks();
+	if (!result || error) {
+		logger.error(error || "Failed to fetch remote block height");
+		process.exit();
+	}
+
+	const [localHeight, remoteHeight] = [database.lastBlockNumber(), new BN(result).toNumber() - 1];
+
+	for (let height = localHeight; height <= remoteHeight; height++) {
+		try {
+			if (queue.size === 1000) {
+				logger.info("Draining Queue...");
+
+				await queue.start().onIdle();
+				queue.pause();
+
+				logger.info("Drained Queue...");
+			}
+
+			queue.add(() =>
+				retry(
+					async () => {
+
+						const { result, error } = await zilliqa.blockchain.getTxBlock(height);
+						if (!result || error) {
+							logger.error(`Failed to fetch block at height ${height}: ${error}`);
+							logger.error(error);
+							process.exit();
+						}
+
+						let transactions: TransactionObj[] = [];
+						if (result.header.NumTxns > 0) {
+							const { result: txResult, error: txError } = await zilliqa.blockchain.getTxnBodiesForTxBlock(height);
+							if (!txResult || txError) {
+								logger.error(`Failed to fetch transactions at height ${height}`);
+								logger.error(txError);
+								process.exit();
+							}
+							transactions = txResult;
+						} 
+
+						database.storeBlockWithTransactions(result, transactions);
+					},
+					{
+						onFailedAttempt: (error) => {
+							logger.error(
+								`Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`,
+							);
+						},
+						retries: 5,
+					},
+				),
+			);
+		} catch (error) {
+			logger.error(error);
+			process.exit();
+		}
+	}
+};
