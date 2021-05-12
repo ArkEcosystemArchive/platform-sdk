13194db20c262486a89d103e036ae6cadec29e31
Brian Faust <hello@basecode.sh>
2021-05-12 17:35:11 +0300

    wip

--- packages/platform-sdk-trx/test/identity.ts
+++ packages/platform-sdk-trx/test/identity.ts
@@ -1,7 +1,7 @@
 export const identity = {
 	// Standard
-	privateKey: "",
-	publicKey: "",
+	privateKey: "990156e4859ff56f433306d71b858b222372dea7c6b7af64f487059bcc97e159",
+	publicKey: "0277a3fb802f02a0fc916370c1fe14355db6cc91d6355ac600e2039a267a7e1b3c",
 	address: "TAq9SwPACv2Ut6YGJK4T8Pw57AGNmFArdP",
 	wif: "",
 	mnemonic: "make kind spread stomach trend wish about sunny nuclear yard drama afraid cycle clay squirrel",
