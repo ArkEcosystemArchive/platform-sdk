0194ed42c065283d4c4e96b29ab4fc349b5821ba
Brian Faust <hello@basecode.sh>
2021-05-12 11:34:58 +0300

    wip

--- packages/platform-sdk-xrp/src/services/identity/wif.ts
+++ packages/platform-sdk-xrp/src/services/identity/wif.ts
@@ -1,8 +1,19 @@
-import { Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import { BIP32 } from "@arkecosystem/platform-sdk-crypto";
 
 export class WIF implements Contracts.WIF {
+	readonly #config: Coins.Config;
+
+	public constructor(config: Coins.Config) {
+		this.#config = config;
+	}
+
 	public async fromMnemonic(mnemonic: string, options?: Contracts.IdentityOptions): Promise<string> {
-		throw new Exceptions.NotSupported(this.constructor.name, "fromMnemonic");
+		try {
+			return BIP32.fromMnemonic(mnemonic).toWIF();
+		} catch (error) {
+			throw new Exceptions.CryptoException(error);
+		}
 	}
 
 	public async fromPrivateKey(privateKey: string): Promise<string> {
