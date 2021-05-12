31ad84ab3c5c6067e7f27d0f5b8e7a9926fa06b5
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-12 16:52:38 -0300

    use mnemonic for signing transaction, not private key

--- packages/platform-sdk-trx/src/services/transaction.test.ts
+++ packages/platform-sdk-trx/src/services/transaction.test.ts
@@ -16,13 +16,14 @@ describe("TransactionService", function () {
 	describe("#transfer", () => {
 		it("should succeed", async () => {
 			nock("https://api.shasta.trongrid.io")
-				.post("/wallet/createtransaction")
+				.post('/wallet/createtransaction', {"to_address":"41f2a1a2377b09618bf68a5cc06651cc01ee829833","owner_address":"4139c3b797ef0f0ef03d7247342d2698d039549532","amount":1})
 				.reply(200, require(`${__dirname}/../../test/fixtures/crypto/transfer.json`));
+			// nock.recorder.rec();
 
 			const result = await subject.transfer({
 				from: testWallet.address,
 				sign: {
-					mnemonic: testWallet.privateKey,
+					mnemonic: testWallet.mnemonic,
 				},
 				data: {
 					to: "TY689z7Q2NpZYBxGfXbYR4PmS2WXyTNrir",
@@ -30,6 +31,7 @@ describe("TransactionService", function () {
 				},
 			});
 
+			// nock.restore();
 			expect(result).toBeObject();
 		});
 	});
