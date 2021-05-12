04289fdc3568312e07df3ea52c50092125e48349
Brian Faust <hello@basecode.sh>
2021-05-12 16:48:33 +0300

    wip

---
+++ packages/platform-sdk-trx/src/services/identity/address.test.ts
@@ -0,0 +1,19 @@
+import "jest-extended";
+
+import { identity } from "../../../test/identity";
+import { createConfig } from "../../../test/helpers";
+import { Address } from "./address";
+
+let subject: Address;
+
+beforeEach(async () => {
+	subject = new Address(createConfig());
+});
+
+describe("Address", () => {
+	describe("#fromMnemonic", () => {
+		it("should generate an output from a mnemonic", async () => {
+			await expect(subject.fromMnemonic(identity.mnemonic)).resolves.toBe("D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib");
+		});
+	});
+});
---
+++ packages/platform-sdk-trx/test/identity.ts
@@ -0,0 +1,15 @@
+export const identity = {
+	// Standard
+	privateKey: "",
+	publicKey: "",
+	address: "",
+	wif: "",
+	mnemonic: "make kind spread stomach trend wish about sunny nuclear yard drama afraid cycle clay squirrel",
+	// Multi Signature
+	multiSignature: {
+		min: 3,
+		publicKeys: [],
+	},
+	multiSignatureAddress: "",
+	multiSignaturePublicKey: "",
+};
