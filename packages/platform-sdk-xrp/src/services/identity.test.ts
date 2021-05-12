ee014d46e89895871e7cb30293ae0871a23be6ea
Brian Faust <hello@basecode.sh>
2021-05-12 11:21:58 +0300

    wip

--- packages/platform-sdk-xrp/src/services/identity.test.ts
+++ packages/platform-sdk-xrp/src/services/identity.test.ts
@@ -10,22 +10,22 @@ beforeEach(async () => (subject = await IdentityService.__construct(createConfig
 
 describe("IdentityService", () => {
 	describe("#address", () => {
-		it("should generate an output from a mnemonic", async () => {
-			const result: any = await subject.address().fromMnemonic(identity.mnemonic);
+		it("should generate an output from a publicKey", async () => {
+			const result: any = await subject.address().fromPublicKey(identity.publicKey);
 
 			expect(result).toBe(identity.address);
 		});
 
-		it("should generate an output from a publicKey", async () => {
-			const result: any = await subject.address().fromPublicKey(identity.publicKey);
+		it("should generate an output from a secret", async () => {
+			const result: any = await subject.address().fromSecret(identity.mnemonic);
 
 			expect(result).toBe(identity.address);
 		});
 	});
 
 	describe("#keys", () => {
-		it("should generate an output from a mnemonic", async () => {
-			const result: any = await subject.keys().fromMnemonic(identity.mnemonic);
+		it("should generate an output from a secret", async () => {
+			const result: any = await subject.keys().fromSecret(identity.mnemonic);
 
 			expect(result).toEqual({
 				privateKey: identity.privateKey,
