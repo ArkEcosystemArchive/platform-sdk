5bf98d58caaae0e34ae53cc961756e4078e612e6
Brian Faust <hello@basecode.sh>
2021-05-12 11:32:31 +0300

    wip

--- packages/platform-sdk-xrp/src/services/identity/public-key.ts
+++ packages/platform-sdk-xrp/src/services/identity/public-key.ts
@@ -1,9 +1,19 @@
-import { Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import { BIP44 } from "@arkecosystem/platform-sdk-crypto";
 import { deriveKeypair } from "ripple-keypairs";
 
 export class PublicKey implements Contracts.PublicKey {
+	readonly #config: Coins.Config;
+
+	public constructor(config: Coins.Config) {
+		this.#config = config;
+	}
+
 	public async fromMnemonic(mnemonic: string, options?: Contracts.IdentityOptions): Promise<string> {
-		throw new Exceptions.NotSupported(this.constructor.name, "fromMnemonic");
+		return BIP44.deriveChild(mnemonic, {
+			coinType: this.#config.get(Coins.ConfigKey.Slip44),
+			index: options?.bip44?.addressIndex,
+		}).publicKey.toString("hex");
 	}
 
 	public async fromMultiSignature(min: number, publicKeys: string[]): Promise<string> {
