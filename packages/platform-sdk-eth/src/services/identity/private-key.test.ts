b36f156423037c031b3e6f122cdb2ea5b901f0fc
Axel DELAMARRE <1091900+axeldelamarre@users.noreply.github.com>
2021-05-12 14:32:57 +0200

    test(identity): wip

---
+++ packages/platform-sdk-eth/src/services/identity/private-key.test.ts
@@ -0,0 +1,21 @@
+import "jest-extended";
+
+import { identity } from "../../../test/fixtures/identity";
+import { createConfig } from "../../../test/helpers";
+import { IdentityService } from ".";
+
+let subject: IdentityService;
+
+beforeEach(async () => (subject = await IdentityService.__construct(createConfig())));
+
+describe("#privateKey", () => {
+	it("should generate an output from a mnemonic", async () => {
+		const result: any = await subject.privateKey().fromMnemonic(identity.mnemonic);
+
+		expect(result).toBe(identity.privateKey);
+	});
+
+	it("should generate an output from a wif", async () => {
+		await expect(subject.privateKey().fromWIF(identity.wif)).rejects.toThrow(/is not supported/);
+	});
+});
---
+++ packages/platform-sdk-eth/src/services/identity/public-key.test.ts
@@ -0,0 +1,27 @@
+import "jest-extended";
+
+import { identity } from "../../../test/fixtures/identity";
+import { createConfig } from "../../../test/helpers";
+import { IdentityService } from ".";
+
+let subject: IdentityService;
+
+beforeEach(async () => (subject = await IdentityService.__construct(createConfig())));
+
+describe("#publicKey", () => {
+	it("should generate an output from a mnemonic", async () => {
+		const result: any = await subject.publicKey().fromMnemonic(identity.mnemonic);
+
+		expect(result).toBe(identity.publicKey);
+	});
+
+	it("should generate an output from a multiSignature", async () => {
+		await expect(
+			subject.publicKey().fromMultiSignature(identity.multiSignature.min, identity.multiSignature.publicKeys),
+		).rejects.toThrow(/is not supported/);
+	});
+
+	it("should generate an output from a wif", async () => {
+		await expect(subject.publicKey().fromWIF(identity.wif)).rejects.toThrow(/is not supported/);
+	});
+});
