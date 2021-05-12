15bdc11846a0bca5b9199fe9b78063452e0c32f6
Axel DELAMARRE <1091900+axeldelamarre@users.noreply.github.com>
2021-05-12 16:16:38 +0200

    refactor(identity): split zil tests

---
+++ packages/platform-sdk-zil/src/services/identity/keys.test.ts
@@ -0,0 +1,38 @@
+import "jest-extended";
+
+import { identity } from "../../../test/fixtures/identity";
+import { createConfig } from "../../../test/config";
+import { getZilliqa } from "../../zilliqa";
+import { Keys } from "./keys";
+
+let subject: Keys;
+
+beforeEach(async () => (subject = new Keys(getZilliqa(createConfig()).wallet)));
+
+describe("#keys", () => {
+	it("should generate an output from a mnemonic", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic);
+
+		expect(result).toEqual({
+			privateKey: identity.privateKey,
+			publicKey: identity.publicKey,
+		});
+	});
+
+	it("should fail from an invalid mnemonic", async () => {
+		await expect(subject.fromMnemonic(identity.mnemonic.slice(0, 10))).rejects.toThrowError();
+	});
+
+	it("should generate an output from a privateKey", async () => {
+		const result: any = await subject.fromPrivateKey(identity.privateKey);
+
+		expect(result).toEqual({
+			privateKey: identity.privateKey,
+			publicKey: identity.publicKey,
+		});
+	});
+
+	it("should fail to generate an output from a wif", async () => {
+		await expect(subject.fromWIF(identity.wif)).rejects.toThrow(/is not supported/);
+	});
+});
