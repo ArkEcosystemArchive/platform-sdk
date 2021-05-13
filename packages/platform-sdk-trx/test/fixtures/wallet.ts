6fea814736ef38be6c8624a49fbf9367b2a88403
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-13 09:55:45 -0300

    undo

--- packages/platform-sdk-trx/test/fixtures/wallet.ts
+++ packages/platform-sdk-trx/test/fixtures/wallet.ts
@@ -1,6 +1,5 @@
 // Generated with https://tronpaperwallet.org/ and https://www.trongrid.io/faucet
 export const testWallet = {
-	address: "TFEdyfF1pqXn2XuqYwJbeLzwGrwwTUufLx",
-	mnemonic: "cabin fold parrot grunt tide exact spoon regular wait mercy very fame",
-	privateKey: "35b7c7af85e87e613c8ba3524089a2ed7350179ca77de4009dae8976f429a8df",
+	address: "TEre3kN6JdPzqCNpiZT8JWM4kt8iGrg1Rm",
+	privateKey: "DDB439227D483FD01448078AC44839EC733E02589C6808F5BF368828A3599196",
 };
