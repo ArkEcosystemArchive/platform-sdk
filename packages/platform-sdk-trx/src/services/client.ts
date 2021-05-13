e4238b778455cd1e115829a487c9ba2ff47123de
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-13 07:14:20 +0300

    Restore client.ts

--- packages/platform-sdk-trx/src/services/client.ts
+++ packages/platform-sdk-trx/src/services/client.ts
@@ -126,7 +126,7 @@ export class ClientService implements Contracts.ClientService {
 			rejected: [],
 			errors: {},
 		};
-		console.log(transactions)
+
 		for (const transaction of transactions) {
 			const response = (await this.#client.post(`${this.getHost()}/wallet/broadcasttransaction`, transaction.toBroadcast())).json();
 
