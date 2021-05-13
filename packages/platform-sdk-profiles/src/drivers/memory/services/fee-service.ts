b40e8a775d2f36cedc604e35b556e5a1ca2f7eec
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-13 11:12:22 +0300

    Restore fee-service.ts

--- packages/platform-sdk-profiles/src/drivers/memory/services/fee-service.ts
+++ packages/platform-sdk-profiles/src/drivers/memory/services/fee-service.ts
@@ -10,12 +10,12 @@ export class FeeService implements IFeeService {
 	readonly #dataRepository: DataRepository = new DataRepository();
 
 	/** {@inheritDoc IFeeService.all} */
-	public all(coin: Coins.Coin): Contracts.TransactionFees {
-		const result: Contracts.TransactionFees | undefined = this.#dataRepository.get(coin.uuid());
+	public all(coin: string, network: string): Contracts.TransactionFees {
+		const result: Contracts.TransactionFees | undefined = this.#dataRepository.get(`${coin}.${network}.fees`);
 
 		if (result === undefined) {
 			throw new Error(
-				`The fees for [${coin.uuid()}] have not been synchronized yet. Please call [syncFees] before using this method.`,
+				`The fees for [${coin}.${network}] have not been synchronized yet. Please call [syncFees] before using this method.`,
 			);
 		}
 
@@ -23,13 +23,21 @@ export class FeeService implements IFeeService {
 	}
 
 	/** {@inheritDoc IFeeService.findByType} */
-	public findByType(coin: Coins.Coin, type: string): Contracts.TransactionFee {
-		return this.all(coin)[type];
+	public findByType(coin: string, network: string, type: string): Contracts.TransactionFee {
+		return this.all(coin, network)[type];
 	}
 
 	/** {@inheritDoc IFeeService.sync} */
-	public async sync(coin: Coins.Coin): Promise<void> {
-		this.#dataRepository.set(coin.uuid(), await coin.fee().all());
+	public async sync(profile: IProfile, coin: string, network: string): Promise<void> {
+		const instance: Coins.Coin = profile.coins().push(coin, network);
+
+		// @TODO: remove this in a refactor
+		/* istanbul ignore next */
+		if (!instance.hasBeenSynchronized()) {
+			await instance.__construct();
+		}
+
+		this.#dataRepository.set(`${coin}.${network}.fees`, await instance.fee().all());
 	}
 
 	/** {@inheritDoc IFeeService.syncAll} */
@@ -38,7 +46,7 @@ export class FeeService implements IFeeService {
 
 		for (const [coin, networks] of profile.coins().entries()) {
 			for (const network of networks) {
-				promises.push(() => this.sync(profile.coins().get(coin, network)));
+				promises.push(() => this.sync(profile, coin, network));
 			}
 		}
 
