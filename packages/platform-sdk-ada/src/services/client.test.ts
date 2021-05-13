3b5698bd20f6336bdf8755d9b6029c7ca29bf278
Brian Faust <hello@basecode.sh>
2021-05-13 08:16:12 +0300

    wip

--- packages/platform-sdk-ada/src/services/client.test.ts
+++ packages/platform-sdk-ada/src/services/client.test.ts
@@ -58,10 +58,12 @@ describe("ClientService", function () {
 			expect(result.items()[0]).toBeInstanceOf(TransactionData);
 		});
 		it("missing senderPublicKey", async () => {
-			await expect(subject.transactions({
-				walletId:
-					"aec30330deaecdd7503195a0d730256faef87027022b1bdda7ca0a61bca0a55e4d575af5a93bdf4905a3702fadedf451ea584791d233ade90965d608bac57304",
-			})).rejects.toThrow(
+			await expect(
+				subject.transactions({
+					walletId:
+						"aec30330deaecdd7503195a0d730256faef87027022b1bdda7ca0a61bca0a55e4d575af5a93bdf4905a3702fadedf451ea584791d233ade90965d608bac57304",
+				}),
+			).rejects.toThrow(
 				"Method ClientService#transactions expects the argument [senderPublicKey] but it was not given",
 			);
 		});
