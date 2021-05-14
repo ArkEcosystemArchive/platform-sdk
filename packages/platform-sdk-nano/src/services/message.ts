4284717070e7c013388ce514a997eb11f21b02a8
Brian Faust <hello@basecode.sh>
2021-05-14 10:00:41 +0300

    wip

--- packages/platform-sdk-nano/src/services/message.ts
+++ packages/platform-sdk-nano/src/services/message.ts
@@ -1,8 +1,18 @@
 import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
+import { BIP39 } from "@arkecosystem/platform-sdk-crypto";
+import { tools } from "nanocurrency-web"
+
+import { IdentityService } from "./identity";
 
 export class MessageService implements Contracts.MessageService {
+	readonly #identityService: IdentityService;
+
+	private constructor(identityService: IdentityService) {
+		this.#identityService = identityService;
+	}
+
 	public static async __construct(config: Coins.Config): Promise<MessageService> {
-		return new MessageService();
+		return new MessageService(await IdentityService.__construct(config));
 	}
 
 	public async __destruct(): Promise<void> {
@@ -10,14 +20,38 @@ export class MessageService implements Contracts.MessageService {
 	}
 
 	public async sign(input: Contracts.MessageInput): Promise<Contracts.SignedMessage> {
-		if (input.mnemonic === undefined) {
-			throw new Exceptions.MissingArgument(this.constructor.name, "sign", "mnemonic");
-		}
+		try {
+			let keys: Contracts.KeyPair | undefined;
 
-		throw new Exceptions.NotImplemented(this.constructor.name, "sign");
+			if (input.mnemonic) {
+				keys = await this.#identityService.keys().fromMnemonic(BIP39.normalize(input.mnemonic));
+			}
+
+			if (!keys) {
+				throw new Error("Failed to retrieve the keys for the signatory wallet.");
+			}
+
+			const { publicKey, privateKey } = keys;
+
+			if (privateKey === undefined) {
+				throw new Error("Failed to retrieve the private key for the signatory wallet.");
+			}
+
+			return {
+				message: input.message,
+				signatory: publicKey,
+				signature: tools.sign(privateKey, input.message),
+			};
+		} catch (error) {
+			throw new Exceptions.CryptoException(error);
+		}
 	}
 
 	public async verify(input: Contracts.SignedMessage): Promise<boolean> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "verify");
+		try {
+			return true;
+		} catch (error) {
+			throw new Exceptions.CryptoException(error);
+		}
 	}
 }
