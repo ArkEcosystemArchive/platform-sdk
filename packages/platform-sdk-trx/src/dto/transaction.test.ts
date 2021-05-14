f7cee42ccf36976ec2f8567018177d61f61096b9
marianogoldman <marianogoldman@users.noreply.github.com>
2021-05-14 16:21:58 +0000

    style: resolve style guide violations

--- packages/platform-sdk-trx/src/dto/transaction.test.ts
+++ packages/platform-sdk-trx/src/dto/transaction.test.ts
@@ -1,18 +1,20 @@
 import "jest-extended";
+
 import nock from "nock";
-import { TransactionData } from "../dto";
+
+import { TransactionData } from ".";
 
 let subject: TransactionData;
 
 beforeAll(() => nock.disableNetConnect());
 
 describe("transaction", function () {
-	it("should parse memo correctly", function() {
+	it("should parse memo correctly", function () {
 		subject = new TransactionData(require(`${__dirname}/../../test/fixtures/client/transactions.json`).data[1]);
 		expect(subject.memo()).toBe("Mariano");
-	})
-	it("should parse missing memo correctly", function() {
+	});
+	it("should parse missing memo correctly", function () {
 		subject = new TransactionData(require(`${__dirname}/../../test/fixtures/client/transactions.json`).data[0]);
 		expect(subject.memo()).toBeUndefined();
-	})
+	});
 });
