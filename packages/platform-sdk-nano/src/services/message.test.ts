9701fa5da69189a82b9b1bb0249f95925b591580
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-14 11:15:26 +0300

    feat(nano): implement `MessageService` (#1509)

---
+++ packages/platform-sdk-nano/src/services/message.test.ts
@@ -0,0 +1,22 @@
+import "jest-extended";
+
+import { identity } from "../../test/fixtures/identity";
+import { createConfig } from "../../test/helpers";
+import { MessageService } from "./message";
+
+let subject: MessageService;
+
+beforeEach(async () => (subject = await MessageService.__construct(createConfig())));
+
+describe("MessageService", () => {
+	it("should sign and verify a message", async () => {
+		const result = await subject.sign({
+			message: "Hello World",
+			mnemonic: identity.mnemonic,
+		});
+
+		console.log(result);
+
+		await expect(subject.verify(result)).resolves.toBeTrue();
+	});
+});
