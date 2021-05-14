b531783e7dfc45b34246ad20bcdfce2d1bee8567
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-14 11:50:51 +0300

    feat(nano): implement `MessageService#verify` (#1510)

--- packages/platform-sdk-nano/src/networks/nano/mainnet.ts
+++ packages/platform-sdk-nano/src/networks/nano/mainnet.ts
@@ -46,6 +46,10 @@ const network: Coins.NetworkManifest = {
 			transaction: true,
 			wallet: true,
 		},
+		Message: {
+			sign: true,
+			verify: true,
+		},
 		Derivation: {
 			bip39: true,
 			bip44: true,
