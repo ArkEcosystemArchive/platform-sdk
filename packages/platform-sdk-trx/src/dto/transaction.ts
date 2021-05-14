dae2b3d97e48b90e5990340366796a7b5d9608fc
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-14 13:04:22 -0300

    fix memo extraction

--- packages/platform-sdk-trx/src/dto/transaction.ts
+++ packages/platform-sdk-trx/src/dto/transaction.ts
@@ -40,7 +40,7 @@ export class TransactionData extends DTO.AbstractTransactionData implements Cont
 	}
 
 	public memo(): string | undefined {
-		return this.data.data || undefined;
+		return Buffer.from(this.data.raw_data.data || "", "hex").toString() || undefined;
 	}
 
 	public asset(): Record<string, unknown> {
