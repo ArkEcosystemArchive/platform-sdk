1965882a066796d3b5a452d38dd185eb9be82b46
Axel DELAMARRE <1091900+axeldelamarre@users.noreply.github.com>
2021-05-14 08:59:01 +0200

    refactor(identity): split lsk tests

---
+++ packages/platform-sdk-lsk/src/services/identity/keys.test.ts
@@ -0,0 +1,24 @@
+import "jest-extended";
+
+import { identity } from "../../../test/fixtures/identity";
+import { createConfig } from "../../../test/helpers";
+import { IdentityService } from ".";
+
+let subject: IdentityService;
+
+beforeEach(async () => (subject = await IdentityService.__construct(createConfig())));
+
+describe("#keys", () => {
+	it("should generate an output from a mnemonic", async () => {
+		const result = await subject.keys().fromMnemonic(identity.mnemonic);
+
+		expect(result).toEqual({
+			privateKey: identity.privateKey,
+			publicKey: identity.publicKey,
+		});
+	});
+
+	it("should generate an output from a wif", async () => {
+		await expect(subject.keys().fromWIF(identity.wif)).rejects.toThrow(/is not supported/);
+	});
+});
