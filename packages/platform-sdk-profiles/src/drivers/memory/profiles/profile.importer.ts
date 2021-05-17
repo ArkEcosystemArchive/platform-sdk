429ceaae829e4a6cf30727d00e715beaf03bd33e
goga-m <goga.mamoulasvili@gmail.com>
2021-05-17 14:55:26 +0300

    refactor: use `IProfileValidator` in profile importer

--- packages/platform-sdk-profiles/src/drivers/memory/profiles/profile.importer.ts
+++ packages/platform-sdk-profiles/src/drivers/memory/profiles/profile.importer.ts
@@ -1,18 +1,18 @@
 import { Base64 } from "@arkecosystem/platform-sdk-crypto";
-import Joi from "joi";
 import { IProfileData, IProfile } from "../../../contracts";
 
-import { Migrator } from "./migrator";
-import { Identifiers } from "../../../environment/container.models";
-import { container } from "../../../environment/container";
 import { ProfileEncrypter } from "./profile.encrypter";
 import { IProfileImporter } from "../../../contracts/profiles/profile.importer";
+import { IProfileValidator } from "../../../contracts/profiles/profile.validator";
+import { ProfileValidator } from "./profile.validator";
 
 export class ProfileImporter implements IProfileImporter {
 	readonly #profile: IProfile;
+	readonly #validator: IProfileValidator;
 
 	public constructor(profile: IProfile) {
 		this.#profile = profile;
+		this.#validator = new ProfileValidator(profile);
 	}
 
 	/** {@inheritDoc IProfileImporter.import} */
@@ -78,71 +78,7 @@ export class ProfileImporter implements IProfileImporter {
 	 * @memberof Profile
 	 */
 	private async validate(data: IProfileData): Promise<IProfileData> {
-		if (container.has(Identifiers.MigrationSchemas) && container.has(Identifiers.MigrationVersion)) {
-			await new Migrator(this.#profile).migrate(
-				container.get(Identifiers.MigrationSchemas),
-				container.get(Identifiers.MigrationVersion),
-			);
-		}
-
-		const { error, value } = Joi.object({
-			id: Joi.string().required(),
-			contacts: Joi.object().pattern(
-				Joi.string().uuid(),
-				Joi.object({
-					id: Joi.string().required(),
-					name: Joi.string().required(),
-					addresses: Joi.array().items(
-						Joi.object({
-							id: Joi.string().required(),
-							coin: Joi.string().required(),
-							network: Joi.string().required(),
-							name: Joi.string().required(),
-							address: Joi.string().required(),
-						}),
-					),
-					starred: Joi.boolean().required(),
-				}),
-			),
-			// TODO: stricter validation to avoid unknown keys or values
-			data: Joi.object().required(),
-			// TODO: stricter validation to avoid unknown keys or values
-			notifications: Joi.object().required(),
-			// TODO: stricter validation to avoid unknown keys or values
-			peers: Joi.object().required(),
-			// TODO: stricter validation to avoid unknown keys or values
-			plugins: Joi.object().required(),
-			// TODO: stricter validation to avoid unknown keys or values
-			settings: Joi.object().required(),
-			wallets: Joi.object().pattern(
-				Joi.string().uuid(),
-				Joi.object({
-					id: Joi.string().required(),
-					coin: Joi.string().required(),
-					network: Joi.string().required(),
-					networkConfig: Joi.object({
-						crypto: Joi.object({
-							slip44: Joi.number().integer().required(),
-						}).required(),
-						networking: Joi.object({
-							hosts: Joi.array().items(Joi.string()).required(),
-							hostsMultiSignature: Joi.array().items(Joi.string()),
-							hostsArchival: Joi.array().items(Joi.string()),
-						}).required(),
-					}),
-					address: Joi.string().required(),
-					publicKey: Joi.string(),
-					data: Joi.object().required(),
-					settings: Joi.object().required(),
-				}),
-			),
-		}).validate(data);
-
-		if (error !== undefined) {
-			throw error;
-		}
-
-		return value as IProfileData;
+		return this.#validator.validate(data);
 	}
 
 	/**
