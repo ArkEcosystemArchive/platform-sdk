ac41a3d34a1fbd740bfbd1611de1bc5a70b68292
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-19 09:05:37 -0300

    fix signatories

--- examples/src/examples/transfer-trx.ts
+++ examples/src/examples/transfer-trx.ts
@@ -1,5 +1,6 @@
+import { Signatories } from "@arkecosystem/platform-sdk";
 import { Environment } from "@arkecosystem/platform-sdk-profiles";
-import { pollTransactionStatus, createProfile, useEnvironment, useLogger } from "../helpers";
+import { createProfile, pollTransactionStatus, useEnvironment, useLogger } from "../helpers";
 
 export default async () => {
 	const logger = useLogger();
@@ -38,10 +39,9 @@ export default async () => {
 	const transactionId = await wallet1
 		.transaction()
 		.signTransfer({
-			from: wallet1.address(),
-			sign: {
-				mnemonic: mnemonic1
-			},
+			signatory: new Signatories.Signatory(
+				new Signatories.MnemonicSignatory(mnemonic1, wallet1.address()),
+			),
 			data: {
 				amount: "200000000", // 2 TRX
 				to: address2,
