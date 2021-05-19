d05af04fec00902fa96aa4e2f1eca055f8792f6d
Axel DELAMARRE <1091900+axeldelamarre@users.noreply.github.com>
2021-05-19 17:25:39 +0200

    refactor(egld-messages): use another library for signing/verifying

--- packages/platform-sdk-egld/src/services/message.ts
+++ packages/platform-sdk-egld/src/services/message.ts
@@ -1,6 +1,6 @@
+import { getPublicKey, sign, verify } from "noble-ed25519";
 import { Coins, Contracts, Exceptions } from "@arkecosystem/platform-sdk";
-import { Mnemonic, UserSecretKey, UserSigner, ISignable, Address } from "@elrondnetwork/erdjs/out";
-import { Signature } from "@elrondnetwork/erdjs/out/signature";
+import { Mnemonic } from "@elrondnetwork/erdjs/out";
 
 export class MessageService implements Contracts.MessageService {
 	public static async __construct(config: Coins.Config): Promise<MessageService> {
@@ -15,42 +15,26 @@ export class MessageService implements Contracts.MessageService {
 		if (!input.mnemonic) {
 			throw new Exceptions.MissingArgument(MessageService.name, this.sign.name, "input.mnemonic");
 		}
-		const mnemonic: Mnemonic = Mnemonic.fromString(input.mnemonic);
-		const secretKey: UserSecretKey = mnemonic.deriveKey(0);
-		const signer: UserSigner = new UserSigner(secretKey);
 
-		const message = new SignableMessage(input.message);
-		await signer.sign(message);
+		try {
+			const mnemonic: Mnemonic = Mnemonic.fromString(input.mnemonic);
+			const privateKey = mnemonic.deriveKey(0).hex();
 
-		return {
-			message: input.message,
-			signatory: secretKey.generatePublicKey().hex(),
-			signature: message.getSignature(),
+			return {
+				message: input.message,
+				signatory: await getPublicKey(privateKey),
+				signature: await sign(Buffer.from(input.message, "utf8").toString("hex"), privateKey),
+			}
+		} catch (error) {
+			throw new Exceptions.CryptoException(error);
 		}
 	}
 
 	public async verify(input: Contracts.SignedMessage): Promise<boolean> {
-		throw new Exceptions.NotImplemented(this.constructor.name, "verify");
-	}
-}
-
-class SignableMessage implements ISignable {
-	#signature: string | undefined;
-
-	constructor(public message: string) {};
-
-	public serializeForSigning(signedBy: Address): Buffer {
-		return Buffer.from(this.message);
-	}
-
-	public applySignature(signature: Signature, signedBy: Address): void {
-		this.#signature = signature.hex();
-	}
-
-	public getSignature() {
-		if (!this.#signature) {
-			throw new Exceptions.BadStateException(this.getSignature.name, "Signature not computed");
+		try {
+			return verify(input.signature, Buffer.from(input.message, "utf8").toString("hex"), input.signatory);
+		} catch (error) {
+			throw new Exceptions.CryptoException(error);
 		}
-		return this.#signature;
 	}
 }
