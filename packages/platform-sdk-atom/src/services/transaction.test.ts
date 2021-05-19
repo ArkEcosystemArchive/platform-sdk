16016a64cc9b2285cf71b392b5840da16b9a34b0
Brian Faust <hello@basecode.sh>
2021-05-19 04:22:33 +0300

    wip

--- packages/platform-sdk-atom/src/services/transaction.test.ts
+++ packages/platform-sdk-atom/src/services/transaction.test.ts
@@ -1,3 +1,4 @@
+import { Signatories } from "@arkecosystem/platform-sdk";
 import "jest-extended";
 
 import nock from "nock";
@@ -21,10 +22,9 @@ describe("TransactionService", () => {
 				.reply(200, require(`${__dirname}/../../test/fixtures/client/wallet-balance.json`));
 
 			const result: any = await subject.transfer({
-				from: "cosmos1fvxjdyfdvat5g0ee7jmyemwl2n95ad7negf7ap",
-				sign: {
-					mnemonic: "this is a top secret passphrase",
-				},
+				signatory: new Signatories.Signatory(
+					new Signatories.MnemonicSignatory("this is a top secret passphrase", "cosmos1fvxjdyfdvat5g0ee7jmyemwl2n95ad7negf7ap"),
+				),
 				data: {
 					amount: "1",
 					to: "cosmos1fvxjdyfdvat5g0ee7jmyemwl2n95ad7negf7ap",
