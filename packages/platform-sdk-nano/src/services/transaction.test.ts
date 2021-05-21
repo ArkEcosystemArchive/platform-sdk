3d9832c90da95a432f1a75043c3cbfd86354da8e
Axel DELAMARRE <1091900+axeldelamarre@users.noreply.github.com>
2021-05-21 10:07:36 +0200

    test(nano): update signatory in tests

--- packages/platform-sdk-nano/src/services/transaction.test.ts
+++ packages/platform-sdk-nano/src/services/transaction.test.ts
@@ -2,6 +2,8 @@ import "jest-extended";
 
 import nock from "nock";
 
+import { Signatories } from "@arkecosystem/platform-sdk";
+
 import { identity } from "../../test/fixtures/identity";
 import { createConfig } from "../../test/helpers";
 import { SignedTransactionData } from "../dto/signed-transaction";
@@ -23,10 +25,14 @@ describe("TransactionService", () => {
 				.reply(200, require(`${__dirname}/../../test/fixtures/client/account-info.json`));
 
 			const result = await subject.transfer({
-				from: identity.address,
-				sign: {
-					mnemonic: identity.mnemonic,
-				},
+				signatory: new Signatories.Signatory(
+					new Signatories.MnemonicSignatory({
+						signingKey: identity.mnemonic,
+						address: identity.address,
+						publicKey: identity.publicKey,
+						privateKey: identity.privateKey,
+					}),
+				),
 				data: {
 					amount: "420.69",
 					to: identity.address,
