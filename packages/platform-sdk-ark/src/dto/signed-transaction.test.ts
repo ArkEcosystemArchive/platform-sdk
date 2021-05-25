334573424683b3320562e7fd2abd263e63fce92f
axeldelamarre <axeldelamarre@users.noreply.github.com>
2021-05-25 12:52:05 +0000

    style: resolve style guide violations

--- packages/platform-sdk-ark/src/dto/signed-transaction.test.ts
+++ packages/platform-sdk-ark/src/dto/signed-transaction.test.ts
@@ -1,9 +1,9 @@
 import "jest-extended";
 
+import { DateTime } from "@arkecosystem/platform-sdk-intl";
 import { BigNumber } from "@arkecosystem/platform-sdk-support";
 
 import { SignedTransactionData } from "./signed-transaction";
-import { DateTime } from "@arkecosystem/platform-sdk-intl";
 
 let subject: SignedTransactionData;
 
@@ -45,5 +45,5 @@ describe("SignedTransactionData", function () {
 
 	test("#timestamp", () => {
 		expect(subject.timestamp()).toEqual(DateTime.make(0));
-	})
+	});
 });
