48d431fecd645718921be39a36255f39a4376f58
Axel DELAMARRE <1091900+axeldelamarre@users.noreply.github.com>
2021-05-26 11:59:27 +0200

    fix(timestamps): change test fixture

--- packages/platform-sdk-trx/src/dto/signed-transaction.test.ts
+++ packages/platform-sdk-trx/src/dto/signed-transaction.test.ts
@@ -10,7 +10,9 @@ beforeEach(() => {
 	subject = new SignedTransactionData(
 		"3e3817fd0c35bc36674f3874c2953fa3e35877cbcdb44a08bdc6083dbd39d572",
 		{
-			timestamp: "1970-01-01T00:00:00.000Z",
+			raw_data: {
+				timestamp: "1970-01-01T00:00:00.000Z",
+			},
 		},
 		"",
 	);
