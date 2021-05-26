cc849afede9bfeb7dda702ff4955a53217f8f6e3
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-26 10:12:37 -0300

    More renames vIn => Input and vOut => output

--- packages/platform-sdk-btc-indexer/src/tx-parsing-helpers.ts
+++ packages/platform-sdk-btc-indexer/src/tx-parsing-helpers.ts
@@ -1,33 +1,33 @@
 import { Input, Output } from "./types";
 
 export const getAmount = (transaction): bigint =>
-	transaction.vout.reduce((c: bigint, v): bigint => {
-		return c + BigInt(Math.round(v.value * 1e8));
+	transaction.vout.reduce((carry: bigint, item): bigint => {
+		return carry + BigInt(Math.round(item.value * 1e8));
 	}, BigInt(0));
 
 export const getOutputs = (transaction): Output[] =>
 	transaction.vout
 		.sort((a, b) => a.n - b.n)
-		.map((vOut) => ({
-			idx: vOut.n,
-			addresses: vOut.scriptPubKey?.addresses,
-			amount: Math.round(vOut.value * 1e8),
+		.map((output) => ({
+			idx: output.n,
+			addresses: output.scriptPubKey?.addresses,
+			amount: Math.round(output.value * 1e8),
 		}));
 
 export const getInputs = (transaction): Input[] =>
 	transaction.vin
-		.filter((vin) => vin.txid !== undefined && vin.vout !== undefined)
-		.map((vin) => ({ txid: vin.txid, vout: vin.vout }));
+		.filter((input) => input.txid !== undefined && input.vout !== undefined)
+		.map((input) => ({ txid: input.txid, vout: input.vout }));
 
-export const getFees = (transaction, vouts): bigint => {
+export const getFees = (transaction, outputs): bigint => {
 	if (transaction.vin[0].coinbase) {
 		return BigInt(0);
 	}
 
 	const outputAmount: bigint = getAmount(transaction);
 	const inputAmount: bigint = transaction.vin
-		.filter((vin) => vin.txid !== undefined && vin.vout !== undefined)
-		.reduce((c: bigint, vin): bigint => c + BigInt(vouts[vin.txid + vin.vout]), BigInt(0));
+		.filter((input) => input.txid !== undefined && input.vout !== undefined)
+		.reduce((c: bigint, input): bigint => c + BigInt(outputs[input.txid + input.vout]), BigInt(0));
 
 	return BigInt(inputAmount) - BigInt(outputAmount);
 };
