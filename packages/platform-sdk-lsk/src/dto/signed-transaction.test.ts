23956724d43aa8a76e48a54c882275595024a4c8
axeldelamarre <axeldelamarre@users.noreply.github.com>
2021-05-26 09:39:20 +0000

    style: resolve style guide violations

--- packages/platform-sdk-lsk/src/dto/signed-transaction.test.ts
+++ packages/platform-sdk-lsk/src/dto/signed-transaction.test.ts
@@ -1,9 +1,9 @@
 import "jest-extended";
 
+import { DateTime } from "@arkecosystem/platform-sdk-intl";
 import { BigNumber } from "@arkecosystem/platform-sdk-support";
 
 import { SignedTransactionData } from "./signed-transaction";
-import { DateTime } from "@arkecosystem/platform-sdk-intl";
 
 let subject: SignedTransactionData;
 
