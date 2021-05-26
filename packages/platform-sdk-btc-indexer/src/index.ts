d06c30e06a1ef0a4fd79f494af8fde6508ed2be7
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-26 09:53:50 -0300

    Resolve the problem with parallel fetching and serial processing

--- packages/platform-sdk-btc-indexer/src/index.ts
+++ packages/platform-sdk-btc-indexer/src/index.ts
@@ -1,4 +1,5 @@
-import pEachSeries from "p-each-series";
+import PQueue from "p-queue";
+import pWaitFor from 'p-wait-for';
 
 import { useClient, useDatabase, useLogger } from "./helpers";
 import { Logger } from "./logger";
@@ -16,16 +17,33 @@ export const subscribe = async (flags: Flags): Promise<void> => {
 	const client = useClient(flags);
 	const step: number = flags.batchSize;
 
+	const fetchingQueue = new PQueue({concurrency: step});
+	const toBeProcessed: object = {};
+
 	// Get the last block we stored in the database and grab the latest block
 	// on the network so that we can sync the missing blocks to complete our
 	// copy of the blockchain to avoid holes in the historical data of users.
 	const [localHeight, remoteHeight] = [await database.lastBlockNumber(), await client.height()];
 
-	for (let i = localHeight; i <= remoteHeight; i += step) {
-		const batch: Promise<Record<string, any>>[] = [];
-		for (let j = i; j < i + step; j++) {
-			batch.push(client.blockWithTransactions(j));
-		}
-		await pEachSeries(batch, (block) => database.storeBlockWithTransactions(block));
+	// Load initial batch of size = {step}
+	for (let i = localHeight; i <= Math.min(localHeight + step, remoteHeight); i++) {
+		logger.info(`adding block ${i} to queue`);
+		fetchingQueue.add(() => client.blockWithTransactions(i))
+			.then(block => toBeProcessed[i] = block);
+	}
+
+	// Process sequential, in order and with no gaps
+	for (let i = localHeight; i <= remoteHeight; i++) {
+		logger.info(`processing block ${i}`);
+		await pWaitFor(() => toBeProcessed[i] !== undefined)
+			.then(() => database.storeBlockWithTransactions(toBeProcessed[i]))
+			.then(() => {
+				// Schedule fetching of next block (if still not done)
+				const nextBlock: number = i + step + 1;
+				if (nextBlock < remoteHeight) {
+					fetchingQueue.add(() => client.blockWithTransactions(nextBlock))
+						.then(block => toBeProcessed[nextBlock] = block);
+				}
+			});
 	}
 };
