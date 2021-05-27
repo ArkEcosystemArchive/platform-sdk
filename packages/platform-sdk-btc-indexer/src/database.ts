b847d1479a68db50b7616c43369fedd9fe22b446
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-27 18:02:13 -0300

    Generated client as part of the distributable

--- packages/platform-sdk-btc-indexer/src/database.ts
+++ packages/platform-sdk-btc-indexer/src/database.ts
@@ -1,5 +1,5 @@
 import { BigNumber } from "@arkecosystem/utils";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated";
 import envPaths from "env-paths";
 import { ensureFileSync } from "fs-extra";
 
