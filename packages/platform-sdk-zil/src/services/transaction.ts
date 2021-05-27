ed17bfed69d68d5694553735a44cf84c10bde4ae
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-27 18:22:37 +0300

    Restore transaction.ts

--- packages/platform-sdk-zil/src/services/transaction.ts
+++ packages/platform-sdk-zil/src/services/transaction.ts
@@ -43,11 +43,7 @@ export class TransactionService implements Contracts.TransactionService {
 		}
 
 		if (input.signatory.signingKey() === undefined) {
-			throw new Exceptions.MissingArgument(
-				this.constructor.name,
-				this.transfer.name,
-				"input.signatory.signingKey",
-			);
+			throw new Exceptions.MissingArgument(this.constructor.name, this.transfer.name, "sign.mnemonic");
 		}
 
 		const address = this.#zilliqa.wallet.addByMnemonic(input.signatory.signingKey());
