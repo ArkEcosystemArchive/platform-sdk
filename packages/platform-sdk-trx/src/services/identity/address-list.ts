c73a3b8384235bff3b217d0cd37e377aefdbc2d6
Brian Faust <hello@basecode.sh>
2021-05-28 06:20:42 +0300

    wip

--- packages/platform-sdk-trx/src/services/identity/address-list.ts
+++ packages/platform-sdk-trx/src/services/identity/address-list.ts
@@ -1,17 +1,5 @@
-import { Contracts } from "@arkecosystem/platform-sdk";
+import { Contracts, Services } from "@arkecosystem/platform-sdk";
 
 export class ExtendedAddressService extends Services.AbstractExtendedAddressService {
-	public async fromMnemonic(
-		mnemonic: string,
-		pageSize: number,
-	): Promise<Contracts.ExtendedAddressDataTransferObject[]> {
-		return [];
-	}
-
-	public async fromPrivateKey(
-		privateKey: string,
-		pageSize: number,
-	): Promise<Contracts.ExtendedAddressDataTransferObject[]> {
-		return [];
-	}
+	//
 }
--- packages/platform-sdk-zil/src/services/identity/address-list.ts
+++ packages/platform-sdk-zil/src/services/identity/address-list.ts
@@ -1,17 +1,5 @@
 import { Contracts, Services } from "@arkecosystem/platform-sdk";
 
 export class ExtendedAddressService extends Services.AbstractExtendedAddressService {
-	public async fromMnemonic(
-		mnemonic: string,
-		pageSize: number,
-	): Promise<Contracts.ExtendedAddressDataTransferObject[]> {
-		return [];
-	}
-
-	public async fromPrivateKey(
-		privateKey: string,
-		pageSize: number,
-	): Promise<Contracts.ExtendedAddressDataTransferObject[]> {
-		return [];
-	}
+	//
 }
