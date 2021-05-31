7506eab00ca93975827bec008f212bd6d1e1223e
Brian Faust <faustbrian@users.noreply.github.com>
2021-05-31 07:06:53 +0300

    refactor(xlm): remove `stellar-hd-wallet` dependency (#1634)

--- packages/platform-sdk-xlm/src/services/identity/address.test.ts
+++ packages/platform-sdk-xlm/src/services/identity/address.test.ts
@@ -14,6 +14,7 @@ describe("Address", () => {
 		expect(result).toMatchInlineSnapshot(`
 		Object {
 		  "address": "GCGYSPQBSQCJKNDXDISBSXAM3THK7MACUVZGEMXF6XRZCPGAWCUGXVNC",
+		  "path": "m/44'/148'/0'",
 		  "type": "bip44",
 		}
 	`);
@@ -25,6 +26,7 @@ describe("Address", () => {
 		expect(result).toMatchInlineSnapshot(`
 		Object {
 		  "address": "GCGYSPQBSQCJKNDXDISBSXAM3THK7MACUVZGEMXF6XRZCPGAWCUGXVNC",
+		  "path": "m/44'/148'/0'",
 		  "type": "bip44",
 		}
 	`);
