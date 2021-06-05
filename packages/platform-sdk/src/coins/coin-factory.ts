a8c592286d93582e0e32a2049159c0aa19a23a5a
Brian Faust <hello@basecode.sh>
2021-06-05 13:18:19 +0300

    wip

--- packages/platform-sdk/src/coins/coin-factory.ts
+++ packages/platform-sdk/src/coins/coin-factory.ts
@@ -8,10 +8,16 @@ import { Request } from "../../../platform-sdk-http-got";
 
 export class CoinFactory {
 	public static make(specification: CoinSpec, options: CoinOptions): Coin {
+		// @TODO: initialise the container here instead of inside the code
+
+		// @TODO: bind this to the container
 		const networks: NetworkRepository = new NetworkRepository(specification.manifest.networks);
 
+		// @TODO: bind this to the container
 		const config: Config = new Config(options, specification.schema);
+		// @TODO: bind this to the container
 		config.set(ConfigKey.Network, networks.get(config.get<string>("network")));
+		// @TODO: bind this to the container
 		config.set(ConfigKey.HttpClient, new Request());
 
 		return new Coin({
