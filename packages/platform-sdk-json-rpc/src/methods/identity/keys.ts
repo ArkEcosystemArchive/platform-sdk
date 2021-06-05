e74a86b457bbbc9c62ea5ca0824ecf47df7185bc
Brian Faust <faustbrian@users.noreply.github.com>
2021-06-05 04:22:49 +0300

    fix(json-rpc): remove `coin` and `network` parameters after consuming them (#1732)

--- packages/platform-sdk-json-rpc/src/methods/identity/keys.ts
+++ packages/platform-sdk-json-rpc/src/methods/identity/keys.ts
@@ -6,21 +6,21 @@ export const registerKeys = () => [
 	{
 		name: "identity.keyPair.fromMnemonic",
 		async method({ coin, network, mnemonic }) {
-			return (await makeCoin(coin, network)).identity().keyPair().fromMnemonic(mnemonic);
+			return (await makeCoin({ coin, network })).identity().keyPair().fromMnemonic(mnemonic);
 		},
 		schema: Joi.object({ ...baseSchema, mnemonic: Joi.string().required() }).required(),
 	},
 	{
 		name: "identity.keyPair.fromPrivateKey",
 		async method({ coin, network, privateKey }) {
-			return (await makeCoin(coin, network)).identity().keyPair().fromPrivateKey(privateKey);
+			return (await makeCoin({ coin, network })).identity().keyPair().fromPrivateKey(privateKey);
 		},
 		schema: Joi.object({ ...baseSchema, privateKey: Joi.string().required() }).required(),
 	},
 	{
 		name: "identity.keyPair.fromWIF",
 		async method({ coin, network, wif }) {
-			return (await makeCoin(coin, network)).identity().keyPair().fromWIF(wif);
+			return (await makeCoin({ coin, network })).identity().keyPair().fromWIF(wif);
 		},
 		schema: Joi.object({ ...baseSchema, wif: Joi.string().required() }).required(),
 	},
