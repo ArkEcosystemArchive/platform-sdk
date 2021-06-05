e7a135be6378522b5c2193b71d04588b57e068b1
Brian Faust <hello@basecode.sh>
2021-06-05 16:03:55 +0300

    wip

--- packages/platform-sdk/src/coins/coin-factory.ts
+++ packages/platform-sdk/src/coins/coin-factory.ts
@@ -9,16 +9,16 @@ import { Network, NetworkManifest } from "../networks";
 export class CoinFactory {
 	public static make(specification: CoinSpec, options: CoinOptions): Coin {
 		// Arrange
-		const config: ConfigRepository = new ConfigRepository(options, specification.schema);
-		const networks: NetworkRepository = new NetworkRepository(specification.manifest.networks);
+		const configRepository: ConfigRepository = new ConfigRepository(options, specification.schema);
+		const networkRepository: NetworkRepository = new NetworkRepository(specification.manifest.networks);
 
 		// Act
 		const container = new Container();
-		container.constant(ServiceKeys.ConfigRepository, config);
+		container.constant(ServiceKeys.ConfigRepository, configRepository);
 		container.constant(ServiceKeys.HttpClient, options.httpClient);
 		container.constant(ServiceKeys.Manifest, new Manifest(specification.manifest));
-		container.constant(ServiceKeys.Network, CoinFactory.#createNetwork(specification, config));
-		container.constant(ServiceKeys.NetworkRepository, networks);
+		container.constant(ServiceKeys.Network, CoinFactory.#createNetwork(specification, configRepository));
+		container.constant(ServiceKeys.NetworkRepository, networkRepository);
 		container.constant(ServiceKeys.Specification, specification);
 
 		// @TODO: use container to resolve this and inject values
