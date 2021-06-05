517c3e3aca0f399afd58ae74591b271bb4edc24f
faustbrian <faustbrian@users.noreply.github.com>
2021-06-05 14:13:16 +0000

    style: resolve style guide violations

--- packages/platform-sdk/src/services/big-number.test.ts
+++ packages/platform-sdk/src/services/big-number.test.ts
@@ -9,22 +9,30 @@ import { BigNumberService } from "./big-number.service";
 test.each([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])("#make(%s)", async (power) => {
 	const container = new Container();
 
-	container.constant(BindingType.ConfigRepository, new ConfigRepository(
-		{
-			network: {
-				currency: {
-					decimals: power,
+	container.constant(
+		BindingType.ConfigRepository,
+		new ConfigRepository(
+			{
+				network: {
+					currency: {
+						decimals: power,
+					},
 				},
 			},
-		},
-		Joi.object({
-			network: Joi.object({
-				currency: Joi.object({
-					decimals: Joi.number(),
+			Joi.object({
+				network: Joi.object({
+					currency: Joi.object({
+						decimals: Joi.number(),
+					}),
 				}),
 			}),
-		}),
-	));
+		),
+	);
 
-	expect(container.resolve(BigNumberService).make(`1${"0".repeat(power)}`).toHuman()).toBe("1");
+	expect(
+		container
+			.resolve(BigNumberService)
+			.make(`1${"0".repeat(power)}`)
+			.toHuman(),
+	).toBe("1");
 });
