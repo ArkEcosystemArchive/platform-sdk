836eef1f4d0c82981ea52f62d77d24afbd31c05a
Brian Faust <hello@basecode.sh>
2021-06-06 11:15:52 +0300

    wip

--- packages/platform-sdk/src/test.ts
+++ packages/platform-sdk/src/test.ts
@@ -1,30 +1,9 @@
 /* istanbul ignore file */
 
-import Joi, { Schema } from "joi";
-
 import { ConfigKey, ConfigRepository } from "./coins";
 import { Container, BindingType } from "./ioc";
 import { NetworkManifest } from "./networks";
 import { BigNumberService } from "./services";
-
-export const createConfig = (config?: object, schema?: Schema): ConfigRepository =>
-	new ConfigRepository(
-		config || {
-			network: {
-				currency: {
-					decimals: 1e8,
-				},
-			},
-		},
-		schema ||
-			Joi.object({
-				network: Joi.object({
-					currency: Joi.object({
-						decimals: Joi.number(),
-					}),
-				}),
-			}),
-	);
 
 export const createService = <T = any>({
 	config,
