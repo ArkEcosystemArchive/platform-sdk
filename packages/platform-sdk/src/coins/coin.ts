15f872f8fefb7f5122dc7bf773779b4987665fcf
Brian Faust <hello@basecode.sh>
2021-06-07 11:05:39 +0300

    fix(sdk): only unbind if binding exists

--- packages/platform-sdk/src/coins/coin.ts
+++ packages/platform-sdk/src/coins/coin.ts
@@ -51,41 +51,41 @@ export class Coin {
 			return;
 		}
 
-		this.#container.unbind(BindingType.AddressService);
-		this.#container.unbind(BindingType.BigNumberService);
-		this.#container.unbind(BindingType.ClientService);
-		this.#container.unbind(BindingType.DataTransferObjectService);
-		this.#container.unbind(BindingType.ExtendedAddressService);
-		this.#container.unbind(BindingType.FeeService);
-		this.#container.unbind(BindingType.KeyPairService);
-		this.#container.unbind(BindingType.KnownWalletService);
-		this.#container.unbind(BindingType.LedgerService);
-		this.#container.unbind(BindingType.LinkService);
-		this.#container.unbind(BindingType.MessageService);
-		this.#container.unbind(BindingType.MultiSignatureService);
-		this.#container.unbind(BindingType.PrivateKeyService);
-		this.#container.unbind(BindingType.PublicKeyService);
-		this.#container.unbind(BindingType.SignatoryService);
-		this.#container.unbind(BindingType.TransactionService);
-		this.#container.unbind(BindingType.WalletDiscoveryService);
-		this.#container.unbind(BindingType.WIFService);
-		this.#container.unbind(BindingType.WalletDiscoveryService);
-		this.#container.unbind(BindingType.TransactionService);
-		this.#container.unbind(BindingType.SignatoryService);
-		this.#container.unbind(BindingType.PublicKeyService);
-		this.#container.unbind(BindingType.PrivateKeyService);
-		this.#container.unbind(BindingType.MultiSignatureService);
-		this.#container.unbind(BindingType.MessageService);
-		this.#container.unbind(BindingType.LinkService);
-		this.#container.unbind(BindingType.LedgerService);
-		this.#container.unbind(BindingType.KnownWalletService);
-		this.#container.unbind(BindingType.KeyPairService);
-		this.#container.unbind(BindingType.FeeService);
-		this.#container.unbind(BindingType.ExtendedAddressService);
-		this.#container.unbind(BindingType.DataTransferObjectService);
-		this.#container.unbind(BindingType.ClientService);
-		this.#container.unbind(BindingType.BigNumberService);
-		this.#container.unbind(BindingType.AddressService);
+		this.#unbind(BindingType.AddressService);
+		this.#unbind(BindingType.BigNumberService);
+		this.#unbind(BindingType.ClientService);
+		this.#unbind(BindingType.DataTransferObjectService);
+		this.#unbind(BindingType.ExtendedAddressService);
+		this.#unbind(BindingType.FeeService);
+		this.#unbind(BindingType.KeyPairService);
+		this.#unbind(BindingType.KnownWalletService);
+		this.#unbind(BindingType.LedgerService);
+		this.#unbind(BindingType.LinkService);
+		this.#unbind(BindingType.MessageService);
+		this.#unbind(BindingType.MultiSignatureService);
+		this.#unbind(BindingType.PrivateKeyService);
+		this.#unbind(BindingType.PublicKeyService);
+		this.#unbind(BindingType.SignatoryService);
+		this.#unbind(BindingType.TransactionService);
+		this.#unbind(BindingType.WalletDiscoveryService);
+		this.#unbind(BindingType.WIFService);
+		this.#unbind(BindingType.WalletDiscoveryService);
+		this.#unbind(BindingType.TransactionService);
+		this.#unbind(BindingType.SignatoryService);
+		this.#unbind(BindingType.PublicKeyService);
+		this.#unbind(BindingType.PrivateKeyService);
+		this.#unbind(BindingType.MultiSignatureService);
+		this.#unbind(BindingType.MessageService);
+		this.#unbind(BindingType.LinkService);
+		this.#unbind(BindingType.LedgerService);
+		this.#unbind(BindingType.KnownWalletService);
+		this.#unbind(BindingType.KeyPairService);
+		this.#unbind(BindingType.FeeService);
+		this.#unbind(BindingType.ExtendedAddressService);
+		this.#unbind(BindingType.DataTransferObjectService);
+		this.#unbind(BindingType.ClientService);
+		this.#unbind(BindingType.BigNumberService);
+		this.#unbind(BindingType.AddressService);
 	}
 
 	public hasBeenSynchronized(): boolean {
@@ -178,5 +178,11 @@ export class Coin {
 
 	public wif(): WIFService {
 		return this.#container.get(BindingType.WIFService);
+	}
+
+	#unbind(key: symbol): void {
+		if (this.#container.has(key)) {
+			this.#container.unbind(key);
+		}
 	}
 }
