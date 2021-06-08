5e4f8418867f6ae2d7738f0000a78acfdab25c97
Brian Faust <hello@basecode.sh>
2021-06-08 15:45:03 +0300

    wip

--- packages/platform-sdk/src/ioc/container.test.ts
+++ packages/platform-sdk/src/ioc/container.test.ts
@@ -11,6 +11,16 @@ it("should prevent multiple bindings of the same key", () => {
 	expect(() => container.constant(BindingType.AddressService, "value")).toThrow(/Duplicate binding attempted/);
 });
 
+it("should bind values independent from container instances", () => {
+	const container1 = new Container();
+
+	expect(() => container1.constant(BindingType.AddressService, "value")).not.toThrow(/Duplicate binding attempted/);
+
+	const container2 = new Container();
+
+	expect(() => container2.constant(BindingType.AddressService, "value")).not.toThrow(/Duplicate binding attempted/);
+});
+
 it("should bind a value and be able to retrieve it", () => {
 	const container = new Container();
 
