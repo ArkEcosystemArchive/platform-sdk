afccd8f0e14868f5658a5c5ec22f987cbed7480d
Mariano Goldman <marianogoldman@users.noreply.github.com>
2021-06-07 22:06:02 -0300

    refactor(btc-indexer): use upserts for blocks, transactions and parts (#1766)

--- packages/platform-sdk-btc-indexer/src/database.ts
+++ packages/platform-sdk-btc-indexer/src/database.ts
@@ -91,17 +91,18 @@ export class Database {
 	 * @memberof Database
 	 */
 	async #storeBlock(block): Promise<void> {
-		try {
-			await this.#prisma.block.create({
-				data: {
-					hash: block.hash,
-					height: block.height,
-				},
-			});
-		} catch {
-			// Ignore, there's nothing to update if already exists
-			// We could query for existence before creation, but it doesn't really make sense
-		}
+		await this.#prisma.block.upsert({
+			where: {
+				height: block.height,
+			},
+			create: {
+				hash: block.hash,
+				height: block.height,
+			},
+			update: {
+				hash: block.hash,
+			},
+		});
 	}
 
 	/**
@@ -160,21 +161,48 @@ export class Database {
 			}),
 		);
 
-		try {
-			await this.#prisma.transaction.create({
-				data: {
-					block_id: blockId,
-					hash: transaction.txid,
-					time: transaction.time,
-					amount: BigInt(amount.toString()),
-					fee: BigInt(fee.toString()),
-					transaction_parts: {
-						create: outputs.map((output) => ({
+		await this.#prisma.transaction.upsert({
+			where: {
+				hash: transaction.txid,
+			},
+			create: {
+				block_id: blockId,
+				hash: transaction.txid,
+				time: transaction.time,
+				amount: BigInt(amount.toString()),
+				fee: BigInt(fee.toString()),
+				transaction_parts: {
+					create: outputs.map((output) => ({
+						output_idx: output.idx,
+						amount: BigInt(output.amount.toString()),
+						address: JSON.stringify(output.addresses),
+					})),
+				},
+			},
+			update: {
+				block_id: blockId,
+				hash: transaction.txid,
+				time: transaction.time,
+				amount: BigInt(amount.toString()),
+				fee: BigInt(fee.toString()),
+				transaction_parts: {
+					upsert: outputs.map((output) => {
+						const outputData = {
 							output_idx: output.idx,
 							amount: BigInt(output.amount.toString()),
 							address: JSON.stringify(output.addresses),
-						})),
-					},
+						};
+						return {
+							where: {
+								output_hash_output_idx: {
+									output_hash: transaction.txid,
+									output_idx: output.idx,
+								},
+							},
+							create: outputData,
+							update: outputData,
+						};
+					}),
 				},
 			});
 		} catch (e) {
