92d662d3cb87279c72d3e7b7a4e7292d54003cc4
Brian Faust <hello@basecode.sh>
2021-06-08 16:26:17 +0300

    fix(ark): `compendia` => `bind`

--- packages/platform-sdk-ark/src/schema.ts
+++ packages/platform-sdk-ark/src/schema.ts
@@ -1,7 +1,7 @@
 import { ValidatorSchema } from "@arkecosystem/platform-sdk-support";
 
 export const schema = ValidatorSchema.object({
-	network: ValidatorSchema.string().valid("ark.mainnet", "ark.devnet", "compendia.mainnet", "compendia.testnet"),
+	network: ValidatorSchema.string().valid("ark.mainnet", "ark.devnet", "bind.mainnet", "bind.testnet"),
 	httpClient: ValidatorSchema.object(),
 	services: ValidatorSchema.object({
 		ledger: ValidatorSchema.object({
