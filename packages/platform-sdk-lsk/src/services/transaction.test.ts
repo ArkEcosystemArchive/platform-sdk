950ee76d29b980d89f3d5fed03412e6ef39b0d1f
Brian Faust <hello@basecode.sh>
2021-06-08 10:20:06 +0300

    wip

--- packages/platform-sdk-lsk/src/services/transaction.test.ts
+++ packages/platform-sdk-lsk/src/services/transaction.test.ts
@@ -3,6 +3,7 @@ import "jest-extended";
 import { IoC, Signatories } from "@arkecosystem/platform-sdk";
 import LedgerTransportNodeHID from "@ledgerhq/hw-transport-node-hid-singleton";
 
+import { identity } from "../../test/fixtures/identity";
 import { createService } from "../../test/helpers";
 import { AddressService } from "./address";
 import { ClientService } from "./client";
@@ -24,6 +25,7 @@ beforeAll(async () => {
 	});
 });
 
+// @TODO: remove, just here for ledger testing
 jest.setTimeout(60000);
 
 describe("TransactionService", () => {
@@ -83,95 +85,95 @@ describe("TransactionService", () => {
 				}).broadcast([signed]),
 			);
 
-			// expect(result).toBeObject();
+			expect(result).toBeObject();
 		});
 	});
 
-	// describe("#secondSignature", () => {
-	// 	it("should verify", async () => {
-	// 		const result = await subject.secondSignature({
-	// 			signatory: new Signatories.Signatory(
-	// 				new Signatories.SecondaryMnemonicSignatory({
-	// 					signingKey: identity.mnemonic,
-	// 					confirmKey: identity.mnemonic,
-	// 					address: "15957226662510576840L",
-	// 					publicKey: "publicKey",
-	// 					privateKey: "privateKey",
-	// 				}),
-	// 			),
-	// 			data: {
-	// 				mnemonic: identity.mnemonic,
-	// 			},
-	// 		});
+	describe("#secondSignature", () => {
+		it("should verify", async () => {
+			const result = await subject.secondSignature({
+				signatory: new Signatories.Signatory(
+					new Signatories.SecondaryMnemonicSignatory({
+						signingKey: identity.mnemonic,
+						confirmKey: identity.mnemonic,
+						address: "15957226662510576840L",
+						publicKey: "publicKey",
+						privateKey: "privateKey",
+					}),
+				),
+				data: {
+					mnemonic: identity.mnemonic,
+				},
+			});
 
-	// 		expect(result).toBeObject();
-	// 	});
-	// });
+			expect(result).toBeObject();
+		});
+	});
 
-	// describe("#delegateRegistration", () => {
-	// 	it("should verify", async () => {
-	// 		const result = await subject.delegateRegistration({
-	// 			signatory: new Signatories.Signatory(
-	// 				new Signatories.MnemonicSignatory({
-	// 					signingKey: identity.mnemonic,
-	// 					address: "15957226662510576840L",
-	// 					publicKey: "publicKey",
-	// 					privateKey: "privateKey",
-	// 				}),
-	// 			),
-	// 			data: {
-	// 				username: "johndoe",
-	// 			},
-	// 		});
+	describe("#delegateRegistration", () => {
+		it("should verify", async () => {
+			const result = await subject.delegateRegistration({
+				signatory: new Signatories.Signatory(
+					new Signatories.MnemonicSignatory({
+						signingKey: identity.mnemonic,
+						address: "15957226662510576840L",
+						publicKey: "publicKey",
+						privateKey: "privateKey",
+					}),
+				),
+				data: {
+					username: "johndoe",
+				},
+			});
 
-	// 		expect(result).toBeObject();
-	// 	});
-	// });
+			expect(result).toBeObject();
+		});
+	});
 
-	// describe("#vote", () => {
-	// 	it("should verify", async () => {
-	// 		const result = await subject.vote({
-	// 			signatory: new Signatories.Signatory(
-	// 				new Signatories.MnemonicSignatory({
-	// 					signingKey: identity.mnemonic,
-	// 					address: "15957226662510576840L",
-	// 					publicKey: "publicKey",
-	// 					privateKey: "privateKey",
-	// 				}),
-	// 			),
-	// 			data: {
-	// 				votes: ["9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f"],
-	// 				unvotes: [],
-	// 			},
-	// 		});
+	describe("#vote", () => {
+		it("should verify", async () => {
+			const result = await subject.vote({
+				signatory: new Signatories.Signatory(
+					new Signatories.MnemonicSignatory({
+						signingKey: identity.mnemonic,
+						address: "15957226662510576840L",
+						publicKey: "publicKey",
+						privateKey: "privateKey",
+					}),
+				),
+				data: {
+					votes: ["9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f"],
+					unvotes: [],
+				},
+			});
 
-	// 		expect(result).toBeObject();
-	// 	});
-	// });
+			expect(result).toBeObject();
+		});
+	});
 
-	// describe("#multiSignature", () => {
-	// 	it("should verify", async () => {
-	// 		const result = await subject.multiSignature({
-	// 			signatory: new Signatories.Signatory(
-	// 				new Signatories.MnemonicSignatory({
-	// 					signingKey: identity.mnemonic,
-	// 					address: "15957226662510576840L",
-	// 					publicKey: "publicKey",
-	// 					privateKey: "privateKey",
-	// 				}),
-	// 			),
-	// 			data: {
-	// 				publicKeys: [
-	// 					"9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f",
-	// 					"141b16ac8d5bd150f16b1caa08f689057ca4c4434445e56661831f4e671b7c0a",
-	// 					"3ff32442bb6da7d60c1b7752b24e6467813c9b698e0f278d48c43580da972135",
-	// 				],
-	// 				lifetime: 34,
-	// 				min: 2,
-	// 			},
-	// 		});
+	describe("#multiSignature", () => {
+		it("should verify", async () => {
+			const result = await subject.multiSignature({
+				signatory: new Signatories.Signatory(
+					new Signatories.MnemonicSignatory({
+						signingKey: identity.mnemonic,
+						address: "15957226662510576840L",
+						publicKey: "publicKey",
+						privateKey: "privateKey",
+					}),
+				),
+				data: {
+					publicKeys: [
+						"9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f",
+						"141b16ac8d5bd150f16b1caa08f689057ca4c4434445e56661831f4e671b7c0a",
+						"3ff32442bb6da7d60c1b7752b24e6467813c9b698e0f278d48c43580da972135",
+					],
+					lifetime: 34,
+					min: 2,
+				},
+			});
 
-	// 		expect(result).toBeObject();
-	// 	});
-	// });
+			expect(result).toBeObject();
+		});
+	});
 });
