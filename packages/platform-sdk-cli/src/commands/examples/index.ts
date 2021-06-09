da74dab711a966a4621cc24b7376e4fa7a3c44f6
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-06-09 18:37:16 -0300

    undo

--- packages/platform-sdk-cli/src/commands/examples/index.ts
+++ packages/platform-sdk-cli/src/commands/examples/index.ts
@@ -6,7 +6,6 @@ import { transferFundsWithADA } from "./commands/ada/transfer-funds";
 import { transferFundsWithARK } from "./commands/ark/transfer-funds";
 import { createProfileWithWallets } from "./commands/create-profile-with-wallets";
 import { openExistingProfile } from "./commands/open-existing-profile";
-import { transferFundsWithLSK } from "./commands/lsk/transfer-funds";
 import { transferFundsWithTRX } from "./commands/trx/transfer-funds";
 import { listTransactionsWithXLM } from "./commands/xlm/list-transactions";
 import { signMessageWithXLM } from "./commands/xlm/sign-message";
@@ -24,7 +23,6 @@ export const createProfile = async (env: Environment): Promise<void> => {
 			{ title: "Open Existing Profile", value: "open-existing-profile" },
 			{ title: "[ADA] Transfer Funds", value: "transfer-funds-with-ada" },
 			{ title: "[ARK] Transfer Funds", value: "transfer-funds-with-ark" },
-			{ title: "[LSK] Transfer Funds", value: "transfer-funds-with-lsk" },
 			{ title: "[TRX] Transfer Funds", value: "transfer-funds-with-trx" },
 			{ title: "[XLM] List Transactions", value: "list-transactions-with-xlm" },
 			{ title: "[XLM] Sign Message", value: "sign-message-with-xlm" },
@@ -43,7 +41,6 @@ export const createProfile = async (env: Environment): Promise<void> => {
 		"open-existing-profile": () => openExistingProfile(env),
 		"transfer-funds-with-ada": () => transferFundsWithADA(env),
 		"transfer-funds-with-ark": () => transferFundsWithARK(env),
-		"transfer-funds-with-lsk": () => transferFundsWithLSK(env),
 		"transfer-funds-with-trx": () => transferFundsWithTRX(env),
 		"list-transactions-with-xlm": () => listTransactionsWithXLM(env),
 		"sign-message-with-xlm": () => signMessageWithXLM(env),
