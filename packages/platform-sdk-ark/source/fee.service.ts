5c34036d90c441c63ce6b0d1208d69848d9d07a4
Brian Faust <faustbrian@users.noreply.github.com>
2021-06-10 09:42:22 +0300

    fix(ark): limit all fees to static (#1790)

--- packages/platform-sdk-ark/source/fee.service.ts
+++ packages/platform-sdk-ark/source/fee.service.ts
@@ -32,11 +32,14 @@ export class FeeService extends Services.AbstractFeeService {
 
 	#transform(type: string, typeGroup: number, staticFees: object, dynamicFees: object): Services.TransactionFee {
 		const dynamicFee = dynamicFees[typeGroup][type];
+		const minimumFee = this.bigNumberService.make(dynamicFee?.min || "0");
+		const averageFee = this.bigNumberService.make(dynamicFee?.avg || "0");
+		const maximumFee = this.bigNumberService.make(staticFees[typeGroup][type]);
 
 		return {
 			static: this.bigNumberService.make(staticFees[typeGroup][type]),
-			min: this.bigNumberService.make(dynamicFee?.min || "0"),
-			avg: this.bigNumberService.make(dynamicFee?.avg || "0"),
+			min: minimumFee.isGreaterThan(maximumFee) ? maximumFee : minimumFee,
+			avg: averageFee.isGreaterThan(maximumFee) ? maximumFee : averageFee,
 			max: this.bigNumberService.make(staticFees[typeGroup][type]),
 		};
 	}
