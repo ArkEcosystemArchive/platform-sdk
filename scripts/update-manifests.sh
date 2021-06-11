13f16dd2eb8b6b01e73df05d1e304776d764116e
Brian Faust <hello@basecode.sh>
2021-06-11 15:41:16 +0300

    wip

--- scripts/update-manifests.sh
+++ scripts/update-manifests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
-    cd $dir
-    echo $PWD
-		node ../../common/scripts/manifest-updater.js
-    cd ../..
+	cd $dir
+	echo $PWD
+	node ../../common/scripts/manifest-updater.js
+	cd ../..
 done
