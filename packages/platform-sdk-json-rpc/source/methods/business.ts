1b07a4e6c433d7dc41a2b50c3d53f178bf8263ec
Brian Faust <hello@basecode.sh>
2021-06-20 09:37:07 +0300

    wip

---
+++ packages/platform-sdk-json-rpc/source/methods/business.ts
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+import { baseSchema, makeCoin } from "../helpers";
+
+export const registerBusiness = () => [
+	{
+		name: "business.withdraw",
+		async method({ coin, network, amount, to, mnemonic }) {
+			const business = await makeCoin({ coin, network });
+
+			return business.client().broadcast([
+				await business.transaction().transfer({
+					data: {
+						amount: amount - (await business.fee().all()).transfer.avg.toHuman(),
+						to,
+					},
+					signatory: await business.signatory().mnemonic(mnemonic),
+				})
+			]);
+		},
+		schema: Joi.object({
+			...baseSchema,
+			amount: Joi.number().required(),
+			to: Joi.string().required(),
+			mnemonic: Joi.string().required(),
+		}).required(),
+	},
+];
