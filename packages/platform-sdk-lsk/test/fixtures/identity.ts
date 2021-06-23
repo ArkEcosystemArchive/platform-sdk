6a88694e73007ea1ae994f922bf0c0f059c64d36
Brian Faust <hello@basecode.sh>
2021-06-23 04:53:59 +0300

    wip

--- packages/platform-sdk-lsk/test/fixtures/identity.ts
+++ packages/platform-sdk-lsk/test/fixtures/identity.ts
@@ -3,7 +3,7 @@ export const identity = {
 	privateKey: "d8839c2432bfd0a67ef10a804ba991eabba19f154a3d707917681d45822a5712",
 	publicKey: "ceb7bb7475a14b729eba069dfb27715331727a910acf5773a950ed4f863c89ed",
 	address: "15957226662510576840L",
-	wif: "9aLBYjSP2C8rKbq3DYo1oHgqkytLkTaBMTtUZ74gPqL26ksTqRM",
+	wif: "er7fZ4K2CG9eh6j2nPL6XJerD85cApHShKgCchmEsnJwr9W8QY2v",
 	mnemonic: "this is a top secret passphrase",
 	// Multi Signature
 	multiSignature: { min: 0, publicKeys: [] },
