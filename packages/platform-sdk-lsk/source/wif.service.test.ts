6d7cb1d69a1326d8ffde82c06ee10619f3101122
Brian Faust <faustbrian@users.noreply.github.com>
2021-06-29 04:12:21 +0300

    feat(lsk): implement `WIFService` (#1873)

---
+++ packages/platform-sdk-lsk/source/wif.service.test.ts
@@ -0,0 +1,38 @@
+import "jest-extended";
+
+import { Exceptions } from "@arkecosystem/platform-sdk";
+
+import { identity } from "../test/fixtures/identity";
+import { createService } from "../test/mocking";
+import { WIFService } from "./wif.service";
+import { WIF } from "@arkecosystem/platform-sdk-crypto";
+
+let subject: WIFService;
+
+beforeEach(async () => {
+	subject = createService(WIFService);
+});
+
+describe("WIF", () => {
+	it("should generate an output from a mnemonic", async () => {
+		const result = await subject.fromMnemonic(identity.mnemonic);
+
+		expect(result).toEqual({ wif: identity.wif });
+		expect(WIF.decode(result.wif).privateKey).toBe(identity.privateKey);
+	});
+
+	it("should fail to generate an output from an invalid mnemonic", async () => {
+		await expect(subject.fromMnemonic(undefined!)).rejects.toThrow(Exceptions.CryptoException);
+	});
+
+	it("should generate an output from a private key", async () => {
+		const result = await subject.fromPrivateKey(identity.privateKey);
+
+		expect(result).toEqual({ wif: identity.wif });
+		expect(WIF.decode(result.wif).privateKey).toBe(identity.privateKey);
+	});
+
+	it("should fail to generate an output from an invalid private key", async () => {
+		await expect(subject.fromPrivateKey(undefined!)).rejects.toThrow(Exceptions.CryptoException);
+	});
+});
