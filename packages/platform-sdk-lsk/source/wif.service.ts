7a00e96525b84bfb73738ef56978163594338de9
Brian Faust <hello@basecode.sh>
2021-06-29 04:11:52 +0300

    wip

--- packages/platform-sdk-lsk/source/wif.service.ts
+++ packages/platform-sdk-lsk/source/wif.service.ts
@@ -1,18 +1,21 @@
-import { Exceptions, IoC, Services } from "@arkecosystem/platform-sdk";
+import { Coins, Exceptions, IoC, Services } from "@arkecosystem/platform-sdk";
 import { WIF } from "@arkecosystem/platform-sdk-crypto";
 import { getPrivateAndPublicKeyFromPassphrase } from "@liskhq/lisk-cryptography";
 
 @IoC.injectable()
 export class WIFService extends Services.AbstractWIFService {
+	@IoC.inject(IoC.BindingType.ConfigRepository)
+	protected readonly configRepository!: Coins.ConfigRepository;
+
 	public override async fromMnemonic(
 		mnemonic: string,
 		options?: Services.IdentityOptions,
 	): Promise<Services.WIFDataTransferObject> {
 		try {
 			return {
-				// 255 isn't the real version but I couldn't find any number documented
 				wif: WIF.encode({
-					version: 255,
+					// Technically this isn't the WIF version but LSK has none.
+					version: this.configRepository.get<number>("network.constants.slip44"),
 					privateKey: getPrivateAndPublicKeyFromPassphrase(mnemonic).privateKey,
 					compressed: true,
 				}),
@@ -25,8 +28,12 @@ export class WIFService extends Services.AbstractWIFService {
 	public override async fromPrivateKey(privateKey: string): Promise<Services.WIFDataTransferObject> {
 		try {
 			return {
-				// 255 isn't the real version but I couldn't find any number documented
-				wif: WIF.encode({ version: 255, privateKey, compressed: true }),
+				wif: WIF.encode({
+					// Technically this isn't the WIF version but LSK has none.
+					version: this.configRepository.get<number>("network.constants.slip44"),
+					privateKey,
+					compressed: true,
+				}),
 			};
 		} catch (error) {
 			throw new Exceptions.CryptoException(error);
--- packages/platform-sdk-lsk/test/fixtures/identity.ts
+++ packages/platform-sdk-lsk/test/fixtures/identity.ts
@@ -3,7 +3,7 @@ export const identity = {
 	privateKey: "d8839c2432bfd0a67ef10a804ba991eabba19f154a3d707917681d45822a5712",
 	publicKey: "ceb7bb7475a14b729eba069dfb27715331727a910acf5773a950ed4f863c89ed",
 	address: "15957226662510576840L",
-	wif: "er7fZ4K2CG9eh6j2nPL6XJerD85cApHShKgCchmEsnJwr9W8QY2v",
+	wif: "LwxNNXiB1fJ1nV85sDa3CFCv6YmgABUUzsJRujqnTt7pn6RR3cf7",
 	mnemonic: "this is a top secret passphrase",
 	// Multi Signature
 	multiSignature: { min: 0, publicKeys: [] },
