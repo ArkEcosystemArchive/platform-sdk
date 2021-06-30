6e5c85daf698c08ef27581d7d030aff74b809a7c
faustbrian <faustbrian@users.noreply.github.com>
2021-06-30 08:15:47 +0000

    style: resolve style guide violations

--- packages/platform-sdk-eth/test/fixtures/identity.ts
+++ packages/platform-sdk-eth/test/fixtures/identity.ts
@@ -1,20 +1,20 @@
 export const identity = {
-    // Standard
-    privateKey: "9486c278c67ca7da28a521c0f080077483b0d0e72fc0f4e0b928f8d38f7bae50",
-    publicKey:
-        "6cf5dea4ea78886c216b8b3d696ea296ff047c952ab5a7be95f706dea2defd64197edae8a969df5f7a60b972231a9e7c9672f842acb01855bc950ae82a7d156f",
-    address: "0x986A007A43D65FF18D040ACDAD844CFE7C349135",
-    wif: "not-available",
-    mnemonic: "bomb open frame quit success evolve gain donate prison very rent later",
-    // Multi Signature
-    multiSignature: {
-        min: 2,
-        publicKeys: [
-            "026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01",
-            "02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9",
-            "03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9",
-        ],
-    },
-    multiSignatureAddress: "36NUkt6FWUi3LAWBqWRdDmdTWbt91Yvfu7",
-    multiSignaturePublicKey: "",
+	// Standard
+	privateKey: "9486c278c67ca7da28a521c0f080077483b0d0e72fc0f4e0b928f8d38f7bae50",
+	publicKey:
+		"6cf5dea4ea78886c216b8b3d696ea296ff047c952ab5a7be95f706dea2defd64197edae8a969df5f7a60b972231a9e7c9672f842acb01855bc950ae82a7d156f",
+	address: "0x986A007A43D65FF18D040ACDAD844CFE7C349135",
+	wif: "not-available",
+	mnemonic: "bomb open frame quit success evolve gain donate prison very rent later",
+	// Multi Signature
+	multiSignature: {
+		min: 2,
+		publicKeys: [
+			"026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01",
+			"02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9",
+			"03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9",
+		],
+	},
+	multiSignatureAddress: "36NUkt6FWUi3LAWBqWRdDmdTWbt91Yvfu7",
+	multiSignaturePublicKey: "",
 };
