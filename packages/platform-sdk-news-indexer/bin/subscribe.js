9a4b20e97af5f2610b18ba8c397663fb1e37aa8a
Brian Faust <hello@basecode.sh>
2021-06-30 10:48:50 +0300

    wip

--- packages/platform-sdk-news-indexer/bin/subscribe.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const { subscribe } = require("../distribution");
-
-const { flags } = require("./meow");
-
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-subscribe(flags);
