3058302ccfd40528753250ca79f69ab428e659f1
Brian Faust <hello@basecode.sh>
2021-06-30 10:24:54 +0300

    wip

--- packages/platform-sdk-atom/source/transaction.service.test.ts
+++ packages/platform-sdk-atom/source/transaction.service.test.ts
@@ -31,23 +31,23 @@ describe("TransactionService", () => {
 	describe("#transfer", () => {
 		it("should verify", async () => {
 			nock("https://stargate.cosmos.network")
-				.get("/auth/accounts/cosmos1fvxjdyfdvat5g0ee7jmyemwl2n95ad7negf7ap")
+				.get("/auth/accounts/cosmos1wqus3z856rwadvum3l0lg0nl4sc957vq0wn8d0")
 				.reply(200, require(`${__dirname}/../test/fixtures/client/wallet.json`))
-				.get("/bank/balances/cosmos1fvxjdyfdvat5g0ee7jmyemwl2n95ad7negf7ap")
+				.get("/bank/balances/cosmos1wqus3z856rwadvum3l0lg0nl4sc957vq0wn8d0")
 				.reply(200, require(`${__dirname}/../test/fixtures/client/wallet-balance.json`));
 
 			const result = await subject.transfer({
 				signatory: new Signatories.Signatory(
 					new Signatories.MnemonicSignatory({
 						signingKey: "bomb open frame quit success evolve gain donate prison very rent later",
-						address: "cosmos1fvxjdyfdvat5g0ee7jmyemwl2n95ad7negf7ap",
+						address: "cosmos1wqus3z856rwadvum3l0lg0nl4sc957vq0wn8d0",
 						publicKey: "publicKey",
 						privateKey: "privateKey",
 					}),
 				),
 				data: {
 					amount: 1,
-					to: "cosmos1fvxjdyfdvat5g0ee7jmyemwl2n95ad7negf7ap",
+					to: "cosmos1wqus3z856rwadvum3l0lg0nl4sc957vq0wn8d0",
 				},
 			});
 
